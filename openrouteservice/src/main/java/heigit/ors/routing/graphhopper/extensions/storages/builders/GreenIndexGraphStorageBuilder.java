226353061f58cc117f431d1e2ecd3236a69d6ebf
Lu Liu <nudtlliu@gmail.com>
2017-03-13 22:13:31 +0100

    read the csv file containing green indices of streets

--- openrouteservice/src/main/java/heigit/ors/routing/graphhopper/extensions/storages/builders/GreenIndexGraphStorageBuilder.java
+++ openrouteservice/src/main/java/heigit/ors/routing/graphhopper/extensions/storages/builders/GreenIndexGraphStorageBuilder.java
@@ -4,14 +4,57 @@ import com.graphhopper.GraphHopper;
 import com.graphhopper.reader.OSMWay;
 import com.graphhopper.storage.GraphExtension;
 import com.graphhopper.util.EdgeIteratorState;
+import heigit.ors.routing.graphhopper.extensions.storages.GreenIndexGraphStorage;
+
+import com.opencsv.CSVReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by lliu on 13/03/2017.
  */
 public class GreenIndexGraphStorageBuilder extends AbstractGraphStorageBuilder {
+    private GreenIndexGraphStorage _storage;
+    private String _greenIndexFile;
+    private Map<Long, Double> _greenIndices = new HashMap<>();
+
+    public GreenIndexGraphStorageBuilder() {
+
+    }
+
     @Override
     public GraphExtension init(GraphHopper graphhopper) throws Exception {
-        return null;
+        if (_storage != null)
+            throw new Exception("GraphStorageBuilder has been already initialized.");
+
+        _greenIndexFile = _parameters.get("filepath");
+        // TODO Check if the _greenIndexFile exists
+        String csvFile = _greenIndexFile;
+        _greenIndices = readGreenIndicesFromCSV(csvFile);
+        _storage = new GreenIndexGraphStorage();
+
+        return _storage;
+    }
+
+    private Map<Long, Double> readGreenIndicesFromCSV(String csvFile) throws IOException {
+        Map<Long, Double> csvData = new HashMap<>();
+
+        CSVReader reader = null;
+        try {
+            reader = new CSVReader(new FileReader(csvFile));
+            String[] line;
+            // Jump the header line
+            reader.readNext();
+            while ((line = reader.readNext()) != null) {
+                csvData.put(Long.parseLong(line[0]), Double.parseDouble(line[1]));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        }
+        return csvData;
     }
 
     @Override
@@ -26,6 +69,6 @@ public class GreenIndexGraphStorageBuilder extends AbstractGraphStorageBuilder {
 
     @Override
     public String getName() {
-        return null;
+        return "GreenIndex";
     }
 }
