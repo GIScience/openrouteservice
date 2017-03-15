db4d1b0f70aa8ab924018117a0c629ce30ed1bb8
Lu Liu <nudtlliu@gmail.com>
2017-03-15 15:07:42 +0100

    the ext_greenindex file can be generated. it looks ok, but has no idea if it will work

--- openrouteservice/src/main/java/heigit/ors/routing/graphhopper/extensions/storages/builders/GreenIndexGraphStorageBuilder.java
+++ openrouteservice/src/main/java/heigit/ors/routing/graphhopper/extensions/storages/builders/GreenIndexGraphStorageBuilder.java
@@ -7,6 +7,7 @@ import com.graphhopper.util.EdgeIteratorState;
 import heigit.ors.routing.graphhopper.extensions.storages.GreenIndexGraphStorage;
 
 import com.opencsv.CSVReader;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
@@ -17,7 +18,7 @@ import java.util.*;
 public class GreenIndexGraphStorageBuilder extends AbstractGraphStorageBuilder {
     private GreenIndexGraphStorage _storage;
     private Map<Long, Double> _greenIndices = new HashMap<>();
-    private static int SLOT_COUNT = 256;
+    private static int SLOT_COUNT = 64;
     private Map<Byte, SlotRange> _slots = new HashMap<>(SLOT_COUNT);
 
     public GreenIndexGraphStorageBuilder() {
@@ -43,7 +44,7 @@ public class GreenIndexGraphStorageBuilder extends AbstractGraphStorageBuilder {
         double min = Collections.min(_greenIndices.values());
         double step = (max - min) / SLOT_COUNT;
         // Divide the range of raw green index values into SLOT_COUNT,
-        // then map the raw value to [0..255]
+        // then map the raw value to [0..SLOT_COUNT - 1]
         for (byte i = 0; i < SLOT_COUNT; i++) {
             _slots.put(i, new SlotRange(min + i * step, min + (i + 1) * step));
         }
@@ -78,7 +79,12 @@ public class GreenIndexGraphStorageBuilder extends AbstractGraphStorageBuilder {
     private class SlotRange {
         double left = 0.0;
         double right = 0.0;
-        SlotRange(double l, double r) {this.left=l;this.right=r;}
+
+        SlotRange(double l, double r) {
+            this.left = l;
+            this.right = r;
+        }
+
         boolean within(double val) {
             // check if the @val falls in (left, right] range
             if ((val <= left) || (val > right)) return false;
@@ -87,11 +93,18 @@ public class GreenIndexGraphStorageBuilder extends AbstractGraphStorageBuilder {
     }
 
     private byte calcGreenIndex(long id) {
+        Double gi = _greenIndices.get(id);
         for (Map.Entry<Byte, SlotRange> s : _slots.entrySet()) {
-            if (s.getValue().within(_greenIndices.get(id)))
-                return s.getKey();
+            if (gi != null) {
+                if (s.getValue().within(_greenIndices.get(id)))
+                    return s.getKey();
+            } else {
+                // No such @id key in the _greenIndices, or the value of it is null
+                // We set its green level to SLOT_COUNT/2 indicating the middle value for such cases
+                return (byte) (SLOT_COUNT / 2);
+            }
         }
-        return (byte)255;
+        return (byte) (SLOT_COUNT - 1);
     }
 
     @Override
