ae51454f77db9468ea4d6558add26eb258710b5a
Rungee <maksim.rylov@gmail.com>
2017-03-28 12:25:28 +0200

    SQL query improvements for long linestrings

--- openrouteservice/docs/services/locations/providers/postgresql/scripts/functions.sql
+++ openrouteservice/docs/services/locations/providers/postgresql/scripts/functions.sql
@@ -5,10 +5,14 @@ CREATE FUNCTION ORS_FindLocations(table_name text, condition text, geom_wkb byte
 DECLARE
   geog_feature geography; 
   geom_feature geometry;
+  geom_feature_simplified geometry;
   geom_env geometry;
   geom_type text;
+  geom_length float;
   max_latitude float;
-  factor float;
+  distance_factor float;
+  distance_buffer float;
+  simplification_tolerance float;
   rec RECORD;
   query text;
 BEGIN
@@ -32,22 +36,44 @@ BEGIN
         IF (limit_size > 0) THEN
            query := query || ' LIMIT '  || limit_size::text; 
         END IF;
-
+        
 	FOR rec IN EXECUTE format(query) USING geog_feature, distance LOOP
-   	  --IF ST_DWithin(rec.location, geog_feature, distance) THEN
+   	  --IF ST_DWithin(rec.location, geog_feature, distance) THEN 
             rec.distance := round(ST_Distance(rec.location, geog_feature)::numeric, 2);   	  
             IF (rec.distance <= distance) THEN
 	       RETURN NEXT rec;
 	    END IF;   
-        END LOOP;        
+        END LOOP;
         
       WHEN 'LINESTRING'  THEN
 	geom_env := ST_Envelope(geom_feature);
-    
+
+        simplification_tolerance := 0.0001;
+        distance_buffer := 0.0;
+        IF (ST_NumPoints(geom_feature) > 200) THEN
+           geom_length := ST_Length(geog_feature);
+           IF geom_length > 500000 THEN
+             distance_buffer := 100;
+             simplification_tolerance := 0.002;
+           ELSEIF geom_length > 300000 THEN      
+             distance_buffer := 80;
+             simplification_tolerance := 0.0007;
+           ELSEIF geom_length > 200000 THEN      
+             distance_buffer := 50;
+             simplification_tolerance := 0.0005;
+           ELSEIF geom_length > 100000 THEN      
+             distance_buffer := 30;
+             simplification_tolerance := 0.0002;
+           END IF;
+        END IF;   
+           
 	-- Simplify geometry to reduce number of vertices
-	geom_feature := ST_Transform(ST_SimplifyPreserveTopology(geom_feature, 0.0001), 900913);
+	geom_feature := ST_SimplifyPreserveTopology(geom_feature, 0.0001);
+	geom_feature_simplified := ST_SimplifyPreserveTopology(geom_feature, simplification_tolerance);
+	geom_feature_simplified := ST_Transform(ST_SimplifyPreserveTopology(geom_feature, simplification_tolerance), 900913);
+	geom_feature := ST_Transform(geom_feature, 900913);
 	max_latitude := GREATEST(abs(ST_YMin(geom_env)), abs(ST_YMax(geom_env)));
-	factor := 1/COS(max_latitude*3.1416/180);
+	distance_factor := 1/COS(max_latitude*3.1416/180);
 
 	query := 'SELECT * FROM ' || table_name ||' WHERE (' || condition || ') AND ST_DWithin(geom, $1 , $2)';
 
@@ -57,16 +83,16 @@ BEGIN
         -- We search points in two passes.
         -- First pass looks for points using DWithin and geometries in 900913, which is much faster than geography. In this approach we have to consider scale factor introduced by Mercator distortion.
         -- Second pass checks found points using more accurate DWithin function that operates with geographies.
-
-	FOR rec IN EXECUTE format(query) USING geom_feature, factor*distance LOOP
+        
+	FOR rec IN EXECUTE format(query) USING geom_feature_simplified, (distance_factor*distance + distance_buffer) LOOP
 	    --IF ST_DWithin(geog_feature, rec.location, distance) THEN -- this approach is 2 times slowe than if we compute ST_Distance directly and make a comparison
 	    -- direct calling ST_Distance(rec.location, geog_feature) is about 150% slower.  
 	    rec.distance := round(ST_Distance(rec.location, ST_Transform(ST_ClosestPoint(geom_feature, rec.geom), 4326)::geography)::numeric, 2);
-	    IF (rec.distance < distance) THEN
+	    IF (rec.distance <= distance) THEN
 		RETURN NEXT rec; 
 	    END IF;
         END LOOP;
-        
+
      WHEN 'POLYGON'  THEN
         geom_feature := ST_Transform(geom_feature, 900913);
 
@@ -86,6 +112,7 @@ BEGIN
     RETURN;
 END;
 $$ LANGUAGE plpgsql;
+
 
 
 --*********************************************************************************************************
