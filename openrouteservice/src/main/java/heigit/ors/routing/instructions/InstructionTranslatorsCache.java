d3c198963320d70a89009f721401316c2230c5d2
Maxim Rylov <maksim.rylov@gmail.com>
2017-04-07 11:34:07 +0200

    Added organization attributes

--- openrouteservice/src/main/java/heigit/ors/routing/instructions/InstructionTranslatorsCache.java
+++ openrouteservice/src/main/java/heigit/ors/routing/instructions/InstructionTranslatorsCache.java
@@ -1,3 +1,14 @@
+/*|----------------------------------------------------------------------------------------------
+ *|														Heidelberg University
+ *|	  _____ _____  _____      _                     	Department of Geography		
+ *|	 / ____|_   _|/ ____|    (_)                    	Chair of GIScience
+ *|	| |  __  | | | (___   ___ _  ___ _ __   ___ ___ 	(C) 2014-2016
+ *|	| | |_ | | |  \___ \ / __| |/ _ \ '_ \ / __/ _ \	
+ *|	| |__| |_| |_ ____) | (__| |  __/ | | | (_|  __/	Berliner Strasse 48								
+ *|	 \_____|_____|_____/ \___|_|\___|_| |_|\___\___|	D-69120 Heidelberg, Germany	
+ *|	        	                                       	http://www.giscience.uni-hd.de
+ *|								
+ *|----------------------------------------------------------------------------------------------*/
 package heigit.ors.routing.instructions;
 
 import java.util.HashMap;
@@ -5,7 +16,8 @@ import java.util.Map;
 
 import heigit.ors.localization.LocalizationManager;
 
-public class InstructionTranslatorsCache {
+public class InstructionTranslatorsCache 
+{
 	private Map<Integer, InstructionTranslator> _translators = null;
 	private static volatile InstructionTranslatorsCache m_instance = null;
 
