88a033cdaee13675b9b6ceae07953c86a2d5f365
Maxim Rylov <maksim.rylov@gmail.com>
2017-04-21 11:43:52 +0200

    Added Math.asin approximation function

--- graphhopper/core/src/main/java/com/graphhopper/util/DistanceCalcEarth.java
+++ graphhopper/core/src/main/java/com/graphhopper/util/DistanceCalcEarth.java
@@ -27,224 +27,250 @@ import static java.lang.Math.*;
  */
 public class DistanceCalcEarth implements DistanceCalc
 {
-    /**
-     * mean radius of the earth
-     */
-    public final static double R = 6371000; // m
-    /**
-     * Radius of the earth at equator
-     */
-    public final static double R_EQ = 6378137; // m
-    /**
-     * Circumference of the earth
-     */
-    public final static double C = 2 * PI * R;
-    public final static double KM_MILE = 1.609344;
+	/**
+	 * mean radius of the earth
+	 */
+	public final static double R = 6371000; // m
+	/**
+	 * Radius of the earth at equator
+	 */
+	public final static double R_EQ = 6378137; // m
+	/**
+	 * Circumference of the earth
+	 */
+	public final static double C = 2 * PI * R;
+	public final static double KM_MILE = 1.609344;
 
-    /**
-     * Calculates distance of (from, to) in meter.
-     * <p>
-     * http://en.wikipedia.org/wiki/Haversine_formula a = sin²(Δlat/2) +
-     * cos(lat1).cos(lat2).sin²(Δlong/2) c = 2.atan2(√a, √(1−a)) d = R.c
-     */
-    @Override
-    public double calcDist( double fromLat, double fromLon, double toLat, double toLon )
-    {
-        double sinDeltaLat = sin(toRadians(toLat - fromLat) / 2);
-        double sinDeltaLon = sin(toRadians(toLon - fromLon) / 2);
-        double normedDist = sinDeltaLat * sinDeltaLat
-                + sinDeltaLon * sinDeltaLon * cos(toRadians(fromLat)) * cos(toRadians(toLat));
-        return R * 2 * asin(sqrt(normedDist));
-    }
+	private final static double HALF_PI = Math.PI / 2.0;
+	private final static double R_2 = 2 * R;
+	
+	public static boolean ASIN_APPROXIMATION = false; 
 
-    public double calcDenormalizedDist( double normedDist )
-    {
-        return R * 2 * asin(sqrt(normedDist));
-    }
+	/**
+	 * Calculates distance of (from, to) in meter.
+	 * <p>
+	 * http://en.wikipedia.org/wiki/Haversine_formula a = sin²(Δlat/2) +
+	 * cos(lat1).cos(lat2).sin²(Δlong/2) c = 2.atan2(√a, √(1−a)) d = R.c
+	 */
+	@Override
+	public double calcDist( double fromLat, double fromLon, double toLat, double toLon )
+	{
+		double sinDeltaLat = sin(toRadians(toLat - fromLat) / 2);
+		double sinDeltaLon = sin(toRadians(toLon - fromLon) / 2);
+		double normedDist = sinDeltaLat * sinDeltaLat
+				+ sinDeltaLon * sinDeltaLon * cos(toRadians(fromLat)) * cos(toRadians(toLat));
+		if (ASIN_APPROXIMATION)
+			return R_2 * asin_approx(sqrt(normedDist));
+		else
+			return R_2 * asin(sqrt(normedDist));
+	}
 
-    /**
-     * Returns the specified length in normalized meter.
-     */
-    @Override
-    public double calcNormalizedDist( double dist )
-    {
-        double tmp = sin(dist / 2 / R);
-        return tmp * tmp;
-    }
+	public double calcDenormalizedDist( double normedDist )
+	{
+		if (ASIN_APPROXIMATION)
+			return R * 2 * asin_approx(sqrt(normedDist));
+		else
+			return R * 2 * asin(sqrt(normedDist));
+	}
 
-    @Override
-    public double calcNormalizedDist( double fromLat, double fromLon, double toLat, double toLon )
-    {
-        double sinDeltaLat = sin(toRadians(toLat - fromLat) / 2);
-        double sinDeltaLon = sin(toRadians(toLon - fromLon) / 2);
-        return sinDeltaLat * sinDeltaLat
-                + sinDeltaLon * sinDeltaLon * cos(toRadians(fromLat)) * cos(toRadians(toLat));
-    }
+	private double asin_approx(double x)
+	{
+		boolean negate = x < 0;
+		x = Math.abs(x);
+		double y1 = x * ( -.0170881256 + ( x * ( .0066700901 + ( x * -.0012624911 ) ) ) );
+		double y2 = x * ( -.0501743046 + ( x * ( .0308918810 + y1 ) ) );
+		double y = 1.5707963050 + ( x * ( -.2145988016 + ( x * ( .0889789874 + y2 ) ) ) );
+		y = HALF_PI - ( Math.sqrt( 1.0 - x ) * y );
 
-    /**
-     * Circumference of the earth at different latitudes (breitengrad)
-     */
-    public double calcCircumference( double lat )
-    {
-        return 2 * PI * R * cos(toRadians(lat));
-    }
+		if (negate)
+			y = -y;
 
-    public boolean isDateLineCrossOver( double lon1, double lon2 )
-    {
-        return abs(lon1 - lon2) > 180.0;
-    }
+		return y;
+	}
 
-    @Override
-    public BBox createBBox( double lat, double lon, double radiusInMeter )
-    {
-        if (radiusInMeter <= 0)
-            throw new IllegalArgumentException("Distance must not be zero or negative! " + radiusInMeter + " lat,lon:" + lat + "," + lon);
+	/**
+	 * Returns the specified length in normalized meter.
+	 */
+	@Override
+	public double calcNormalizedDist( double dist )
+	{
+		double tmp = sin(dist / 2 / R);
+		return tmp * tmp;
+	}
 
-        // length of a circle at specified lat / dist
-        double dLon = (360 / (calcCircumference(lat) / radiusInMeter));
+	@Override
+	public double calcNormalizedDist( double fromLat, double fromLon, double toLat, double toLon )
+	{
+		double sinDeltaLat = sin(toRadians(toLat - fromLat) / 2);
+		double sinDeltaLon = sin(toRadians(toLon - fromLon) / 2);
+		return sinDeltaLat * sinDeltaLat
+				+ sinDeltaLon * sinDeltaLon * cos(toRadians(fromLat)) * cos(toRadians(toLat));
+	}
 
-        // length of a circle is independent of the longitude
-        double dLat = (360 / (DistanceCalcEarth.C / radiusInMeter));
+	/**
+	 * Circumference of the earth at different latitudes (breitengrad)
+	 */
+	public double calcCircumference( double lat )
+	{
+		return 2 * PI * R * cos(toRadians(lat));
+	}
 
-        // Now return bounding box in coordinates
-        return new BBox(lon - dLon, lon + dLon, lat - dLat, lat + dLat);
-    }
+	public boolean isDateLineCrossOver( double lon1, double lon2 )
+	{
+		return abs(lon1 - lon2) > 180.0;
+	}
 
-    @Override
-    public double calcNormalizedEdgeDistance( double r_lat_deg, double r_lon_deg,
-                                              double a_lat_deg, double a_lon_deg,
-                                              double b_lat_deg, double b_lon_deg )
-    {
-        return calcNormalizedEdgeDistanceNew(r_lat_deg, r_lon_deg, a_lat_deg, a_lon_deg, b_lat_deg, b_lon_deg, false);
-    }
+	@Override
+	public BBox createBBox( double lat, double lon, double radiusInMeter )
+	{
+		if (radiusInMeter <= 0)
+			throw new IllegalArgumentException("Distance must not be zero or negative! " + radiusInMeter + " lat,lon:" + lat + "," + lon);
 
-    /**
-     * New edge distance calculation where no validEdgeDistance check would be necessary
-     * <p>
-     * @return the normalized distance of the query point "r" to the project point "c" onto the line
-     * segment a-b
-     */
-    public double calcNormalizedEdgeDistanceNew( double r_lat_deg, double r_lon_deg,
-                                                 double a_lat_deg, double a_lon_deg,
-                                                 double b_lat_deg, double b_lon_deg, boolean reduceToSegment )
-    {
-        double shrinkFactor = cos(toRadians((a_lat_deg + b_lat_deg) / 2));
+		// length of a circle at specified lat / dist
+		double dLon = (360 / (calcCircumference(lat) / radiusInMeter));
 
-        double a_lat = a_lat_deg;
-        double a_lon = a_lon_deg * shrinkFactor;
+		// length of a circle is independent of the longitude
+		double dLat = (360 / (DistanceCalcEarth.C / radiusInMeter));
 
-        double b_lat = b_lat_deg;
-        double b_lon = b_lon_deg * shrinkFactor;
+		// Now return bounding box in coordinates
+		return new BBox(lon - dLon, lon + dLon, lat - dLat, lat + dLat);
+	}
 
-        double r_lat = r_lat_deg;
-        double r_lon = r_lon_deg * shrinkFactor;
+	@Override
+	public double calcNormalizedEdgeDistance( double r_lat_deg, double r_lon_deg,
+			double a_lat_deg, double a_lon_deg,
+			double b_lat_deg, double b_lon_deg )
+	{
+		return calcNormalizedEdgeDistanceNew(r_lat_deg, r_lon_deg, a_lat_deg, a_lon_deg, b_lat_deg, b_lon_deg, false);
+	}
 
-        double delta_lon = b_lon - a_lon;
-        double delta_lat = b_lat - a_lat;
+	/**
+	 * New edge distance calculation where no validEdgeDistance check would be necessary
+	 * <p>
+	 * @return the normalized distance of the query point "r" to the project point "c" onto the line
+	 * segment a-b
+	 */
+	public double calcNormalizedEdgeDistanceNew( double r_lat_deg, double r_lon_deg,
+			double a_lat_deg, double a_lon_deg,
+			double b_lat_deg, double b_lon_deg, boolean reduceToSegment )
+	{
+		double shrinkFactor = cos(toRadians((a_lat_deg + b_lat_deg) / 2));
 
-        if (delta_lat == 0)
-            // special case: horizontal edge
-            return calcNormalizedDist(a_lat_deg, r_lon_deg, r_lat_deg, r_lon_deg);
+		double a_lat = a_lat_deg;
+		double a_lon = a_lon_deg * shrinkFactor;
 
-        if (delta_lon == 0)
-            // special case: vertical edge        
-            return calcNormalizedDist(r_lat_deg, a_lon_deg, r_lat_deg, r_lon_deg);
+		double b_lat = b_lat_deg;
+		double b_lon = b_lon_deg * shrinkFactor;
 
-        double norm = delta_lon * delta_lon + delta_lat * delta_lat;
-        double factor = ((r_lon - a_lon) * delta_lon + (r_lat - a_lat) * delta_lat) / norm;
+		double r_lat = r_lat_deg;
+		double r_lon = r_lon_deg * shrinkFactor;
 
-        // make new calculation compatible to old
-        if (reduceToSegment)
-        {
-            if (factor > 1)
-                factor = 1;
-            else if (factor < 0)
-                factor = 0;
-        }
-        // x,y is projection of r onto segment a-b
-        double c_lon = a_lon + factor * delta_lon;
-        double c_lat = a_lat + factor * delta_lat;
-        return calcNormalizedDist(c_lat, c_lon / shrinkFactor, r_lat_deg, r_lon_deg);
-    }
+		double delta_lon = b_lon - a_lon;
+		double delta_lat = b_lat - a_lat;
 
-    @Override
-    public GHPoint calcCrossingPointToEdge( double r_lat_deg, double r_lon_deg,
-                                            double a_lat_deg, double a_lon_deg,
-                                            double b_lat_deg, double b_lon_deg )
-    {
-        double shrinkFactor = cos(toRadians((a_lat_deg + b_lat_deg) / 2));
-        double a_lat = a_lat_deg;
-        double a_lon = a_lon_deg * shrinkFactor;
+		if (delta_lat == 0)
+			// special case: horizontal edge
+			return calcNormalizedDist(a_lat_deg, r_lon_deg, r_lat_deg, r_lon_deg);
 
-        double b_lat = b_lat_deg;
-        double b_lon = b_lon_deg * shrinkFactor;
+		if (delta_lon == 0)
+			// special case: vertical edge        
+			return calcNormalizedDist(r_lat_deg, a_lon_deg, r_lat_deg, r_lon_deg);
 
-        double r_lat = r_lat_deg;
-        double r_lon = r_lon_deg * shrinkFactor;
+		double norm = delta_lon * delta_lon + delta_lat * delta_lat;
+		double factor = ((r_lon - a_lon) * delta_lon + (r_lat - a_lat) * delta_lat) / norm;
 
-        double delta_lon = b_lon - a_lon;
-        double delta_lat = b_lat - a_lat;
+		// make new calculation compatible to old
+		if (reduceToSegment)
+		{
+			if (factor > 1)
+				factor = 1;
+			else if (factor < 0)
+				factor = 0;
+		}
+		// x,y is projection of r onto segment a-b
+		double c_lon = a_lon + factor * delta_lon;
+		double c_lat = a_lat + factor * delta_lat;
+		return calcNormalizedDist(c_lat, c_lon / shrinkFactor, r_lat_deg, r_lon_deg);
+	}
 
-        if (delta_lat == 0)
-            // special case: horizontal edge
-            return new GHPoint(a_lat_deg, r_lon_deg);
+	@Override
+	public GHPoint calcCrossingPointToEdge( double r_lat_deg, double r_lon_deg,
+			double a_lat_deg, double a_lon_deg,
+			double b_lat_deg, double b_lon_deg )
+	{
+		double shrinkFactor = cos(toRadians((a_lat_deg + b_lat_deg) / 2));
+		double a_lat = a_lat_deg;
+		double a_lon = a_lon_deg * shrinkFactor;
 
-        if (delta_lon == 0)
-            // special case: vertical edge        
-            return new GHPoint(r_lat_deg, a_lon_deg);
+		double b_lat = b_lat_deg;
+		double b_lon = b_lon_deg * shrinkFactor;
 
-        double norm = delta_lon * delta_lon + delta_lat * delta_lat;
-        double factor = ((r_lon - a_lon) * delta_lon + (r_lat - a_lat) * delta_lat) / norm;
+		double r_lat = r_lat_deg;
+		double r_lon = r_lon_deg * shrinkFactor;
 
-        if (false)
-        {
-            if (factor > 1)
-                factor = 1;
-            else if (factor < 0)
-                factor = 0;
-        }
+		double delta_lon = b_lon - a_lon;
+		double delta_lat = b_lat - a_lat;
 
-        // x,y is projection of r onto segment a-b
-        double c_lon = a_lon + factor * delta_lon;
-        double c_lat = a_lat + factor * delta_lat;
-        return new GHPoint(c_lat, c_lon / shrinkFactor);
-    }
+		if (delta_lat == 0)
+			// special case: horizontal edge
+			return new GHPoint(a_lat_deg, r_lon_deg);
 
-    @Override
-    public boolean validEdgeDistance( double r_lat_deg, double r_lon_deg,
-                                      double a_lat_deg, double a_lon_deg,
-                                      double b_lat_deg, double b_lon_deg )
-    {
-        double shrinkFactor = cos(toRadians((a_lat_deg + b_lat_deg) / 2));
-        double a_lat = a_lat_deg;
-        double a_lon = a_lon_deg * shrinkFactor;
+		if (delta_lon == 0)
+			// special case: vertical edge        
+			return new GHPoint(r_lat_deg, a_lon_deg);
 
-        double b_lat = b_lat_deg;
-        double b_lon = b_lon_deg * shrinkFactor;
+		double norm = delta_lon * delta_lon + delta_lat * delta_lat;
+		double factor = ((r_lon - a_lon) * delta_lon + (r_lat - a_lat) * delta_lat) / norm;
 
-        double r_lat = r_lat_deg;
-        double r_lon = r_lon_deg * shrinkFactor;
+		if (false)
+		{
+			if (factor > 1)
+				factor = 1;
+			else if (factor < 0)
+				factor = 0;
+		}
 
-        double ar_x = r_lon - a_lon;
-        double ar_y = r_lat - a_lat;
-        double ab_x = b_lon - a_lon;
-        double ab_y = b_lat - a_lat;
-        double ab_ar = ar_x * ab_x + ar_y * ab_y;
+		// x,y is projection of r onto segment a-b
+		double c_lon = a_lon + factor * delta_lon;
+		double c_lat = a_lat + factor * delta_lat;
+		return new GHPoint(c_lat, c_lon / shrinkFactor);
+	}
 
-        double rb_x = b_lon - r_lon;
-        double rb_y = b_lat - r_lat;
-        double ab_rb = rb_x * ab_x + rb_y * ab_y;
+	@Override
+	public boolean validEdgeDistance( double r_lat_deg, double r_lon_deg,
+			double a_lat_deg, double a_lon_deg,
+			double b_lat_deg, double b_lon_deg )
+	{
+		double shrinkFactor = cos(toRadians((a_lat_deg + b_lat_deg) / 2));
+		double a_lat = a_lat_deg;
+		double a_lon = a_lon_deg * shrinkFactor;
 
-        // calculate the exact degree alpha(ar, ab) and beta(rb,ab) if it is case 1 then both angles are <= 90°
-        // double ab_ar_norm = Math.sqrt(ar_x * ar_x + ar_y * ar_y) * Math.sqrt(ab_x * ab_x + ab_y * ab_y);
-        // double ab_rb_norm = Math.sqrt(rb_x * rb_x + rb_y * rb_y) * Math.sqrt(ab_x * ab_x + ab_y * ab_y);
-        // return Math.acos(ab_ar / ab_ar_norm) <= Math.PI / 2 && Math.acos(ab_rb / ab_rb_norm) <= Math.PI / 2;
-        return ab_ar > 0 && ab_rb > 0;
-    }
+		double b_lat = b_lat_deg;
+		double b_lon = b_lon_deg * shrinkFactor;
 
-    @Override
-    public String toString()
-    {
-        return "EXACT";
-    }
+		double r_lat = r_lat_deg;
+		double r_lon = r_lon_deg * shrinkFactor;
+
+		double ar_x = r_lon - a_lon;
+		double ar_y = r_lat - a_lat;
+		double ab_x = b_lon - a_lon;
+		double ab_y = b_lat - a_lat;
+		double ab_ar = ar_x * ab_x + ar_y * ab_y;
+
+		double rb_x = b_lon - r_lon;
+		double rb_y = b_lat - r_lat;
+		double ab_rb = rb_x * ab_x + rb_y * ab_y;
+
+		// calculate the exact degree alpha(ar, ab) and beta(rb,ab) if it is case 1 then both angles are <= 90°
+		// double ab_ar_norm = Math.sqrt(ar_x * ar_x + ar_y * ar_y) * Math.sqrt(ab_x * ab_x + ab_y * ab_y);
+		// double ab_rb_norm = Math.sqrt(rb_x * rb_x + rb_y * rb_y) * Math.sqrt(ab_x * ab_x + ab_y * ab_y);
+		// return Math.acos(ab_ar / ab_ar_norm) <= Math.PI / 2 && Math.acos(ab_rb / ab_rb_norm) <= Math.PI / 2;
+		return ab_ar > 0 && ab_rb > 0;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "EXACT";
+	}
 }
