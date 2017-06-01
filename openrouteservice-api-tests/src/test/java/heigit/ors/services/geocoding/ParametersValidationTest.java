525ef354ec48159ea8293d55fb917beeb11ddf92
Maxim Rylov <maksim.rylov@gmail.com>
2017-06-01 11:26:05 +0200

    Added API tests for Geocoding service

---
+++ openrouteservice-api-tests/src/test/java/heigit/ors/services/geocoding/ParametersValidationTest.java
@@ -0,0 +1,142 @@
+package heigit.ors.services.geocoding;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.Test;
+
+import heigit.ors.services.common.EndPointAnnotation;
+import heigit.ors.services.common.ServiceTest;
+
+@EndPointAnnotation(name="geocode")
+public class ParametersValidationTest extends ServiceTest {
+
+	public ParametersValidationTest() {
+	}
+
+	@Test
+	public void mandatoryParametersTest() {
+		given()
+		.param("query2", "Heidelberg")
+		.when()
+		.get(getEndPointName())
+		.then()
+		.assertThat()
+		.body("error.code", is(101))
+		.statusCode(400);
+	}
+	
+	@Test
+	public void locationWrongValueTest() {
+		given()
+		.param("location", "8.67678")
+		.when()
+		.get(getEndPointName())
+		.then()
+		.assertThat()
+		.body("error.code", is(102))
+		.statusCode(400);
+		
+		given()
+		.param("location", "8.67678k,49.662")
+		.when()
+		.get(getEndPointName())
+		.then()
+		.assertThat()
+		.body("error.code", is(103))
+		.statusCode(400);
+	}
+
+	@Test
+	public void limitWrongValueTest() {
+		given()
+		.param("query", "Heidelberg")
+		.param("limit", "10b")
+		.when()
+		.get(getEndPointName())
+		.then()
+		.assertThat()
+		.body("error.code", is(103))
+		.statusCode(400);
+	}
+	
+	@Test
+	public void boundaryTypeUnknownValuesTest() {
+		given()
+		.param("query", "Heidelberg")
+		.param("limit", "10")
+		.param("boundary_type", "rectt")
+		.when()
+		.get(getEndPointName())
+		.then()
+		.assertThat()
+		.body("error.code", is(103))
+		.statusCode(400);
+		
+		given()
+		.param("query", "Heidelberg")
+		.param("limit", "10")
+		.param("boundary_type", "rect")
+		.param("rectg", "4,5,4.5,6")
+		.when()
+		.get(getEndPointName())
+		.then()
+		.assertThat()
+		.body("error.code", is(101))
+		.statusCode(400);
+		
+		given()
+		.param("query", "Heidelberg")
+		.param("limit", "10")
+		.param("boundary_type", "circle")
+		.param("circlt", "4,5,4.5")
+		.when()
+		.get(getEndPointName())
+		.then()
+		.assertThat()
+		.body("error.code", is(101))
+		.statusCode(400);
+	}
+	
+	@Test
+	public void boundaryTypeWrongRectValueTest() {
+		given()
+		.param("query", "Heidelberg")
+		.param("limit", "10")
+		.param("boundary_type", "rect")
+		.param("rect", "8.45,12.5,8.47,12.6hhhh")
+		.when()
+		.get(getEndPointName())
+		.then()
+		.assertThat()
+		.body("error.code", is(102))
+		.statusCode(400);
+	}
+	
+	@Test
+	public void boundaryTypeWrongCircleValueTest() {
+		given()
+		.param("query", "Heidelberg")
+		.param("limit", "10")
+		.param("boundary_type", "circle")
+		.param("circle", "8.45,12.5,8.47,12.6")
+		.when()
+		.get(getEndPointName())
+		.then()
+		.assertThat()
+		.body("error.code", is(103))
+		.statusCode(400);
+		
+		given()
+		.param("query", "Heidelberg")
+		.param("limit", "10")
+		.param("boundary_type", "circle")
+		.param("circle", "b8.45,12.5,8.47")
+		.when()
+		.get(getEndPointName())
+		.then()
+		.assertThat()
+		.body("error.code", is(102))
+		.statusCode(400);
+	}
+}
