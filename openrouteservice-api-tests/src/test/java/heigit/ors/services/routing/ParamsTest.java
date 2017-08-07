782b05d8ed67d29c4519a9c048dd09ab9d7d27a4
Maxim Rylov <maksim.rylov@gmail.com>
2017-08-07 12:19:40 +0200

    Empty commit

--- openrouteservice-api-tests/src/test/java/heigit/ors/services/routing/ParamsTest.java
+++ openrouteservice-api-tests/src/test/java/heigit/ors/services/routing/ParamsTest.java
@@ -13,7 +13,6 @@ import heigit.ors.services.common.ServiceTest;
 public class ParamsTest extends ServiceTest {
 
 	public ParamsTest() {
-
 		addParameter("coordinatesShort", "8.680916,49.410973|8.687782,49.424597");
 		addParameter("coordinatesShortFaulty", "8.680916a,49.41b0973|8.6c87782,049gbd.424597");
 		addParameter("coordinatesLong", "8.502045,49.47794|4.78906,53.071752");
