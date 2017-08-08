ab2c9986d6a0ee3a2e1a4d3dd5ae959be0c18b6d
Maxim Rylov <maksim.rylov@gmail.com>
2017-08-08 08:44:01 +0200

    Update app.config.test

--- openrouteservice/src/main/java/heigit/ors/io/ByteArrayOutputStreamEx.java
+++ openrouteservice/src/main/java/heigit/ors/io/ByteArrayOutputStreamEx.java
@@ -2,12 +2,20 @@ package heigit.ors.io;
 
 import java.io.ByteArrayOutputStream;
 
-public class ByteArrayOutputStreamEx extends ByteArrayOutputStream  {
-	 public ByteArrayOutputStreamEx() { super(); }
-	    public ByteArrayOutputStreamEx(int size) { super(size); }
+public class ByteArrayOutputStreamEx extends ByteArrayOutputStream  
+{
+	public ByteArrayOutputStreamEx() 
+	{
+		super(); 
+	}
 
-	    /** Returns the internal buffer of this ByteArrayOutputStream, without copying. */
-	    public synchronized byte[] getBuffer() {
-	        return this.buf;
-	    }
+	public ByteArrayOutputStreamEx(int size) 
+	{ 
+		super(size); 
+	}
+
+	/** Returns the internal buffer of this ByteArrayOutputStream, without copying. */
+	public synchronized byte[] getBuffer() {
+		return this.buf;
+	}
 }
