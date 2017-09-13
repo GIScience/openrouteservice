2ad350b96c13038ddfd7177cd6a5ed966ef421cd
Lu Liu <nudtlliu@gmail.com>
2017-09-13 14:44:48 +0200

    remove the graphhopper subdir because it is a standalone project now

--- graphhopper/core/src/main/java/com/graphhopper/routing/util/EdgeAnnotator.java
+++
@@ -1,5 +0,0 @@
-package com.graphhopper.routing.util;
-
-public interface EdgeAnnotator {
-	public String getAnnotation(int edgeId);
-}
--- graphhopper/core/src/main/java/com/graphhopper/storage/GraphStorageFactory.java
+++
@@ -1,8 +0,0 @@
-package com.graphhopper.storage;
-
-import com.graphhopper.GraphHopper;
-
-public interface GraphStorageFactory {
-
-	public GraphHopperStorage createStorage(GHDirectory dir, GraphHopper gh); 
-}
