41601b18845b924fc1e3a119108807a8b7a68c65
Maxim Rylov <maksim.rylov@gmail.com>
2017-09-14 10:51:58 +0200

    Updating source file headers.

--- openrouteservice/src/main/java/heigit/ors/routing/graphhopper/extensions/edgefilters/AvoidSteepnessEdgeFilter.java
+++ openrouteservice/src/main/java/heigit/ors/routing/graphhopper/extensions/edgefilters/AvoidSteepnessEdgeFilter.java
@@ -1,17 +1,23 @@
-/*|----------------------------------------------------------------------------------------------
- *|														Heidelberg University
- *|	  _____ _____  _____      _                     	Department of Geography		
- *|	 / ____|_   _|/ ____|    (_)                    	Chair of GIScience
- *|	| |  __  | | | (___   ___ _  ___ _ __   ___ ___ 	(C) 2014
- *|	| | |_ | | |  \___ \ / __| |/ _ \ '_ \ / __/ _ \	
- *|	| |__| |_| |_ ____) | (__| |  __/ | | | (_|  __/	Berliner Strasse 48								
- *|	 \_____|_____|_____/ \___|_|\___|_| |_|\___\___|	D-69120 Heidelberg, Germany	
- *|	        	                                       	http://www.giscience.uni-hd.de
- *|								
- *|----------------------------------------------------------------------------------------------*/
-
-// Authors: M. Rylov 
-
+/*
+ *  Licensed to GIScience Research Group, Heidelberg University (GIScience)
+ *
+ *   http://www.giscience.uni-hd.de
+ *   http://www.heigit.org
+ *
+ *  under one or more contributor license agreements. See the NOTICE file 
+ *  distributed with this work for additional information regarding copyright 
+ *  ownership. The GIScience licenses this file to you under the Apache License, 
+ *  Version 2.0 (the "License"); you may not use this file except in compliance 
+ *  with the License. You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package heigit.ors.routing.graphhopper.extensions.edgefilters;
 
 import heigit.ors.routing.graphhopper.extensions.storages.*;
