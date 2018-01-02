43492ba18bd219e1fdc05faf27fd242a506474a7
Adam Rousell <adamrousell@hotmail.com>
2018-01-02 15:50:59 +0100

    Update PersonType.java

--- openrouteservice/src/main/java/heigit/ors/util/gpxUtil/PersonType.java
+++ openrouteservice/src/main/java/heigit/ors/util/gpxUtil/PersonType.java
@@ -16,6 +16,7 @@ import javax.xml.bind.annotation.XmlType;
  * <p>The following schema fragment specifies the expected content contained within this class.
  * 
  * <pre>
+ * {@code
  * <complexType name="personType">
  *   <complexContent>
  *     <restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
@@ -27,6 +28,7 @@ import javax.xml.bind.annotation.XmlType;
  *     </restriction>
  *   </complexContent>
  * </complexType>
+ * }
  * </pre>
  * 
  * 
