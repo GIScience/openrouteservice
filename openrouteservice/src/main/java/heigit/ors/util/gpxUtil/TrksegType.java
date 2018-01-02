59baca28f5686326c17fa48a12db9dec4c88aea6
Adam Rousell <adamrousell@hotmail.com>
2018-01-02 15:55:04 +0100

    Update TrksegType.java

--- openrouteservice/src/main/java/heigit/ors/util/gpxUtil/TrksegType.java
+++ openrouteservice/src/main/java/heigit/ors/util/gpxUtil/TrksegType.java
@@ -18,6 +18,7 @@ import java.util.List;
  * <p>The following schema fragment specifies the expected content contained within this class.
  * 
  * <pre>
+ * {@code
  * <complexType name="trksegType">
  *   <complexContent>
  *     <restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
@@ -28,6 +29,7 @@ import java.util.List;
  *     </restriction>
  *   </complexContent>
  * </complexType>
+ * }
  * </pre>
  * 
  * 
