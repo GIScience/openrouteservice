d33b7c90c699e349b96ec86070d63b679d127c8c
Hendrik Leuschner <hendrik@openrouteservice.org>
2018-02-01 17:38:05 +0100

    Modify tollway to better represent reality of OSM tags

--- openrouteservice/src/main/java/heigit/ors/routing/pathprocessors/TollwayExtractor.java
+++ openrouteservice/src/main/java/heigit/ors/routing/pathprocessors/TollwayExtractor.java
@@ -26,55 +26,57 @@ import heigit.ors.routing.graphhopper.extensions.storages.TollwaysGraphStorage;
 import heigit.ors.routing.parameters.ProfileParameters;
 import heigit.ors.routing.parameters.VehicleParameters;
 
-public class TollwayExtractor 
-{
+public class TollwayExtractor {
 	private VehicleParameters _vehicleParams;
 	private int _vehicleType;
 	private TollwaysGraphStorage _storage;
 	private byte[] _buffer = new byte[4];
 
-	public TollwayExtractor(TollwaysGraphStorage storage, int vehileType, ProfileParameters vehicleParams)
-	{
+	public TollwayExtractor(TollwaysGraphStorage storage, int vehicleType, ProfileParameters vehicleParams) {
 		_storage = storage;
-
+		_vehicleType = vehicleType;
 		if (vehicleParams instanceof VehicleParameters)
-			_vehicleParams = (VehicleParameters)vehicleParams;
-	}   
-
-	public int getValue(int edgeId)
-	{
+			_vehicleParams = (VehicleParameters) vehicleParams;
+	}
+	/**
+	 * return if a way is a tollway for the configured vehicle. If _vehicleType != 0, it is a heavy vehicle.
+	 * If it is a heavy vehicle and weight parameter is provided, return the toll attribute based on the weight
+	 *
+	 * @param edgeId				The edgeId for which toll should be checked
+	 * @see HeavyVehicleAttributes
+	 */
+	public int getValue(int edgeId) {
 		int value = _storage.getEdgeValue(edgeId, _buffer);
 
-		if (value != TollwayType.None)
-		{
-			// normal car
-			if (_vehicleParams == null)
-			{
-				if (TollwayType.isLType(value) || TollwayType.isMType(value))
-					return 1;
-				else
-					return 0;
-			}
+		if (value != TollwayType.None) {
 
+			// Check if "toll=yes" is present. If no and you're a car, you're good to go
+			if (TollwayType.isLType(value) || TollwayType.isMType(value))
+				return 1;
+			if (_vehicleType == 0)
+				return 0;
+
+			//Check if tag "toll:hgv" is present -> assume toll for any hgv type
+			if (TollwayType.isNType(value) && value == TollwayType.N)
+				return 1;
+
+			//Check if weight specific toll tags are present even though the weight is unset
 			double weight = _vehicleParams.getWeight();
-
+			if (weight == 0 && TollwayType.isNType(value))
+					return 1;
+			//Check in which weight range the hgv falls and return accordingly
 			//toll:N1=yes - Für Fahrzeuge bis 3,5 Tonnen eingesetzt, Bsp. Pick-up-Truck. (siehe Europäische Fahrzeugklassifikation N1)
 			//toll:N2=yes - Für Fahrzeuge von 3,5 Tonnen bis zu 12 Tonnen eingesetzt, Bsp. Commercial Truck.
 			//toll:N3=yes Für LKW mit einem zulässigen Gesamtgewicht > 12 t
-
-			if (TollwayType.isNType(value))
-			{
+			else {
 				if (weight < 3.5 && value == TollwayType.N1)
 					return 1;
 				else if (weight >= 3.5 && weight < 12 && value == TollwayType.N2)
 					return 1;
 				else if (weight >= 12 && value == TollwayType.N3)
 					return 1;
-				else if (_vehicleType == HeavyVehicleAttributes.GOODS && value == TollwayType.N)
-					return 1;
 			}
 		}
-
 		return 0;
 	}
 }
