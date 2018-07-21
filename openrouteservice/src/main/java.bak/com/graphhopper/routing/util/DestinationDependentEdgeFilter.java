d1e09e387b0c5741ecd074cb98690b274161073e
marq24 <marquardt24@gmail.com>
2018-07-21 11:05:48 +0200

    removed old gh backup classes -> all this is on the ors-gh branch now

--- openrouteservice/src/main/java.bak/com/graphhopper/routing/util/DestinationDependentEdgeFilter.java
+++
@@ -1,9 +0,0 @@
-package com.graphhopper.routing.util;
-
-import com.graphhopper.storage.Graph;
-import com.graphhopper.util.EdgeIteratorState;
-
-public interface DestinationDependentEdgeFilter extends EdgeFilter
-{
-    void setDestinationEdge(EdgeIteratorState edge, Graph graph, FlagEncoder encoder, TraversalMode tMode);
-}
