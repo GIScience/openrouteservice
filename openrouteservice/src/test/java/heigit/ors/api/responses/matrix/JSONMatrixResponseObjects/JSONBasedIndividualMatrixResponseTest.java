00a11789fe102499458df847a1577845fd3ee5fc
Julian Psotta <psotta@stud.uni-heidelberg.de>
2018-11-10 18:37:43 +0100

    bare files for last tests

---
+++ openrouteservice/src/test/java/heigit/ors/api/responses/matrix/JSONMatrixResponseObjects/JSONBasedIndividualMatrixResponseTest.java
@@ -0,0 +1,19 @@
+package heigit.ors.api.responses.matrix.JSONMatrixResponseObjects;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class JSONBasedIndividualMatrixResponseTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void constructDestinations() {
+    }
+
+    @Test
+    public void constructSources() {
+    }
+}
---
+++ openrouteservice/src/test/java/heigit/ors/api/responses/matrix/JSONMatrixResponseObjects/JSONLocationTest.java
@@ -0,0 +1,19 @@
+package heigit.ors.api.responses.matrix.JSONMatrixResponseObjects;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class JSONLocationTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void getSnapped_distance() {
+    }
+
+    @Test
+    public void getLocation() {
+    }
+}
