409eb660418ed4c9eef0d479268075e25018b374
Julian Psotta <psotta@stud.uni-heidelberg.de>
2018-11-10 18:37:43 +0100

    bare files for last tests

---
+++ openrouteservice/src/test/java/heigit/ors/api/responses/matrix/JSONMatrixResponseObjects/JSON2DSourcesTest.java
@@ -0,0 +1,10 @@
+package heigit.ors.api.responses.matrix.JSONMatrixResponseObjects;
+
+import org.junit.Test;
+
+public class JSON2DSourcesTest {
+
+    @Test
+    public void getLocation() {
+    }
+}
