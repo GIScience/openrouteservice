dfe0990d68ee8cbcb6906c4ef998e16e9c50de55
aoles <andrzej.oles@gmail.com>
2018-11-12 15:36:57 +0100

    Remove paved/unpaved from avoid features

--- openrouteservice/src/main/java/heigit/ors/routing/AvoidFeatureFlags.java
+++ openrouteservice/src/main/java/heigit/ors/routing/AvoidFeatureFlags.java
@@ -18,18 +18,16 @@ public class AvoidFeatureFlags {
 	public static final int Tollways = 2; // 1 << 1;
 	public static final int Steps = 2; // 1 << 1;
 	public static final int Ferries = 4; // 1 << 2;
-	public static final int UnpavedRoads = 8; // 1 << 3;
 	public static final int Tracks = 16; // 1 << 4;
 	public static final int Tunnels = 32; // 1 << 5;
-	public static final int PavedRoads = 64; // 1 << 6;
 	public static final int Fords = 128; // 1 << 7;
 	
 	public static final int Bridges = 256; // does not work as it is greater than byte limit of 255.
 	public static final int Borders = 512; 
 	public static final int Hills = 1024;
 
-	public static final int DrivingFeatures = Highways | Tollways | Ferries | UnpavedRoads | Tracks | Tunnels | Fords | Bridges | Borders;
-	public static final int CyclingFeatures = Steps | Ferries | UnpavedRoads | PavedRoads | Fords;
+	public static final int DrivingFeatures = Highways | Tollways | Ferries | Tracks | Tunnels | Fords | Bridges | Borders;
+	public static final int CyclingFeatures = Steps | Ferries | Fords;
 	public static final int WalkingFeatures =  Steps | Ferries | Fords;
 	public static final int WheelchairFeatures = Ferries;
 
@@ -43,16 +41,12 @@ public class AvoidFeatureFlags {
 				return Tollways;
 			case "ferries":
 				return Ferries;
-			case "unpavedroads":
-				return UnpavedRoads;
 			case "steps":
 				return Steps;
 			case "tracks":
 				return Tracks;
 			case "tunnels":
 				return Tunnels;
-			case "pavedroads":
-				return PavedRoads;
 			case "fords":
 				return Fords;
 			case "bridges":
