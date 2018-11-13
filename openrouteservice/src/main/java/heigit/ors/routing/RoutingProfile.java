589341a59ad48cb83ff01be5b5e95a246ea45517
Adam Rousell <adam@openrouteservice.org>
2018-11-13 12:03:20 +0100

    Set flexible mode to be true when using the wheelchair profile

--- openrouteservice/src/main/java/heigit/ors/routing/RoutingProfile.java
+++ openrouteservice/src/main/java/heigit/ors/routing/RoutingProfile.java
@@ -882,6 +882,10 @@ public class RoutingProfile {
                 }
             }
 
+            if(profileType == RoutingProfileType.WHEELCHAIR) {
+                flexibleMode = true;
+            }
+
             if (RoutingProfileType.isDriving(profileType) && RealTrafficDataProvider.getInstance().isInitialized())
                 req.setEdgeAnnotator(new TrafficEdgeAnnotator(mGraphHopper.getGraphHopperStorage()));
 
