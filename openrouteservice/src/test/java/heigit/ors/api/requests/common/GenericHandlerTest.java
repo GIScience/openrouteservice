c8c38bc6c4adcca8bdcc1ed4d8687f8f1c595521
Julian Psotta <psotta@stud.uni-heidelberg.de>
2018-12-02 14:39:03 +0100

    added test bodies for GenericHandlerTest.java

---
+++ openrouteservice/src/test/java/heigit/ors/api/requests/common/GenericHandlerTest.java
@@ -0,0 +1,53 @@
+package heigit.ors.api.requests.common;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GenericHandlerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void convertAPIEnumListToStrings() {
+    }
+
+    @Test
+    public void convertAPIEnum() {
+    }
+
+    @Test
+    public void convertVehicleType() {
+    }
+
+    @Test
+    public void convertAvoidBorders() {
+    }
+
+    @Test
+    public void convertRouteProfileType() {
+    }
+
+    @Test
+    public void convertAvoidAreas() {
+    }
+
+    @Test
+    public void convertFeatureTypes() {
+    }
+
+    @Test
+    public void convertParameters() {
+    }
+
+    @Test
+    public void convertParameters1() {
+    }
+
+    @Test
+    public void convertSpecificProfileParameters() {
+    }
+}
