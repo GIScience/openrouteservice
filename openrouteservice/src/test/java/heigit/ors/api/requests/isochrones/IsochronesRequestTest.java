e70610eb2a3dca1a9765c50661831046d0f3266a
Julian Psotta <psotta@stud.uni-heidelberg.de>
2019-01-10 15:18:56 +0100

    generated missing tests + restructure

--- openrouteservice/src/test/java/heigit/ors/api/requests/isochrones/IsochronesRequestTest.java
+++ openrouteservice/src/test/java/heigit/ors/api/requests/isochrones/IsochronesRequestTest.java
@@ -9,14 +9,17 @@ import org.junit.Test;
 import static heigit.ors.util.HelperFunctions.fakeArrayLocations;
 
 public class IsochronesRequestTest {
+    IsochronesRequest request;
 
     @Before
     public void setUp() {
+        request = new IsochronesRequest();
+
+
     }
 
     @Test
     public void idTest() {
-        IsochronesRequest request = new IsochronesRequest();
         Assert.assertFalse(request.hasId());
         request.setId("1");
         Assert.assertTrue(request.hasId());
@@ -25,7 +28,6 @@ public class IsochronesRequestTest {
 
     @Test
     public void smoothingTest() {
-        IsochronesRequest request = new IsochronesRequest();
         Assert.assertFalse(request.hasSmoothing());
         request.setSmoothing(1.5);
         Assert.assertTrue(request.hasSmoothing());
@@ -34,13 +36,149 @@ public class IsochronesRequestTest {
 
     @Test
     public void attributesTest() {
-        IsochronesRequest request = new IsochronesRequest();
         Assert.assertFalse(request.hasAttributes());
         request.setAttributes(new IsochronesRequestEnums.Attributes[]{IsochronesRequestEnums.Attributes.AREA});
         Assert.assertTrue(request.hasAttributes());
         Assert.assertEquals(request.getAttributes().length, 1);
         Assert.assertEquals(IsochronesRequestEnums.Attributes.AREA, request.getAttributes()[0]);
     }
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void setId() {
+    }
+
+    @Test
+    public void hasId() {
+    }
+
+    @Test
+    public void getAreaUnit() {
+    }
+
+    @Test
+    public void setAreaUnit() {
+    }
+
+    @Test
+    public void getSmoothing() {
+    }
+
+    @Test
+    public void setSmoothing() {
+    }
+
+    @Test
+    public void hasSmoothing() {
+    }
+
+    @Test
+    public void getResponseType() {
+    }
+
+    @Test
+    public void setResponseType() {
+    }
+
+    @Test
+    public void getIntersections() {
+    }
+
+    @Test
+    public void setIntersection() {
+    }
+
+    @Test
+    public void getRangeUnits() {
+    }
+
+    @Test
+    public void setRangeUnits() {
+    }
+
+    @Test
+    public void getAttributes() {
+    }
+
+    @Test
+    public void setAttributes() {
+    }
+
+    @Test
+    public void hasAttributes() {
+    }
+
+    @Test
+    public void getCalcMethod() {
+    }
+
+    @Test
+    public void setCalcMethod() {
+    }
+
+    @Test
+    public void getLocation() {
+    }
+
+    @Test
+    public void setLocation() {
+    }
+
+    @Test
+    public void getLocationType() {
+    }
+
+    @Test
+    public void setLocationType() {
+    }
+
+    @Test
+    public void getProfile() {
+    }
+
+    @Test
+    public void setProfile() {
+    }
+
+    @Test
+    public void getIsochronesOptions() {
+    }
+
+    @Test
+    public void setIsochronesOptions() {
+    }
+
+    @Test
+    public void hasIsochronesOptions() {
+    }
+
+    @Test
+    public void getRange() {
+    }
+
+    @Test
+    public void setRange() {
+    }
+
+    @Test
+    public void getRangeType() {
+    }
+
+    @Test
+    public void setRangeType() {
+    }
+
+    @Test
+    public void getInterval() {
+    }
+
+    @Test
+    public void setInterval() {
+    }
+
 
     @Test(expected = ParameterValueException.class)
     public void tooSmallLocationTest() throws ParameterValueException {
