eb46b4f2e4b95d4d4d6d77c032c2b8521118730d
Benjamin Bergstein <bennyjbergstein+github@gmail.com>
2019-10-31 21:08:12 +0000

    wip

--- docker/foo.sh
+++ docker/foo.sh
@@ -1,5 +1,9 @@
 ll1="-122.438811,37.773033"
 ll2="-122.447608,37.770828"
+url="http://localhost:8080/ors/health"
+curl $url
+exit
+
 url="http://localhost:8080/ors/v2/directions/driving-car?start=$ll1&end=$ll2"
 
 curl --include \
