1e9ff892181d5affee62bbd347220790985ddb82
Johannes Visintini <johannes.visintini@heigit.org>
2020-08-06 12:49:13 +0200

    Implement EdgeFilter independent from AvoidAreas

--- openrouteservice/src/main/java/org/heigit/ors/weightaugmentation/PolygonEdgeFilter.java
+++ openrouteservice/src/main/java/org/heigit/ors/weightaugmentation/PolygonEdgeFilter.java
@@ -2,19 +2,43 @@ package org.heigit.ors.weightaugmentation;
 
 import com.graphhopper.routing.util.EdgeFilter;
 import com.graphhopper.util.EdgeIteratorState;
+import com.graphhopper.util.PointList;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineString;
 import com.vividsolutions.jts.geom.Polygon;
-import org.heigit.ors.routing.graphhopper.extensions.edgefilters.AvoidAreasEdgeFilter;
 
-// TODO this is a hack to test the Tests. Need to be implemented!
 public class PolygonEdgeFilter implements EdgeFilter {
-  final EdgeFilter superEdgeFilter;
+  final Polygon[] polygons;
 
   public PolygonEdgeFilter(Polygon[] polygons) {
-    superEdgeFilter = new AvoidAreasEdgeFilter(polygons);
+    this.polygons = polygons;
   }
 
   @Override
   public boolean accept(EdgeIteratorState edgeIteratorState) {
-    return !superEdgeFilter.accept(edgeIteratorState);
+    LineString edgeGeometry = toLineString(edgeIteratorState.fetchWayGeometry(3));
+    for (Polygon polygon: polygons) {
+      if (polygon.contains(edgeGeometry) || polygon.crosses(edgeGeometry)) {
+        return true;
+      }
+    }
+    return false;
   }
+
+  private static LineString toLineString(PointList pointList) {
+    GeometryFactory gf = new GeometryFactory();
+    Coordinate[] coordinates = new Coordinate[pointList.getSize() == 1 ? 2 : pointList.getSize()];
+
+    for(int i = 0; i < pointList.getSize(); ++i) {
+      coordinates[i] = new Coordinate(PointList.round6(pointList.getLongitude(i)), PointList.round6(pointList.getLatitude(i)));
+    }
+
+    if (pointList.getSize() == 1) {
+      coordinates[1] = coordinates[0];
+    }
+
+    return gf.createLineString(coordinates);
+  }
+
 }
