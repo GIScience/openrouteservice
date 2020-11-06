90cb7c1a26a45ce0b6f8dfdbc47866088c8e9341
Hendrik Leuschner <hendrik@openrouteservice.org>
2020-11-06 15:19:53 +0100

    Enable TrafficSpeedCalculator

--- openrouteservice/src/main/java/org/heigit/ors/routing/traffic/TrafficSpeedCalculator.java
+++ openrouteservice/src/main/java/org/heigit/ors/routing/traffic/TrafficSpeedCalculator.java
@@ -7,30 +7,32 @@ import com.graphhopper.routing.util.FlagEncoder;
 import com.graphhopper.routing.util.SpeedCalculator;
 import com.graphhopper.storage.GraphHopperStorage;
 import com.graphhopper.util.EdgeIteratorState;
+import org.heigit.ors.routing.graphhopper.extensions.storages.GraphStorageUtils;
+import org.heigit.ors.routing.graphhopper.extensions.storages.TrafficGraphStorage;
 
 public class TrafficSpeedCalculator implements SpeedCalculator {
     protected final DecimalEncodedValue avSpeedEnc;
     // time-dependent stuff
-//    private final TrafficGraphStorage trafficGraphStorage;
+    private final TrafficGraphStorage trafficGraphStorage;
 
     public TrafficSpeedCalculator(GraphHopperStorage graph, FlagEncoder encoder) {
         avSpeedEnc = encoder.getAverageSpeedEnc();
 
         // time-dependent stuff
         EncodingManager encodingManager = graph.getEncodingManager();
-//        trafficGraphStorage = GraphStorageUtils.getGraphExtension(graph, TrafficGraphStorage.class);
+        trafficGraphStorage = GraphStorageUtils.getGraphExtension(graph, TrafficGraphStorage.class);
     }
 
     public double getSpeed(EdgeIteratorState edge, boolean reverse, long time) {
         double speed = reverse ? edge.getReverse(avSpeedEnc) : edge.get(avSpeedEnc);
 
 //         retrieve time-dependent maxspeed here
-//        if (time != -1) {
-//            int edgeId = EdgeKeys.getOriginalEdge(edge);
-//            double trafficSpeed = trafficGraphStorage.getEdgeValue(edgeId, edge.getBaseNode(), time);
-//            if (trafficSpeed < speed)
-//                speed = trafficSpeed;
-//        }
+        if (time != -1) {
+            int edgeId = EdgeKeys.getOriginalEdge(edge);
+            double trafficSpeed = trafficGraphStorage.getSpeedValue(edgeId, edge.getBaseNode(), edge.getAdjNode(), time);
+            if (trafficSpeed < speed)
+                speed = trafficSpeed;
+        }
         return speed;
     }
 }
