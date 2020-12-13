88d3407e33f18cf358511cba2a7e8bb7029675ed
Julian Psotta <julian@openrouteservice.org>
2020-12-13 22:04:40 +0100

    reduce amount of necessary geometry calls
    Signed-off-by: Julian Psotta <julian@openrouteservice.org>

--- openrouteservice/src/main/java/org/heigit/ors/routing/graphhopper/extensions/reader/traffic/TrafficLink.java
+++ openrouteservice/src/main/java/org/heigit/ors/routing/graphhopper/extensions/reader/traffic/TrafficLink.java
@@ -1,6 +1,7 @@
 package org.heigit.ors.routing.graphhopper.extensions.reader.traffic;
 
 import com.graphhopper.util.DistanceCalc;
+import com.graphhopper.util.DistanceCalcEarth;
 import org.apache.log4j.Logger;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.Geometry;
@@ -15,12 +16,13 @@ import java.util.Map;
 
 public class TrafficLink {
     private static final Logger LOGGER = Logger.getLogger(TrafficLink.class);
+    private double coordinateFirstY;
+    private double coordinateFirstX;
+    private double coordinateLastY;
+    private double coordinateLastX;
 
     private int linkId;
-    private double minLat = 180f;
-    private double minLon = 180f;
-    private double maxLat = -180f;
-    private double maxLon = -180f;
+    private double linkLength;
 
     private LineString linkGeometry;
     private TrafficLinkMetadata trafficLinkMetadata;
@@ -33,47 +35,69 @@ public class TrafficLink {
      * @param linkId       Link ID of the link
      * @param linkGeometry Geometry representing the link
      * @param properties   Properties of the link
+     * @param distanceCalcEarth   Initialized {@link DistanceCalcEarth} object that can easily be reused to calculate the lengths.
      * @throws InvalidObjectException Provides detailed information when a geometry was invalid.
      */
-    public TrafficLink(int linkId, Geometry linkGeometry, Collection<Property> properties) throws InvalidObjectException {
+    public TrafficLink(int linkId, Geometry linkGeometry, Collection<Property> properties, DistanceCalcEarth distanceCalcEarth) throws InvalidObjectException {
         this.linkId = linkId;
-        GeometryFactory gf = new GeometryFactory();
 
         this.trafficPatternIdsFrom = new EnumMap<>(TrafficEnums.WeekDay.class);
         this.trafficPatternIdsTo = new EnumMap<>(TrafficEnums.WeekDay.class);
 
-        if (linkGeometry.getGeometryType().equals("LineString"))
-            this.linkGeometry = gf.createLineString(linkGeometry.getCoordinates());
-        else {
-            LOGGER.error("Invalid geometry - " + linkGeometry.getGeometryType());
-            throw new InvalidObjectException("Invalid geometry for linkId " + linkId);
-        }
+        this.setLinkGeometry(linkGeometry);
+        this.setLinkLength(linkGeometry, distanceCalcEarth);
 
-        Geometry bbox = linkGeometry.getEnvelope();
-        for (Coordinate c : bbox.getCoordinates()) {
-            if (c.x < minLon) minLon = c.x;
-            if (c.x > maxLon) maxLon = c.x;
-            if (c.y < minLat) minLat = c.y;
-            if (c.y > maxLat) maxLat = c.y;
-        }
         trafficLinkMetadata = new TrafficLinkMetadata(properties);
 
     }
 
-    public double[] getBBox() {
-        return new double[]{minLon, maxLon, minLat, maxLat};
+    private void setLinkLength(Geometry linkGeometry, DistanceCalcEarth dc) {
+        double temporaryLength = 0;
+
+        if (this.getLinkGeometry() != null) {
+            LineString ls = this.getLinkGeometry();
+            int nPoints = ls.getNumPoints();
+
+            if (nPoints > 1) {
+                Coordinate c = ls.getCoordinateN(0);
+                double x0 = c.x;
+                double y0 = c.y;
+                for (int i = 1; i < ls.getNumPoints(); i++) {
+                    c = ls.getCoordinateN(i);
+
+                    temporaryLength += dc.calcDist(y0, x0, c.y, c.x);
+                    x0 = c.x;
+                    y0 = c.y;
+                }
+            }
+        }
+        this.linkLength = temporaryLength;
+    }
+
+    public double getLinkLength() {
+        return this.linkLength;
     }
 
     public int getLinkId() {
-        return linkId;
+        return this.linkId;
+    }
+
+    private void setLinkGeometry(Geometry linkGeometry) throws InvalidObjectException {
+        GeometryFactory gf = new GeometryFactory();
+        if (linkGeometry.getGeometryType().equals("LineString")) {
+            this.linkGeometry = gf.createLineString(linkGeometry.getCoordinates());
+            this.coordinateFirstX = this.linkGeometry.getCoordinateN(0).x;
+            this.coordinateFirstY = this.linkGeometry.getCoordinateN(0).y;
+            this.coordinateLastX = this.linkGeometry.getCoordinateN(this.linkGeometry.getCoordinates().length - 1).x;
+            this.coordinateLastY = this.linkGeometry.getCoordinateN(this.linkGeometry.getCoordinates().length - 1).y;
+        } else {
+            LOGGER.error("Invalid geometry - " + linkGeometry.getGeometryType());
+            throw new InvalidObjectException("Invalid geometry for linkId " + linkId);
+        }
     }
 
     public LineString getLinkGeometry() {
-        return linkGeometry;
-    }
-
-    public void setLinkGeometry(LineString linkGeometry) {
-        this.linkGeometry = linkGeometry;
+        return this.linkGeometry;
     }
 
     public void setTrafficPatternId(TrafficEnums.TravelDirection travelDirection, TrafficEnums.WeekDay weekDay, Integer trafficPatternId) {
@@ -81,14 +105,6 @@ public class TrafficLink {
             this.trafficPatternIdsTo.put(weekDay, trafficPatternId);
         } else {
             this.trafficPatternIdsFrom.put(weekDay, trafficPatternId);
-        }
-    }
-
-    public Integer getTrafficPatternId(TrafficEnums.TravelDirection travelDirection, TrafficEnums.WeekDay weekDay) {
-        if (travelDirection == TrafficEnums.TravelDirection.TO) {
-            return this.trafficPatternIdsTo.get(weekDay);
-        } else {
-            return this.trafficPatternIdsFrom.get(weekDay);
         }
     }
 
@@ -107,35 +123,8 @@ public class TrafficLink {
         return trafficLinkMetadata.functionalClass() != TrafficEnums.FunctionalClass.CLASS5;
     }
 
-    public TrafficEnums.FunctionalClass getWayType() {
-        return trafficLinkMetadata.functionalClass();
-    }
-
     public boolean isBothDirections() {
         return trafficLinkMetadata.getTravelDirection() == TrafficEnums.LinkTravelDirection.BOTH;
-    }
-
-    public double getLength(DistanceCalc dc) {
-        double res = 0;
-
-        if (this.getLinkGeometry() != null) {
-            LineString ls = this.getLinkGeometry();
-            int nPoints = ls.getNumPoints();
-
-            if (nPoints > 1) {
-                Coordinate c = ls.getCoordinateN(0);
-                double x0 = c.x;
-                double y0 = c.y;
-                for (int i = 1; i < ls.getNumPoints(); i++) {
-                    c = ls.getCoordinateN(i);
-
-                    res += dc.calcDist(y0, x0, c.y, c.x);
-                    x0 = c.x;
-                    y0 = c.y;
-                }
-            }
-        }
-        return res;
     }
 
     public int getFunctionalClass() {
@@ -143,18 +132,16 @@ public class TrafficLink {
     }
 
     public Geometry getToGeometry() {
-        Coordinate coordinateFirst = this.linkGeometry.getCoordinateN(0);
-        Coordinate coordinateLast = this.linkGeometry.getCoordinateN(this.linkGeometry.getCoordinates().length - 1);
-        if (coordinateFirst.y < coordinateLast.y) {
+        if (this.coordinateFirstY < this.coordinateLastY) {
             // First is Reference if its latitude is lower. Most common case!
             return this.linkGeometry.reverse();
-        } else if (coordinateFirst.y > coordinateLast.y) {
+        } else if (this.coordinateFirstY > this.coordinateLastY) {
             // Last is Reference if its latitude is lower
             return this.linkGeometry;
-        } else if (coordinateFirst.x < coordinateLast.x) {
+        } else if (this.coordinateFirstX < this.coordinateLastX) {
             // First is Reference if latitudes are equal but its longitude is lower
             return this.linkGeometry.reverse();
-        } else if (coordinateFirst.x > coordinateLast.x) {
+        } else if (this.coordinateFirstX > this.coordinateLastX) {
             // Last is Reference if latitudes are equal but its longitude is lower
             return this.linkGeometry;
         } else {
@@ -165,19 +152,17 @@ public class TrafficLink {
     }
 
     public Geometry getFromGeometry() {
-        Coordinate coordinateFirst = this.linkGeometry.getCoordinateN(0);
-        Coordinate coordinateLast = this.linkGeometry.getCoordinateN(this.linkGeometry.getCoordinates().length - 1);
-        if (coordinateFirst.y < coordinateLast.y) {
+        if (this.coordinateFirstY < this.coordinateLastY) {
             // First coordinate is the reference if its latitude is lower. Most common case!
             return this.linkGeometry;
-        } else if (coordinateFirst.y > coordinateLast.y) {
+        } else if (this.coordinateFirstY > this.coordinateLastY) {
             // Last  coordinate is Reference if its latitude is lower.
             return this.linkGeometry.reverse();
-        } else if (coordinateFirst.x < coordinateLast.x) {
+        } else if (this.coordinateFirstX < this.coordinateLastX) {
             // First coordinate is the reference if latitudes are equal but its longitude is lower.
             // This represents horizontal lines >------>
             return this.linkGeometry;
-        } else if (coordinateFirst.x > coordinateLast.x) {
+        } else if (this.coordinateFirstX > this.coordinateLastX) {
             // First coordinate is the reference if latitudes are equal but its longitude is lower.
             return this.linkGeometry.reverse();
         } else {
@@ -188,9 +173,5 @@ public class TrafficLink {
 
     public boolean isOnlyFromDirection() {
         return trafficLinkMetadata.getTravelDirection() == TrafficEnums.LinkTravelDirection.FROM;
-    }
-
-    public boolean isOnlyToDirection() {
-        return trafficLinkMetadata.getTravelDirection() == TrafficEnums.LinkTravelDirection.TO;
     }
 }
