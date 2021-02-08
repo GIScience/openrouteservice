c1a7707b6e76b49b180b77ee808e07c6671417fb
koebi <koebi@ezelo.de>
2021-02-08 17:40:41 +0100

    remove unused GeoJSON-Response class and import

--- openrouteservice/src/main/java/org/heigit/ors/api/responses/centrality/geojson/GeoJSONCentralityResponse.java
+++
@@ -1,6 +0,0 @@
-package org.heigit.ors.api.responses.centrality.geojson;
-
-import org.heigit.ors.api.responses.centrality.CentralityResponse;
-
-public class GeoJSONCentralityResponse extends CentralityResponse {
-}
