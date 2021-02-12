ef2dcec022a54a917c4f9628e355194146342e4a
koebi <koebi@ezelo.de>
2021-02-12 12:22:19 +0100

    throw StatusCodeException on validation error

--- openrouteservice/src/main/java/org/heigit/ors/routing/graphhopper/extensions/userspeed/RoadPropertySpeedParser.java
+++ openrouteservice/src/main/java/org/heigit/ors/routing/graphhopper/extensions/userspeed/RoadPropertySpeedParser.java
@@ -1,17 +1,19 @@
 package org.heigit.ors.routing.graphhopper.extensions.userspeed;
 
+import org.heigit.ors.exceptions.*;
+import org.heigit.ors.routing.RoutingErrorCodes;
 import org.json.JSONObject;
 
 public class RoadPropertySpeedParser {
-    public RoadPropertySpeedMap parse(String input) {
+    public RoadPropertySpeedMap parse(String input) throws StatusCodeException {
         return parse(new JSONObject(input));
     }
 
-    public RoadPropertySpeedMap parse(org.json.simple.JSONObject input) {
+    public RoadPropertySpeedMap parse(org.json.simple.JSONObject input) throws StatusCodeException {
         return parse(input.toJSONString());
     }
 
-    public RoadPropertySpeedMap parse(JSONObject json) {
+    public RoadPropertySpeedMap parse(JSONObject json) throws StatusCodeException {
         RoadPropertySpeedMap rsm = new RoadPropertySpeedMap();
 
         // parse units
@@ -19,9 +21,14 @@ public class RoadPropertySpeedParser {
         if (json.has("unit")) {
             unit = json.getString("unit");
         }
-        double unitFactor = 1.0;
-        if (unit.equals("mph")) {
+
+        double unitFactor;
+        if (unit.equals("kmh")) {
+            unitFactor = 1.0;
+        } else if (unit.equals("mph")) {
             unitFactor = 1.60934;
+        } else {
+            throw new ParameterValueException(RoutingErrorCodes.INVALID_PARAMETER_VALUE, "unit", unit);
         }
 
         // parse road speeds
@@ -29,7 +36,15 @@ public class RoadPropertySpeedParser {
         if (json.has("roadSpeeds")) {
             roadSpeeds = json.getJSONObject("roadSpeeds");
             for (String roadType : roadSpeeds.keySet()) {
-                rsm.addRoadPropertySpeed(roadType, roadSpeeds.getDouble(roadType)*unitFactor);
+                try {
+                    rsm.addRoadPropertySpeed(roadType, roadSpeeds.getDouble(roadType) * unitFactor);
+                } catch (IllegalArgumentException e) {
+                    if (e.getMessage().contains("must be")) {
+                        throw new ParameterOutOfRangeException(RoutingErrorCodes.INVALID_PARAMETER_VALUE, "speed");
+                    } else {
+                        throw new UnknownParameterException(RoutingErrorCodes.UNKNOWN_PARAMETER, e.getMessage().substring(23));
+                    }
+                }
             }
         }
 
@@ -38,7 +53,15 @@ public class RoadPropertySpeedParser {
         if (json.has("surfaceSpeeds")) {
            surfaceSpeeds = json.getJSONObject("surfaceSpeeds");
             for (String surfaceType : surfaceSpeeds.keySet()) {
-                rsm.addRoadPropertySpeed(surfaceType, surfaceSpeeds.getDouble(surfaceType)*unitFactor);
+                try {
+                    rsm.addRoadPropertySpeed(surfaceType, surfaceSpeeds.getDouble(surfaceType) * unitFactor);
+                } catch (IllegalArgumentException e) {
+                    if (e.getMessage().contains("must be")) {
+                        throw new ParameterOutOfRangeException(RoutingErrorCodes.INVALID_PARAMETER_VALUE, "speed");
+                    } else {
+                        throw new UnknownParameterException(RoutingErrorCodes.UNKNOWN_PARAMETER, e.getMessage().substring(23));
+                    }
+                }
             }
         }
 
