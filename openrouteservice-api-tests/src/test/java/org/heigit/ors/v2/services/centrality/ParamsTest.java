22b73fddb9786626538d71bbf269d483c0157219
koebi <koebi@ezelo.de>
2021-02-13 18:36:40 +0100

    restructure test data setup, add test for wrong size coordinates and bbox

--- openrouteservice-api-tests/src/test/java/org/heigit/ors/v2/services/centrality/ParamsTest.java
+++ openrouteservice-api-tests/src/test/java/org/heigit/ors/v2/services/centrality/ParamsTest.java
@@ -17,25 +17,49 @@ import static org.hamcrest.Matchers.*;
 public class ParamsTest extends ServiceTest {
 
     public ParamsTest() {
-        // set up valid bounding box encompassing main Heidelberg
-        JSONArray heidelbergBBox = new JSONArray();
+        // set up coordinates for testing later
         JSONArray lowerLeft = new JSONArray();
         JSONArray upperRight = new JSONArray();
+        JSONArray tooLargeCoordinate = new JSONArray();
+        JSONArray tooSmallCoordinate = new JSONArray();
         lowerLeft.put(8.655705);
         lowerLeft.put(49.395446);
         upperRight.put(8.718184);
         upperRight.put(49.434366);
+        tooLargeCoordinate.put(49.395446);
+        tooLargeCoordinate.put(49.395446);
+        tooLargeCoordinate.put(49.395446);
+        tooSmallCoordinate.put(8.65538);
 
+        // set up valid bounding box encompassing main Heidelberg
+        JSONArray heidelbergBBox = new JSONArray();
         heidelbergBBox.put(lowerLeft);
         heidelbergBBox.put(upperRight);
-
-        addParameter("heidelberg", heidelbergBBox);
-
+        addParameter("heidelbergBox", heidelbergBBox);
 
         // set up invalid bounding box containing too few coordinates
-        JSONArray tooFewCoordsBox = new JSONArray();
-        tooFewCoordsBox.put(lowerLeft);
-        addParameter("invalidBox", tooFewCoordsBox);
+        JSONArray tooSmallBox = new JSONArray();
+        tooSmallBox.put(lowerLeft);
+        addParameter("tooSmallBox", tooSmallBox);
+
+        // set up invalid bounding box containing too many coordinates
+        JSONArray tooLargeBox = new JSONArray();
+        tooLargeBox.put(lowerLeft);
+        tooLargeBox.put(upperRight);
+        tooLargeBox.put(upperRight);
+        addParameter("tooLargeBox", tooLargeBox);
+
+        // set up invalid bounding box containing a coordinate with too many ordinates
+        JSONArray tooLargeCoordBox = new JSONArray();
+        tooLargeCoordBox.put(lowerLeft);
+        tooLargeCoordBox.put(tooLargeCoordinate);
+        addParameter("tooLargeCoordBox", tooLargeCoordBox);
+
+        //set up invalid bounding box contaning a coordinate with too few ordinates
+        JSONArray tooSmallCoordBox = new JSONArray();
+        tooSmallCoordBox.put(lowerLeft);
+        tooSmallCoordBox.put(tooSmallCoordinate);
+        addParameter("tooSmallCoordBox", tooSmallCoordBox);
 
         // set up invalid/non-present Node
         JSONArray nonPresentNodes = new JSONArray();
@@ -54,7 +78,7 @@ public class ParamsTest extends ServiceTest {
     @Test
     public void testBasicFunctionality() {
         JSONObject body = new JSONObject();
-        body.put("bbox", getParameter("heidelberg"));
+        body.put("bbox", getParameter("heidelbergBox"));
 
         given()
                 .header("Accept", "application/json")
@@ -75,7 +99,7 @@ public class ParamsTest extends ServiceTest {
     @Test
     public void testExcludeNodes() {
         JSONObject body = new JSONObject();
-        body.put("bbox", getParameter("heidelberg"));
+        body.put("bbox", getParameter("heidelbergBox"));
 
         // check that nodes to exclude are present in the response
         List<Integer> nodeIds =  given()
@@ -120,7 +144,7 @@ public class ParamsTest extends ServiceTest {
     @Test
     public void testInvalidExcludeNodes() {
         JSONObject body = new JSONObject();
-        body.put("bbox", getParameter("heidelberg"));
+        body.put("bbox", getParameter("heidelbergBox"));
         body.put("excludeNodes", getParameter("invalidNodes"));
 
         given()
@@ -137,12 +161,11 @@ public class ParamsTest extends ServiceTest {
                 .statusCode(200);
     }
 
-    // test wrong bounding box:
-    // + too few coords
+    // test that invalid bounding boxes with too few/too many coordinates return an error
     @Test
-    public void testInvalidBBox() {
+    public void testWrongSizeBBox() {
         JSONObject body = new JSONObject();
-        body.put("bbox", getParameter("invalidBox"));
+        body.put("bbox", getParameter("tooSmallBox"));
 
         given()
                 .header("Accept", "application/json")
@@ -156,9 +179,53 @@ public class ParamsTest extends ServiceTest {
                 .log().ifValidationFails()
                 .body("error.code", is(CentralityErrorCodes.INVALID_PARAMETER_VALUE))
                 .statusCode(400);
-    }
-    // + too many coords
-    // + coords out of range?
 
-    //
+        body.put("bbox", getParameter("tooLargeBox"));
+        given()
+                .header("Accept", "application/json")
+                .header("Content-Type", "application/json")
+                .pathParam("profile", getParameter("carProfile"))
+                .body(body.toString())
+                .when()
+                .log().ifValidationFails()
+                .post(getEndPointPath()+"/{profile}/json")
+                .then()
+                .log().ifValidationFails()
+                .body("error.code", is(CentralityErrorCodes.INVALID_PARAMETER_VALUE))
+                .statusCode(400);
+    }
+
+    // test that invalid bounding boxes with too large/too small coordinates return an error
+    @Test
+    public void testWrongSizedCoordinates() {
+        JSONObject body = new JSONObject();
+        body.put("bbox", getParameter("tooSmallCoordBox"));
+
+        given()
+                .header("Accept", "application/json")
+                .header("Content-Type", "application/json")
+                .pathParam("profile", getParameter("carProfile"))
+                .body(body.toString())
+                .when()
+                .log().ifValidationFails()
+                .post(getEndPointPath()+"/{profile}/json")
+                .then()
+                .log().ifValidationFails()
+                .body("error.code", is(CentralityErrorCodes.INVALID_PARAMETER_VALUE))
+                .statusCode(400);
+
+        body.put("bbox", getParameter("tooLargeCoordBox"));
+        given()
+                .header("Accept", "application/json")
+                .header("Content-Type", "application/json")
+                .pathParam("profile", getParameter("carProfile"))
+                .body(body.toString())
+                .when()
+                .log().ifValidationFails()
+                .post(getEndPointPath()+"/{profile}/json")
+                .then()
+                .log().ifValidationFails()
+                .body("error.code", is(CentralityErrorCodes.INVALID_PARAMETER_VALUE))
+                .statusCode(400);
+    }
 }
