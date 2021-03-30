0d15015015df9e3918c6a04484066da52af30101
Takara Baumbach <takara.baumbach@heigit.org>
2021-03-30 09:31:00 +0200

    add inline doc

--- openrouteservice/src/main/java/org/heigit/ors/routing/RouteResultBuilder.java
+++ openrouteservice/src/main/java/org/heigit/ors/routing/RouteResultBuilder.java
@@ -322,7 +322,10 @@ class RouteResultBuilder
 	}
 
 	private int getEndWayPointIndex(int startIndex, InstructionType instrType, Instruction instr) {
-		if (instrType == InstructionType.FINISH || (instrType == InstructionType.DEPART && instr.getDistance() == 0.0 && instr.getPoints().size() == 1))
+		if (instrType == InstructionType.FINISH
+                // "empty" departure instruction means start and end coordinates are the same, index should not increase
+                || (instrType == InstructionType.DEPART && instr.getDistance() == 0.0 && instr.getPoints().size() == 1)
+            )
 			return startIndex;
 		else
 			return startIndex + instr.getPoints().size();
