e1d109ab59bad89bbd2407273d7cd0a257f91f8f
aoles <andrzej.oles@gmail.com>
2021-06-08 17:28:32 +0200

    Set HGV maximum possible speed and use it in traffic speed calculator

--- openrouteservice/src/main/java/org/heigit/ors/routing/traffic/RoutingTrafficSpeedCalculator.java
+++ openrouteservice/src/main/java/org/heigit/ors/routing/traffic/RoutingTrafficSpeedCalculator.java
@@ -7,14 +7,24 @@ import com.graphhopper.storage.GraphHopperStorage;
 import com.graphhopper.util.EdgeIteratorState;
 
 public class RoutingTrafficSpeedCalculator extends TrafficSpeedCalculator {
+    private double maxPossibleSpeed;
 
     public RoutingTrafficSpeedCalculator(SpeedCalculator superSpeedCalculator, GraphHopperStorage graphHopperStorage, FlagEncoder flagEncoder) {
         super(superSpeedCalculator);
         init(graphHopperStorage, flagEncoder);
+        maxPossibleSpeed = flagEncoder.getMaxSpeed();
     }
 
     @Override
     public double getSpeed(EdgeIteratorState edge, boolean reverse, long time) {
+        double speed = super.getSpeed(edge, reverse, time);
+
+        if (speed > maxPossibleSpeed)
+            speed = maxPossibleSpeed;
+
+        return speed;
+
+        /*
         double speed = superSpeedCalculator.getSpeed(edge, reverse, time);
         if (time != -1) {
             int edgeId = EdgeKeys.getOriginalEdge(edge);
@@ -28,5 +38,6 @@ public class RoutingTrafficSpeedCalculator extends TrafficSpeedCalculator {
             }
         }
         return speed;
+        */
     }
 }
