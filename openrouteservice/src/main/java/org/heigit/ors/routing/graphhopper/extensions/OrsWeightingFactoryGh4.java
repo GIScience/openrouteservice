e303a04be84f3b269e1e48681470e1f80c51036e
Sascha Fendrich <sascha.fendrich@heigit.org>
2022-04-06 12:44:20 +0200

    Enable further weightings
    Further weightings have been added to OrsWeightingFactoryGh4 in order
    to move forward the transition from the deprecated ORSWeightingFactory.

--- openrouteservice/src/main/java/org/heigit/ors/routing/graphhopper/extensions/OrsWeightingFactoryGh4.java
+++ openrouteservice/src/main/java/org/heigit/ors/routing/graphhopper/extensions/OrsWeightingFactoryGh4.java
@@ -9,6 +9,12 @@ import com.graphhopper.routing.weighting.TurnCostProvider;
 import com.graphhopper.routing.weighting.Weighting;
 import com.graphhopper.storage.GraphHopperStorage;
 import com.graphhopper.util.PMap;
+import org.heigit.ors.routing.ProfileWeighting;
+import org.heigit.ors.routing.graphhopper.extensions.weighting.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * This class is a preliminary adaptation of ORSWeightingFactory to the new
@@ -21,20 +27,109 @@ import com.graphhopper.util.PMap;
  * ones are remnants of unmaintained features.
  */
 public class OrsWeightingFactoryGh4 extends DefaultWeightingFactory {
+    private GraphHopperStorage graphStorage;
+
     public OrsWeightingFactoryGh4(GraphHopperStorage ghStorage, EncodingManager encodingManager) {
         super(ghStorage, encodingManager);
+        graphStorage = ghStorage; // TODO: cleanup - this references the same storage as in super
     }
 
     @Override
     protected Weighting handleExternalOrsWeightings(String weightingStr, PMap hints, FlagEncoder encoder, TurnCostProvider turnCostProvider) {
         Weighting weighting = null;
-        if (weightingStr.equalsIgnoreCase("recommended")) {
+        if ("priority".equalsIgnoreCase(weightingStr)) {
+            weighting = new PreferencePriorityWeighting(encoder, hints);
+        } else if ("recommended_pref".equalsIgnoreCase(weightingStr)) {
             if (encoder.supports(PriorityWeighting.class)) {
-                weighting = new PriorityWeighting(encoder, hints, turnCostProvider);
+                weighting = new PreferencePriorityWeighting(encoder, hints, turnCostProvider);
             } else {
                 weighting = new FastestWeighting(encoder, hints, turnCostProvider);
             }
+        } else if ("recommended".equalsIgnoreCase(weightingStr)) {
+            if (encoder.supports(PriorityWeighting.class)) {
+                weighting = new OptimizedPriorityWeighting(encoder, hints, turnCostProvider);
+            } else {
+                weighting = new FastestWeighting(encoder, hints, turnCostProvider);
+            }
+        } else {
+            if (encoder.supports(PriorityWeighting.class)) {
+                weighting = new FastestSafeWeighting(encoder, hints); // TODO: do we need turnCostProvider here?
+            } else {
+                weighting = new FastestWeighting(encoder, hints); // TODO: do we need turnCostProvider here?
+            }
+        }
+
+        weighting = applySoftWeightings(hints, encoder, weighting);
+
+        return weighting;
+    }
+
+    private Weighting applySoftWeightings(PMap hints, FlagEncoder encoder, Weighting weighting) {
+        // TODO (cleanup): The term "custom_weighting" is easily confused with GH's custom
+        //                 weighting and should be renamed.
+        if (hints.getBool("custom_weightings", false))
+        {
+            Map<String, Object> map = hints.toMap();
+
+            List<String> weightingNames = new ArrayList<>();
+            for (Map.Entry<String, Object> kv : map.entrySet())
+            {
+                String name = ProfileWeighting.decodeName(kv.getKey());
+                if (name != null && !weightingNames.contains(name))
+                    weightingNames.add(name);
+            }
+
+            List<Weighting> softWeightings = new ArrayList<>();
+
+            for (String weightingName : weightingNames) {
+                switch (weightingName) {
+                    case "steepness_difficulty":
+                        softWeightings.add(new SteepnessDifficultyWeighting(encoder, getWeightingProps(weightingName, map), graphStorage));
+                        break;
+                    case "avoid_hills":
+                        softWeightings.add(new AvoidHillsWeighting(encoder, getWeightingProps(weightingName, map), graphStorage));
+                        break;
+                    case "green":
+                        softWeightings.add(new GreenWeighting(encoder, getWeightingProps(weightingName, map), graphStorage));
+                        break;
+                    case "quiet":
+                        softWeightings.add(new QuietWeighting(encoder, getWeightingProps(weightingName, map), graphStorage));
+                        break;
+                    case "acceleration":
+                        softWeightings.add(new AccelerationWeighting(encoder, getWeightingProps(weightingName, map), graphStorage));
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            if (!softWeightings.isEmpty()) {
+                // TODO (cleanup): We do not want to mess around with implementation
+                //                 details of AdditionWeighting which should accept
+                //                 any collection instead of its internal array.
+                Weighting[] arrWeightings = new Weighting[softWeightings.size()];
+                arrWeightings = softWeightings.toArray(arrWeightings);
+                weighting = new AdditionWeighting(arrWeightings, weighting);
+            }
         }
         return weighting;
+    }
+
+    private PMap getWeightingProps(String weightingName, Map<String, Object> map)
+    {
+        PMap res = new PMap();
+
+        String prefix = "weighting_#" + weightingName;
+        int n = prefix.length();
+
+        for (Map.Entry<String, Object> kv : map.entrySet())
+        {
+            String name = kv.getKey();
+            int p = name.indexOf(prefix);
+            if (p >= 0)
+                res.putObject(name.substring(p + n + 1), kv.getValue());
+        }
+
+        return res;
     }
 }
