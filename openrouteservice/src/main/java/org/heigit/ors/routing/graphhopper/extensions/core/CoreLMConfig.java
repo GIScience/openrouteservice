7baf2baca0abb25486b08b2e4d509dcd4e83de54
aoles <andrzej.oles@gmail.com>
2022-05-13 17:38:02 +0200

    Fix the creation of preparations for different core LM sets

--- openrouteservice/src/main/java/org/heigit/ors/routing/graphhopper/extensions/core/CoreLMConfig.java
+++ openrouteservice/src/main/java/org/heigit/ors/routing/graphhopper/extensions/core/CoreLMConfig.java
@@ -4,6 +4,8 @@ import com.graphhopper.routing.lm.LMConfig;
 import com.graphhopper.routing.weighting.Weighting;
 import org.heigit.ors.routing.graphhopper.extensions.edgefilters.core.LMEdgeFilterSequence;
 
+import java.util.Objects;
+
 public class CoreLMConfig extends LMConfig {
     LMEdgeFilterSequence edgeFilter;
 
@@ -22,10 +24,27 @@ public class CoreLMConfig extends LMConfig {
 
     @Override
     public String getName() {
-        return super.getName() + "_" + edgeFilter.getName();
+        return getSuperName() + "_" + edgeFilter.getName();
     }
 
     public String getSuperName() {
         return super.getName();
+    }
+
+    @Override
+    public String toString() {
+        return getName();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!super.equals(o)) {
+            return false;
+        } else if (o != null && this.getClass() == o.getClass()) {
+            CoreLMConfig lmConfig = (CoreLMConfig) o;
+            return Objects.equals(this.edgeFilter, lmConfig.edgeFilter);
+        } else {
+            return false;
+        }
     }
 }
