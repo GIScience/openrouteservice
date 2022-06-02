8b2059474d0257a745770deaee7d605d3616a06a
Jakob Schnell <koebi@ezelo.de>
2022-06-02 09:39:09 +0200

    add unit tests for waterway=lock_gate access

--- openrouteservice/src/test/java/org/heigit/ors/routing/graphhopper/extensions/flagencoders/PedestrianFlagEncoderTest.java
+++ openrouteservice/src/test/java/org/heigit/ors/routing/graphhopper/extensions/flagencoders/PedestrianFlagEncoderTest.java
@@ -336,4 +336,27 @@ public class PedestrianFlagEncoderTest {
 //        assertTrue(throwsError);
     }
 
+    @Test
+    public void acceptLockGateFootAllowed() {
+        way.setTag("waterway", "lock_gate");
+        way.setTag("foot", "yes");
+
+        assertTrue(flagEncoder.getAccess(way).isWay());
+    }
+
+    @Test
+    public void rejectLockGateFootAccessMissing() {
+        way.setTag("waterway", "lock_gate");
+
+        assertTrue(flagEncoder.getAccess(way).canSkip());
+    }
+
+    @Test
+    public void rejectLockGateFootForbidden() {
+        way.setTag("waterway", "lock_gate");
+        way.setTag("foot", "no");
+
+        assertTrue(flagEncoder.getAccess(way).canSkip());
+    }
+
 }
