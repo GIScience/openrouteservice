f2024e6de18c1f7a200a1284b65d8e4f6249bf13
Julian Psotta <julian.psotta@heigit.org>
2023-02-18 11:48:23 +0100

    fix: Fix the broken verify stage.
    spring-boot starts in the ./openrouteservice subfolder instead of ../. This requires paths adjustments in the api-tests startup script. Additionally some ant tasks for cleaning ors.run are introduced.

--- openrouteservice-api-tests/run_api_tests.sh
+++ openrouteservice-api-tests/run_api_tests.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-while [ ! -f  $1/ors.run ]
+while [ ! -f  "$1"/ors.run ]
 do
-  sleep 1
+  sleep 5
 done
-mvn -B test --file $1/openrouteservice-api-tests/pom.xml
+mvn -B test --file "$1"/../openrouteservice-api-tests/pom.xml
 res=$?
-rm $1/ors.run
+
 if [ "$res" -ne 0 ] ; then
   echo 'API tests failed!'
   exit $res
