a87e8690ea026d58f508d359cdc5372de959b760
Julian Psotta <julian.psotta@heigit.org>
2023-03-13 17:52:08 +0100

    test(api-tests): enhance api-tests
    These changes let ors start the api-test routine with random ports and pass this port to the api-test execution. Therefore the api-tests can be executed non-blocking.

--- openrouteservice-api-tests/run_api_tests.sh
+++ openrouteservice-api-tests/run_api_tests.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-while [ ! -f  $1/ors.run ]
-do
-  sleep 1
+while [ ! -f "$1"/ors.run ]; do
+  sleep 5
 done
-mvn -B test --file $1/openrouteservice-api-tests/pom.xml
+port=${2}
+echo "Running API-Tests on port: $port"
+mvn -Dserver.port="$port" -B test --file "$1"/../openrouteservice-api-tests/pom.xml
 res=$?
-rm $1/ors.run
-if [ "$res" -ne 0 ] ; then
+
+if [ "$res" -ne 0 ]; then
   echo 'API tests failed!'
   exit $res
 fi
