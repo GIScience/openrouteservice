c4d4942b07d69f5009cca9271c8b66dd951a3e88
Julian Psotta <julian.psotta@heigit.org>
2023-03-13 18:03:30 +0100

    refactor(backend): remove illegal System.runFinalization call
    For whatever reason System.runFinalization(); was called at context closure. This cuts short the native java closing routines, e.g. garbage collector etc. This is handled by spring-boot through the Bean context.

--- openrouteservice/src/main/java/org/heigit/ors/servlet/listeners/ORSInitContextListener.java
+++ openrouteservice/src/main/java/org/heigit/ors/servlet/listeners/ORSInitContextListener.java
@@ -4,14 +4,14 @@
  *   http://giscience.uni-hd.de
  *   http://heigit.org
  *
- *  under one or more contributor license agreements. See the NOTICE file 
- *  distributed with this work for additional information regarding copyright 
- *  ownership. The GIScience licenses this file to you under the Apache License, 
- *  Version 2.0 (the "License"); you may not use this file except in compliance 
+ *  under one or more contributor license agreements. See the NOTICE file
+ *  distributed with this work for additional information regarding copyright
+ *  ownership. The GIScience licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except in compliance
  *  with the License. You may obtain a copy of the License at
- * 
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,41 +31,34 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 public class ORSInitContextListener implements ServletContextListener {
-	private static final Logger LOGGER = Logger.getLogger("org.heigit.ors.logging");
+    private static final Logger LOGGER = Logger.getLogger("org.heigit.ors.logging");
 
-	@Override
-	public void contextInitialized(ServletContextEvent contextEvent)  {
-		Runnable runnable = () -> {
-		    try {
-		    	RoutingProfileManager.getInstance();
-		    }
-		    catch (Exception e) {
-		    	LOGGER.warn("Unable to initialize ORS." + e);
-		    }
-		};
+    @Override
+    public void contextInitialized(ServletContextEvent contextEvent) {
+        Runnable runnable = () -> {
+            try {
+                RoutingProfileManager.getInstance();
+            } catch (Exception e) {
+                LOGGER.warn("Unable to initialize ORS." + e);
+            }
+        };
 
-		Thread thread = new Thread(runnable);
-		thread.setName("ORS-Init");
-		thread.start();
-	}
+        Thread thread = new Thread(runnable);
+        thread.setName("ORS-Init");
+        thread.start();
+    }
 
-	@Override
-	public void contextDestroyed(ServletContextEvent contextEvent) {
-		try {
-			LOGGER.info("Start shutting down ORS and releasing resources.");
-			FormatUtility.unload();
-			if (RoutingProfileManagerStatus.isReady())
-				RoutingProfileManager.getInstance().destroy();
-			StatisticsProviderFactory.releaseProviders();
-			LogFactory.release(Thread.currentThread().getContextClassLoader());
-		} catch (Exception e) {
-			LOGGER.error(e.getStackTrace());
-		} finally {
-			try {
-				System.runFinalization();
-			} catch(Exception e) {
-				LOGGER.error("Failed to perform finalization." + e);
-			}
-		}
-	}
+    @Override
+    public void contextDestroyed(ServletContextEvent contextEvent) {
+        try {
+            LOGGER.info("Start shutting down ORS and releasing resources.");
+            FormatUtility.unload();
+            if (RoutingProfileManagerStatus.isReady())
+                RoutingProfileManager.getInstance().destroy();
+            StatisticsProviderFactory.releaseProviders();
+            LogFactory.release(Thread.currentThread().getContextClassLoader());
+        } catch (Exception e) {
+            LOGGER.error(e.getStackTrace());
+        }
+    }
 } 
