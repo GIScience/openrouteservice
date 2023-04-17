6762841c71c6d9deb1d9ff9714926b14d327746b
Julian Psotta <julian.psotta@heigit.org>
2023-04-17 12:00:40 +0200

    chore(api-test): Move cors test to apitests module and rename to cors

--- openrouteservice/src/test/java/org/heigit/ors/apitests/cors/CORSSettingsTest.java
+++ openrouteservice/src/test/java/org/heigit/ors/apitests/cors/CORSSettingsTest.java
@@ -11,7 +11,7 @@
  *  You should have received a copy of the GNU Lesser General Public License along with this library;
  *  if not, see <https://www.gnu.org/licenses/>.
  */
-package org.heigit.ors.apisettings;
+package org.heigit.ors.apitests.cors;
 
 import io.restassured.response.Response;
 import org.heigit.ors.apitests.common.EndPointAnnotation;
