dfa9f0868d8387557d904b3a7117ae24ddcee058
Jakob Schnell <koebi@ezelo.de>
2023-05-10 12:28:11 +0200

    refactor: remove jakarta.servlet dependency; use java-internal Status Codes
    Co-authored-by: Johannes Link <business@johanneslink.net>

--- ors-routing/src/main/java/org/heigit/ors/common/StatusCode.java
+++ ors-routing/src/main/java/org/heigit/ors/common/StatusCode.java
@@ -13,46 +13,48 @@
  */
 package org.heigit.ors.common;
 
+import java.net.HttpURLConnection;
+
 public class StatusCode {
     /**
      * Status code (200) indicating the request succeeded normally.
      */
-    public static final int OK = javax.servlet.http.HttpServletResponse.SC_OK;
+    public static final int OK = HttpURLConnection.HTTP_OK;
 	 /**
      * Status code (400) indicating the request sent by the client was
      * syntactically incorrect.
      */
-   public static final int BAD_REQUEST = javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
+   public static final int BAD_REQUEST = HttpURLConnection.HTTP_BAD_REQUEST;
    /**
     * Status code (405) indicating that the method specified in the
     * <code><em>Request-Line</em></code> is not allowed for the resource
     * identified by the <code><em>Request-URI</em></code>.
     */
-   public static final int METHOD_NOT_ALLOWED = javax.servlet.http.HttpServletResponse.SC_METHOD_NOT_ALLOWED;
+   public static final int METHOD_NOT_ALLOWED = HttpURLConnection.HTTP_BAD_METHOD;
    
    /**
     * Status code (500) indicating an error inside the HTTP server
     * which prevented it from fulfilling the request.
     */
-   public static final int INTERNAL_SERVER_ERROR = javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+   public static final int INTERNAL_SERVER_ERROR = HttpURLConnection.HTTP_INTERNAL_ERROR;
    
    /**
     * Status code (501) indicating the HTTP server does not support
     * the functionality needed to fulfill the request.
     */
-   public static final int NOT_IMPLEMENTED = javax.servlet.http.HttpServletResponse.SC_NOT_IMPLEMENTED;
+   public static final int NOT_IMPLEMENTED = HttpURLConnection.HTTP_NOT_IMPLEMENTED;
    
    /**
     * Status code (503) indicating that the End Point is
     * temporarily overloaded, and unable to handle the request.
     */
-   public static final int SERVICE_UNAVAILABLE =  javax.servlet.http.HttpServletResponse.SC_SERVICE_UNAVAILABLE;
+   public static final int SERVICE_UNAVAILABLE = HttpURLConnection.HTTP_UNAVAILABLE;
 
    /**
     * Status code (404) indicating that the request was processed but
     * no information was found (i.e. a geocoding request that did not find a corresponding address)
     */
-   public static final int NOT_FOUND = javax.servlet.http.HttpServletResponse.SC_NOT_FOUND;
+   public static final int NOT_FOUND = HttpURLConnection.HTTP_NOT_FOUND;
 
    private StatusCode() {}
 }
