b9e41c5ba31c8daef17792362339c783c59290d0
Takara Baumbach <takara.baumbach@heigit.org>
2023-06-12 11:54:35 +0200

    fix: adjust test result

--- openrouteservice/src/test/java/org/heigit/ors/apitests/routing/ResultTest.java
+++ openrouteservice/src/test/java/org/heigit/ors/apitests/routing/ResultTest.java
@@ -3653,7 +3653,7 @@ class ResultTest extends ServiceTest {
                 .assertThat()
                 .body("any { it.key == 'routes' }", is(true))
                 .body("routes[0].summary.distance", is(closeTo(1327.1, 1)))
-                .body("routes[0].summary.duration", is(closeTo(495.0, 1)))
+                .body("routes[0].summary.duration", is(closeTo(477.9, 1)))
                 .statusCode(200);
     }
 
