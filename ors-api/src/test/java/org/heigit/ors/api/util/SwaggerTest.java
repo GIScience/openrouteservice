b9d6315b191243955f0b7c23c4badcaded5f8093
Jakob Schnell <koebi@ezelo.de>
2023-07-19 14:25:59 +0200

    test: re-enable openapi unittest

--- ors-api/src/test/java/org/heigit/ors/api/util/SwaggerTest.java
+++ ors-api/src/test/java/org/heigit/ors/api/util/SwaggerTest.java
@@ -1,20 +1,32 @@
-//package org.heigit.ors.api.util;
-//
-//import org.heigit.ors.api.OpenApiConfiguration;
-//import org.junit.jupiter.api.Test;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//
-//class OpenAPITest {
-//    @Test
-//    void testGetOpenAPIDocs() {
-//        OpenAPIConfig oas_config = new OpenAPIConfig();
-//        Docket api = oas_config.api();
-//        assertEquals(DocumentationType.SWAGGER_2, api.getDocumentationType());
-//        assertTrue(api.isEnabled());
-//    }
-//}
+package org.heigit.ors.api.util;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.SpecVersion;
+import jakarta.servlet.ServletContext;
+import org.heigit.ors.api.EndpointsProperties;
+import org.heigit.ors.api.OpenAPIConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+@ActiveProfiles("unittest")
+class OpenAPITest {
+    @Autowired
+    private final EndpointsProperties endpointsProperties = new EndpointsProperties();
+
+    @Autowired
+    private final ServletContext servletContext = new MockServletContext();
+
+    @Test
+    void testGetOpenAPIDocs() {
+        OpenAPIConfig oas_config = new OpenAPIConfig(endpointsProperties);
+        OpenAPI api = oas_config.customOpenAPI(servletContext);
+        assertEquals(SpecVersion.V30, api.getSpecVersion());
+    }
+}
