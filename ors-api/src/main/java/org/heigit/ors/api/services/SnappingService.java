e6a6a6c42120ef40b74e13bc7950e4870fbea1ec
Jochen Haeussler <jochen.haeussler@heigit.org>
2023-10-04 14:46:27 +0200

    refactor(snapping): moved computing method to the service class

--- ors-api/src/main/java/org/heigit/ors/api/services/SnappingService.java
+++ ors-api/src/main/java/org/heigit/ors/api/services/SnappingService.java
@@ -1,18 +1,28 @@
 package org.heigit.ors.api.services;
 
 import com.graphhopper.GraphHopper;
+import com.graphhopper.routing.util.AccessFilter;
+import com.graphhopper.routing.util.FlagEncoder;
+import com.graphhopper.routing.weighting.Weighting;
+import com.graphhopper.storage.GraphHopperStorage;
+import com.graphhopper.util.PMap;
+import org.heigit.ors.api.requests.snapping.SnappingApiRequest;
 import org.heigit.ors.common.StatusCode;
 import org.heigit.ors.exceptions.ParameterValueException;
 import org.heigit.ors.exceptions.PointNotFoundException;
 import org.heigit.ors.exceptions.StatusCodeException;
-import org.heigit.ors.snapping.SnappingErrorCodes;
+import org.heigit.ors.matrix.MatrixSearchContext;
+import org.heigit.ors.matrix.MatrixSearchContextBuilder;
 import org.heigit.ors.routing.RoutingProfile;
 import org.heigit.ors.routing.RoutingProfileManager;
+import org.heigit.ors.routing.RoutingProfileType;
+import org.heigit.ors.routing.WeightingMethod;
+import org.heigit.ors.routing.graphhopper.extensions.ORSWeightingFactory;
+import org.heigit.ors.snapping.SnappingErrorCodes;
 import org.heigit.ors.snapping.SnappingRequest;
-import org.heigit.ors.api.requests.snapping.SnappingApiRequest;
 import org.heigit.ors.snapping.SnappingResult;
+import org.heigit.ors.util.ProfileTools;
 import org.locationtech.jts.geom.Coordinate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -27,7 +37,7 @@ public class SnappingService extends ApiService {
             RoutingProfileManager rpm = RoutingProfileManager.getInstance();
             RoutingProfile rp = rpm.getProfiles().getRouteProfile(snappingRequest.getProfileType());
             GraphHopper gh = rp.getGraphhopper();
-            return snappingRequest.computeResult(gh);
+            return computeResult(snappingRequest, gh);
         } catch (PointNotFoundException e) {
             throw new StatusCodeException(StatusCode.NOT_FOUND, SnappingErrorCodes.POINT_NOT_FOUND, e.getMessage());
         } catch (StatusCodeException e) {
@@ -69,5 +79,23 @@ public class SnappingService extends ApiService {
             throw new ParameterValueException(SnappingErrorCodes.INVALID_PARAMETER_VALUE, SnappingApiRequest.PARAM_LOCATIONS);
         }
         return new Coordinate(location.get(0), location.get(1));
+    }
+
+    public SnappingResult computeResult(SnappingRequest snappingRequest, GraphHopper gh) throws Exception {
+        String encoderName = RoutingProfileType.getEncoderName(snappingRequest.getProfileType());
+        FlagEncoder flagEncoder = gh.getEncodingManager().getEncoder(encoderName);
+        PMap hintsMap = new PMap();
+        int weightingMethod = WeightingMethod.RECOMMENDED; // Only needed to create the profile string
+        ProfileTools.setWeightingMethod(hintsMap, weightingMethod, snappingRequest.getProfileType(), false);
+        ProfileTools.setWeighting(hintsMap, weightingMethod, snappingRequest.getProfileType(), false);
+        String profileName = ProfileTools.makeProfileName(encoderName, hintsMap.getString("weighting", ""), false);
+        GraphHopperStorage ghStorage = gh.getGraphHopperStorage();
+        String graphDate = ghStorage.getProperties().get("datareader.import.date");
+
+        // TODO: replace usage of matrix search context by snapping-specific class
+        MatrixSearchContextBuilder builder = new MatrixSearchContextBuilder(ghStorage, gh.getLocationIndex(), AccessFilter.allEdges(flagEncoder.getAccessEnc()), true);
+        Weighting weighting = new ORSWeightingFactory(ghStorage, gh.getEncodingManager()).createWeighting(gh.getProfile(profileName), hintsMap, false);
+        MatrixSearchContext mtxSearchCntx = builder.create(ghStorage.getBaseGraph(), null, weighting, profileName, snappingRequest.getLocations(), snappingRequest.getLocations(), snappingRequest.getMaximumSearchRadius());
+        return new SnappingResult(mtxSearchCntx.getSources().getLocations(), graphDate);
     }
 }
--- ors-engine/src/main/java/org/heigit/ors/snapping/SnappingRequest.java
+++ ors-engine/src/main/java/org/heigit/ors/snapping/SnappingRequest.java
@@ -1,18 +1,6 @@
 package org.heigit.ors.snapping;
 
-import com.graphhopper.GraphHopper;
-import com.graphhopper.routing.util.AccessFilter;
-import com.graphhopper.routing.util.FlagEncoder;
-import com.graphhopper.routing.weighting.Weighting;
-import com.graphhopper.storage.GraphHopperStorage;
-import com.graphhopper.util.PMap;
 import org.heigit.ors.common.ServiceRequest;
-import org.heigit.ors.matrix.MatrixSearchContext;
-import org.heigit.ors.matrix.MatrixSearchContextBuilder;
-import org.heigit.ors.routing.RoutingProfileType;
-import org.heigit.ors.routing.WeightingMethod;
-import org.heigit.ors.routing.graphhopper.extensions.ORSWeightingFactory;
-import org.heigit.ors.util.ProfileTools;
 import org.locationtech.jts.geom.Coordinate;
 
 public class SnappingRequest extends ServiceRequest {
@@ -26,26 +14,15 @@ public class SnappingRequest extends ServiceRequest {
         this.maximumSearchRadius = maximumSearchRadius;
     }
 
-    public SnappingResult computeResult(GraphHopper gh) throws Exception {
-        String encoderName = RoutingProfileType.getEncoderName(profileType);
-        FlagEncoder flagEncoder = gh.getEncodingManager().getEncoder(encoderName);
-        PMap hintsMap = new PMap();
-        int weightingMethod = WeightingMethod.RECOMMENDED; // Only needed to create the profile string
-        ProfileTools.setWeightingMethod(hintsMap, weightingMethod, profileType, false);
-        ProfileTools.setWeighting(hintsMap, weightingMethod, profileType, false);
-        String profileName = ProfileTools.makeProfileName(encoderName, hintsMap.getString("weighting", ""), false);
-        GraphHopperStorage ghStorage = gh.getGraphHopperStorage();
-        String graphDate = ghStorage.getProperties().get("datareader.import.date");
-
-        // TODO: replace usage of matrix search context by snapping-specific class
-        MatrixSearchContextBuilder builder = new MatrixSearchContextBuilder(ghStorage, gh.getLocationIndex(), AccessFilter.allEdges(flagEncoder.getAccessEnc()), true);
-        Weighting weighting = new ORSWeightingFactory(ghStorage, gh.getEncodingManager()).createWeighting(gh.getProfile(profileName), hintsMap, false);
-        MatrixSearchContext mtxSearchCntx = builder.create(ghStorage.getBaseGraph(), null, weighting, profileName, locations, locations, maximumSearchRadius);
-        return new SnappingResult(mtxSearchCntx.getSources().getLocations(), graphDate);
-    }
-
     public int getProfileType() {
         return profileType;
     }
 
+    public Coordinate[] getLocations() {
+        return locations;
+    }
+
+    public double getMaximumSearchRadius() {
+        return maximumSearchRadius;
+    }
 }
