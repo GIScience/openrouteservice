d77eac9a1ac7f7bacd44602dbac100e1fbde50ad
aoles <andrzej.oles@gmail.com>
2023-10-04 13:06:49 +0200

    refactor(snapping): remove dependency on CH graph

--- ors-engine/src/main/java/org/heigit/ors/snapping/SnappingRequest.java
+++ ors-engine/src/main/java/org/heigit/ors/snapping/SnappingRequest.java
@@ -3,13 +3,15 @@ package org.heigit.ors.snapping;
 import com.graphhopper.GraphHopper;
 import com.graphhopper.routing.util.AccessFilter;
 import com.graphhopper.routing.util.FlagEncoder;
-import com.graphhopper.storage.RoutingCHGraph;
+import com.graphhopper.routing.weighting.Weighting;
+import com.graphhopper.storage.GraphHopperStorage;
 import com.graphhopper.util.PMap;
 import org.heigit.ors.common.ServiceRequest;
 import org.heigit.ors.matrix.MatrixSearchContext;
 import org.heigit.ors.matrix.MatrixSearchContextBuilder;
 import org.heigit.ors.routing.RoutingProfileType;
 import org.heigit.ors.routing.WeightingMethod;
+import org.heigit.ors.routing.graphhopper.extensions.ORSWeightingFactory;
 import org.heigit.ors.util.ProfileTools;
 import org.locationtech.jts.geom.Coordinate;
 
@@ -31,14 +33,14 @@ public class SnappingRequest extends ServiceRequest {
         int weightingMethod = WeightingMethod.RECOMMENDED; // Only needed to create the profile string
         ProfileTools.setWeightingMethod(hintsMap, weightingMethod, profileType, false);
         ProfileTools.setWeighting(hintsMap, weightingMethod, profileType, false);
-        String CHProfileName = ProfileTools.makeProfileName(encoderName, hintsMap.getString("weighting", ""), false);
-        String profileName = CHProfileName;
-        String graphDate = gh.getGraphHopperStorage().getProperties().get("datareader.import.date");
+        String profileName = ProfileTools.makeProfileName(encoderName, hintsMap.getString("weighting", ""), false);
+        GraphHopperStorage ghStorage = gh.getGraphHopperStorage();
+        String graphDate = ghStorage.getProperties().get("datareader.import.date");
 
         // TODO: replace usage of matrix search context by snapping-specific class
-        RoutingCHGraph routingCHGraph = gh.getGraphHopperStorage().getRoutingCHGraph(profileName);
-        MatrixSearchContextBuilder builder = new MatrixSearchContextBuilder(gh.getGraphHopperStorage(), gh.getLocationIndex(), AccessFilter.allEdges(flagEncoder.getAccessEnc()), true);
-        MatrixSearchContext mtxSearchCntx = builder.create(routingCHGraph.getBaseGraph(), routingCHGraph, routingCHGraph.getWeighting(), profileName, locations, locations, maximumSearchRadius);
+        MatrixSearchContextBuilder builder = new MatrixSearchContextBuilder(ghStorage, gh.getLocationIndex(), AccessFilter.allEdges(flagEncoder.getAccessEnc()), true);
+        Weighting weighting = new ORSWeightingFactory(ghStorage, gh.getEncodingManager()).createWeighting(gh.getProfile(profileName), hintsMap, false);
+        MatrixSearchContext mtxSearchCntx = builder.create(ghStorage.getBaseGraph(), null, weighting, profileName, locations, locations, maximumSearchRadius);
         return new SnappingResult(mtxSearchCntx.getSources().getLocations(), graphDate);
     }
 
