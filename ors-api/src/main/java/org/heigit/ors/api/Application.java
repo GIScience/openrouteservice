37e3de991ae671df02294c712970142bf0c2790d
Jochen Haeussler <jochen.haeussler@heigit.org>
2023-11-03 14:58:32 +0100

    chore: log ors version and buildDate on startup and shutdown

--- ors-api/src/main/java/org/heigit/ors/api/Application.java
+++ ors-api/src/main/java/org/heigit/ors/api/Application.java
@@ -1,7 +1,9 @@
 package org.heigit.ors.api;
 
 import jakarta.servlet.ServletContextListener;
+import org.apache.log4j.Logger;
 import org.heigit.ors.api.servlet.listeners.ORSInitContextListener;
+import org.heigit.ors.api.util.AppInfo;
 import org.heigit.ors.routing.RoutingProfileManagerStatus;
 import org.heigit.ors.util.StringUtility;
 import org.springframework.boot.SpringApplication;
@@ -14,6 +16,7 @@ import org.springframework.context.annotation.Bean;
 @ServletComponentScan("org.heigit.ors.api.servlet.listeners")
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
+    private static final Logger LOG = Logger.getLogger(Application.class.getName());
 
     static {
         System.setProperty("java.util.logging.manager", "org.apache.logging.log4j.jul.LogManager");
@@ -24,6 +27,7 @@ public class Application extends SpringBootServletInitializer {
             System.setProperty(ORSEnvironmentPostProcessor.ORS_CONFIG_LOCATION_PROPERTY, args[0]);
         }
         SpringApplication.run(Application.class, args);
+        LOG.info("openrouteservice %s".formatted(AppInfo.getEngineInfo()));
         if (RoutingProfileManagerStatus.hasFailed()) {
             System.exit(1);
         }
