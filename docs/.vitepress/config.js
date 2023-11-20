a8be605242c670601cacfe61c63f088c04bd090c
Jakob Schnell <koebi@ezelo.de>
2023-11-20 17:37:46 +0100

    docs: finish sidebar

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -25,7 +25,72 @@ export default defineConfig({
                 text: 'Home', link: '/',
                 items: [
                     {text: 'Getting Started', link: '/getting-started'},
-                    {text: 'Installation and Usage', link: 'installation/Installation-and-Usage'}
+                    {
+                        text: 'Installation and Usage', collapsed: true, link: '/installation/Installation-and-Usage',
+                        items: [
+                            {text: 'Running with Docker', link: '/installation/Running-with-Docker'},
+                            {text: 'Building from Source', link: '/installation/Building-from-Source'},
+                            {text: 'System Requirements', link: '/installation/System-Requirements'},
+                            {text: 'Configuration', link: '/installation/Configuration'},
+                            {text: 'Advanced Docker Setup', link: '/installation/Advanced-Docker-Setup'}
+                        ]
+                    },
+                    {text: 'Data', link: '/Data'},
+                    {
+                        text: 'Contributing', collapsed: true, link: '/contributing/Contributing',
+                        items: [
+                            {text: 'Contributing Translations', link: '/contributing/Contributing-Translations'}
+                        ]
+                    },
+                    {
+                        text: 'Documentation', collapsed: true, link: '/documentation/Documentation',
+                        items: [
+                            {text: 'Tag Filtering', link: '/documentation/Tag-Filtering'},
+                            {
+                                text: 'Travel Speeds',
+                                collapsed: true,
+                                link: '/documentation/travel-speeds/Travel-Speeds',
+                                items: [
+                                    {text: 'Waytype Speeds', link: '/documentation/travel-speeds/Waytype-Speeds'},
+                                    {text: 'Surface Speeds', link: '/documentation/travel-speeds/Surface-Speeds'},
+                                    {text: 'Tracktype Speeds', link: '/documentation/travel-speeds/Tracktype-Speeds'},
+                                    {text: 'Country Speeds', link: '/documentation/travel-speeds/Country-Speeds'}
+                                ]
+                            },
+                            {text: 'Route Attributes', link: '/documentation/Route-Attributes'},
+                            {
+                                text: 'Routing Options',
+                                collapsed: true,
+                                link: '/documentation/routing-options/Routing-Options',
+                                items: [
+                                    {text: 'Examples', link: '/documentation/routing-options/Examples'},
+                                    {text: 'Country List', link: '/documentation/routing-options/Country-List'},
+                                ]
+                            },
+                            {text: 'Instruction Types', link: '/documentation/Instruction-Types'},
+                            {
+                                text: 'Extra Info', collapsed: true, link: '/documentation/extra-info/Extra-Info',
+                                items: [
+                                    {text: 'Steepness', link: '/documentation/extra-info/Steepness'},
+                                    {text: 'Surface', link: '/documentation/extra-info/Surface'},
+                                    {text: 'Waycategory', link: '/documentation/extra-info/Waycategory'},
+                                    {text: 'Waytype', link: '/documentation/extra-info/Waytype'},
+                                    {text: 'Trail Difficulty', link: '/documentation/extra-info/Trail-Difficulty'},
+                                    {
+                                        text: 'Road Access Restrictions',
+                                        link: '/documentation/extra-info/Road-Access-Restrictions'
+                                    }
+                                ]
+                            },
+                            {text: 'Geometry Decoding', link: '/documentation/Geometry-Decoding'},
+                            {text: 'Structured Geocoding Query', link: '/documentation/Structured-Geocoding-Query'},
+                            {text: 'Matrix Response', link: '/documentation/Matrix-Response'},
+                            {text: 'Places Request and Response', link: '/documentation/Places-Request-and-Response'},
+                            {text: 'Geocoding Response', link: '/documentation/Geocoding-Response'},
+                            {text: 'Error Codes', link: '/documentation/Error-Codes'},
+                        ]
+                    },
+                    {text: 'FAQ', link: '/Frequently-Asked-Questions'}
                 ]
             }
         ],
