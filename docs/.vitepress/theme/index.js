81f2780bd06421aa2c33c7a034141525cbbdbff4
rizwan <rizwan@uni-heidelberg.de>
2023-11-21 15:40:39 +0100

    docs: modify font family to reflect current docs

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,5 +1,5 @@
 // .vitepress/theme/index.js
-import DefaultTheme from 'vitepress/theme'
+import DefaultTheme from 'vitepress/theme-without-fonts'
 import './custom.css'
 
 export default DefaultTheme
