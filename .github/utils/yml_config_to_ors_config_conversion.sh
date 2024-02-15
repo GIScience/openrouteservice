1b080cb308561c653a6fb0a1c0aba724e2035851
Amandus Butzer <amandus.butzer@heigit.org>
2024-02-15 13:11:15 +0100

    chore(config-conversion): rework & add properties conversion
    - move application.yml validation to separate file
    - add error exit codes
    - use yq to edit & validate
    - use sed instead of awk (for os compatibility)
    - move comments to echo output
    
    - add a new script to parse application.yml to a properties file
    
    The scripts are using yq to make the rewrite process more robust.
    The less we use sed magic, the better.

--- .github/utils/yml_config_to_ors_config_conversion.sh
+++ .github/utils/yml_config_to_ors_config_conversion.sh
@@ -7,37 +7,36 @@ fi
 
 input_file=$1
 output_file=$2
-ors_file="ors-api/src/test/files/heidelberg.osm.gz"
+
+echo ""
+echo "Copy $input_file to $output_file"
+cp $input_file $output_file
 
 ###########################
-### Validate input file ###
+### Replace parameters ####
 ###########################
-# Fail if ors: can't be found
-echo "Checking for ors section"
-awk '/^ors:/{print "Found ors section"}' "$input_file" || exit 1
+echo ""
+echo "Replace parameters:"
 
-# For engine and source file section, make sure they are in place
-echo "Checking for engine and source_file section"
-awk '/  engine:/{getline; if ($0 ~ /source_file:/) {print "Found engine and source_file section"} else { exit 1 }}' "$input_file" || exit 1
+echo "- enable ors.engine.profiles.car"
+yq -i '.ors.engine.profiles.car.enabled = true' "$output_file" || exit 1
 
-# For profiles section for car wit enabled
-echo "Checking for profiles section"
-awk '/    profiles:/{getline; if ($0 ~ /      car:/) {getline; if ($0 ~ /        enabled:/) {print "Found profiles section"} else { exit 1 }}}' "$input_file" || exit 1
+echo "- set ors.engine.source_file to ors-api/src/test/files/heidelberg.osm.gz"
+yq -i '.ors.engine.source_file = "ors-api/src/test/files/heidelberg.osm.gz"' "$output_file" || exit 1
 
 ###########################
 ### Convert input file ####
 ###########################
-# Add # to the beginning of each line that is not empty or a comment
-awk '!/^[[:space:]]*#/ && !/^[[:space:]]*$/ {print "#" $0; next} {print}' "$input_file" > "$output_file"
+echo ""
+echo "Converting input file:"
+## Add # to the beginning of each line that is not empty or a comment
+echo "- Comment everything"
+sed -i '/^\s*[^#]/ s/^/#/' "$output_file" || exit 1
 
-# Replace '#ors:' with 'ors:'
-sed -i 's/#ors:/ors:/g' "$output_file"
+echo "- Uncomment ors, engine and source_file"
+sed -i -e '/^#ors:/s/^#//' -e '/^#.*engine:/s/^#//' -e '/^#.*source_file:/s/^#//' "$output_file"
 
-
-# Remove the comments for the engine and source_file section
-awk -i inplace "/engine:/{getline; if (\$0 ~ /source_file:/) {print \"  engine:\"; print \"    source_file: '$ors_file'\"; next} else { exit1 }} {print}" "$output_file"
-
-# Remove the comments for the profiles section for car
-awk -i inplace "/#    profiles:/{getline; if (\$0 ~ /#      car:/) {getline; if (\$0 ~ /#        enabled:/) {print \"    profiles:\"; print \"      car:\"; print \"        enabled: true\"; next}}}{print}" "$output_file"
+echo "- Uncomment subsequent lines for profiles.car.enabled in ors.engine"
+sed -i -e '/^#    profiles:/,/^#        enabled:/ s/^#//' "$output_file"
 
 echo "Parsing complete. Result saved to $output_file"
