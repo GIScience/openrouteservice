2240efb9558a63f478028c5e071861f6bbf373da
Jochen Haeussler <jochen.haeussler@heigit.org>
2024-02-21 14:50:13 +0100

    test: add tests with startparam

---
+++ .integration-scenarios/debian-12-jar-mvn/testsuites/jar/test-find-config-prefer-startparam-over-conffile.sh
@@ -0,0 +1,14 @@
+source $TESTFILES_DIR/testfunctions.sh
+
+clearEnvironment
+
+cp $TESTFILES_DIR/config-car.yml $WORK_DIR/ors-config.yml
+
+cd $WORK_DIR
+java -jar $WORK_DIR/ors-api/target/ors.jar $TESTFILES_DIR/config-hgv.yml & # 1>/dev/null 2>&1 &
+
+awaitOrsReady 30
+
+profiles=$(requestEnabledProfiles)
+#stopOrs
+assertEquals "driving-hgv" "$profiles"
