585c25900f88b4a16d6dcfed8024e62c162e1669
Jochen Haeussler <jochen.haeussler@heigit.org>
2024-02-22 20:24:30 +0100

    test: add tests for env variables

---
+++ .integration-scenarios/debian-12-jar-mvn/tests/missing-config-but-profile-enabled-as-env-upper.sh
@@ -0,0 +1,16 @@
+TESTROOT="$( cd "$(dirname "$0")"/.. >/dev/null 2>&1 ; pwd -P )"
+source $TESTROOT/files/testfunctions.sh
+source $TESTROOT/files/test.conf
+prepareTest $1 $(basename $0)
+
+# Even if no yml config file is present, the ors is runnable
+# if at least one routing profile is enabled with a environment variable.
+podman run --replace --name "$CONTAINER" -p $HOST_PORT:8082 \
+  --env ORS_ENGINE_PROFILES_HGV_ENABLED=true \
+  local/"$IMAGE":latest &
+
+awaitOrsReady 60 $HOST_PORT
+profiles=$(requestEnabledProfiles $HOST_PORT)
+podman stop "$CONTAINER"
+
+assertEquals "driving-hgv" "$profiles"
---
+++ .integration-scenarios/debian-12-jar-mvn/tests/specify-yml-prefer-arg-over-env.sh
@@ -0,0 +1,18 @@
+TESTROOT="$( cd "$(dirname "$0")"/.. >/dev/null 2>&1 ; pwd -P )"
+source $TESTROOT/files/testfunctions.sh
+source $TESTROOT/files/test.conf
+prepareTest $1 $(basename $0)
+
+# The profile configured as run argument should be preferred over environment variable.
+podman run --replace --name "$CONTAINER" -p $HOST_PORT:8082 \
+  -v "$TESTROOT"/files/config-car.yml:$WORK_DIR/config-car.yml \
+  -v "$TESTROOT"/files/config-hgv.yml:$WORK_DIR/config-hgv.yml \
+  --env ORS_CONFIG_LOCATION=$WORK_DIR/config-hgv.yml \
+  local/"$IMAGE":latest \
+  $(getProgramArguments $runType $WORK_DIR/config-car.yml) &
+
+awaitOrsReady 60 $HOST_PORT
+profiles=$(requestEnabledProfiles $HOST_PORT)
+podman stop "$CONTAINER"
+
+assertEquals "driving-car" "$profiles"
