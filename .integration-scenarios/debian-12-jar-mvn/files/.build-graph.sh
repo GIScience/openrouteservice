56f8ae823fa37a83e7af8500fd54c188bbdf9a3a
Jochen Haeussler <jochen.haeussler@heigit.org>
2024-02-29 18:32:50 +0100

    test: cleanup

--- .integration-scenarios/debian-12-jar-mvn/files/.build-graph.sh
+++ .integration-scenarios/debian-12-jar-mvn/files/.build-graph.sh
@@ -17,18 +17,11 @@ ors:
       ${enabledProfile}:
         enabled: true")
 
-
-# The start param sets a property, that is not defined in the (loaded) yml config,
-# but is present as internal default. This default should be overridden, but the
-# properties from $CONTAINER_CONF_DIR_USER/ors-config.yml should also be loaded.
 podman run --replace --name "${CONTAINER}" -p "${HOST_PORT}":8082 \
   -v "${M2_FOLDER}":/root/.m2 \
   -v "${TESTROOT}/graphs_volume":"${CONTAINER_WORK_DIR}/graphs" \
   -v "${orsConfig}":"${CONTAINER_WORK_DIR}/ors-config.yml" \
   "local/${IMAGE}:latest" &
-#ok  -Dspring-boot.run.arguments="--ors.engine.profiles.car.enabled=true --ors.engine.source_file=ors-api/src/test/files/heidelberg.osm.gz" &
-#ok:  -Dspring-boot.run.arguments='--ors.engine.profiles.car.enabled=true --ors.engine.source_file=ors-api/src/test/files/heidelberg.osm.gz' &
-#nok:  -Dspring-boot.run.arguments='ors.engine.profiles.car.enabled=true ors.engine.source_file=ors-api/src/test/files/heidelberg.osm.gz' &
 
 awaitOrsReady 60 "${HOST_PORT}"
 profiles=$(requestEnabledProfiles $HOST_PORT)
