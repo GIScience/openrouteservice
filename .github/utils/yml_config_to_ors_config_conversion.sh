2b2cb4e9b257bde5b2aa674fb15e5399c087df6d
Jochen Haeussler <jochen.haeussler@heigit.org>
2024-04-11 19:42:10 +0200

    feat: adapt workflow

--- .github/utils/yml_config_to_ors_config_conversion.sh
+++ .github/utils/yml_config_to_ors_config_conversion.sh
@@ -1,40 +1,50 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 input.yaml output.yaml"
+function checkPropertyEq() {
+  local file=$1
+  local path=$2
+  local expected=$3
+  echo -n "- checking for ${path} = ${expected}:"
+  if [ "$(yq ".${path}" < ${file})" = "${expected}" ]; then
+    echo -e "\e[1;32m ok \e[0m"
+  else
+    echo -e "\e[1;31m error \e[0m"
+    hasError=1
+  fi
+}
+
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 input-1.yaml input-2.yaml output.yaml"
   exit 1
 fi
 
-input_file=$1
-output_file=$2
+input_file1=$1
+input_file2=$2
+output_file=$3
 
 echo ""
-echo "Copy $input_file to $output_file"
-cp $input_file $output_file
+echo "Merge $input_file1 and $input_file2 to $output_file"
+cp $input_file1 $output_file
+yq eval-all '. as $item ireduce ({}; . *+ $item)' "$input_file1" "$input_file2" > "$output_file"
 
 ###########################
 ### Replace parameters ####
 ###########################
+#echo ""
+#echo "Replace parameters:"
+#
+#echo "- set ors.engine.source_file to ors-api/src/test/files/heidelberg.osm.gz"
+#yq -i '.ors.engine.source_file = "ors-api/src/test/files/heidelberg.osm.gz"' "$output_file" || exit 1
+
+############################
+### Validate output file ###
+############################
 echo ""
-echo "Replace parameters:"
+echo "Validate output file:"
+hasError=0
+checkPropertyEq $output_file "ors.engine.source_file" "ors-api/src/test/files/heidelberg.osm.gz"
+checkPropertyEq $output_file "ors.engine.profiles.car.enabled" "true"
+(($hasError)) && exit 1
 
-echo "- set ors.engine.source_file to ors-api/src/test/files/heidelberg.osm.gz"
-yq -i '.ors.engine.source_file = "ors-api/src/test/files/heidelberg.osm.gz"' "$output_file" || exit 1
-
-###########################
-### Convert input file ####
-###########################
 echo ""
-echo "Converting input file:"
-## Add # to the beginning of each line that is not empty or a comment
-echo "- Comment everything"
-sed -i '/^\s*[^#]/ s/^/#/' "$output_file" || exit 1
-
-echo "- Uncomment ors, engine and source_file"
-sed -i -e '/^#ors:/s/^#//' -e '/^#.*engine:/s/^#//' -e '/^#.*source_file:/s/^#//' "$output_file"
-
-echo "- Uncomment subsequent lines for profiles.car.enabled in ors.engine"
-sed -i -e 's/^#    profiles:/    profiles:/' "$output_file"
-sed -i -e 's/^#      car:/      car:\n        enabled: true/' "$output_file"
-
 echo "Parsing complete. Result saved to $output_file"
