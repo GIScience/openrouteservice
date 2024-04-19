13962c83ee0981a732d368fa80cbb9f2d45348fe
Takara Baumbach <takara.baumbach@heigit.org>
2024-04-19 13:47:55 +0200

    fix: config conversion failing due to missing enabled property

--- .github/utils/yml_config_validation.sh
+++ .github/utils/yml_config_validation.sh
@@ -17,5 +17,5 @@ yq 'true' $input_file /dev/null || exit 1
 echo "- checking if ors.engine.source_file exists and has 'source_file' property"
 yq  --exit-status '.ors.engine | has("source_file")' $input_file > /dev/null || exit 1
 # For profiles section for car with enabled using yq and contains
-echo "- checking if ors.engine.profiles.car exists and has 'enabled' property"
-yq  --exit-status '.ors.engine.profiles.car | has("enabled")' $input_file > /dev/null || exit 1
+#echo "- checking if ors.engine.profiles.car exists and has 'enabled' property"
+#yq  --exit-status '.ors.engine.profiles.car | has("enabled")' $input_file > /dev/null || exit 1
