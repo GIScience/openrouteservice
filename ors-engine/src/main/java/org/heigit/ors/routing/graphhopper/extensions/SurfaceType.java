db68a9c4966861316498047520e3f44df2f0d504
aoles <andrzej.oles@gmail.com>
2024-05-22 13:17:33 +0200

    feat: classify ways tagged with `surface=cement` as surface type "Concrete"
    According to OSM wiki `surface=cement` is often used, by mistake, instead of `surface=concrete`.

--- ors-engine/src/main/java/org/heigit/ors/routing/graphhopper/extensions/SurfaceType.java
+++ ors-engine/src/main/java/org/heigit/ors/routing/graphhopper/extensions/SurfaceType.java
@@ -61,7 +61,7 @@ public enum SurfaceType {
             case "paved" -> SurfaceType.PAVED;
             case "unpaved", "woodchips", "rock", "rocks", "stone", "shells", "salt" -> SurfaceType.UNPAVED;
             case "asphalt", "chipseal", "bitmac", "tarmac" -> SurfaceType.ASPHALT;
-            case "concrete" -> SurfaceType.CONCRETE;
+            case "concrete", "cement" -> SurfaceType.CONCRETE;
             case "paving_stones", "paved_stones", "sett", "cobblestone", "unhewn_cobblestone", "bricks", "brick" -> SurfaceType.PAVING_STONE;
             case "metal" -> SurfaceType.METAL;
             case "wood" -> SurfaceType.WOOD;
