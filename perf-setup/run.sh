83b96b2974f503a31ffbfabc47c5fe0951953987
Julian Psotta <julian.psotta@heigit.org>
2024-07-09 11:43:20 +0200

    fix: Fix thread parameters

--- perf-setup/run.sh
+++ perf-setup/run.sh
@@ -22,7 +22,7 @@ do
     echo "Running iteration $i with ${thread} threads"
     ch_threads="ors.engine.profile_default.preparation.methods.ch.threads=${thread}"
     fastisochrones_threads="ors.engine.profile_default.preparation.fastisochrones.threads=${thread}"
-    docker run -it --user $UID -d -p 8082:8082 --env-file ./config.env -e ${ch_threads} -e {fastisochrones_threads} -v ./karlsruhe-regbez-latest.osm.pbf:/home/ors/files/osm.pbf:ro --name ors-instance openrouteservice/openrouteservice:v8.1.0
+    docker run -it --user $UID -d -p 8082:8082 --env-file ./config.env -e "${ch_threads}" -e "${fastisochrones_threads}" -v ./karlsruhe-regbez-latest.osm.pbf:/home/ors/files/osm.pbf:ro --name ors-instance openrouteservice/openrouteservice:v8.1.0
     ../.github/utils/url_check.sh 127.0.0.1 8082 /ors/v2/health 200 1800 10 3
     end_time=$(date +%s)
     iteration_times+="$((end_time - start_time))s "
