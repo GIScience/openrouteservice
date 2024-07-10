b967abf150c0f7b17f28aad45453821f91cee51b
Julian Psotta <julian.psotta@heigit.org>
2024-07-10 15:27:37 +0200

    fix: Adjust script to only do iterations without configuring the threads

--- perf-setup/run.sh
+++ perf-setup/run.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-max_iteration=2
-max_threads=(1 15)
+OSM_FILE=${1:-"./heidelberg.osm.gz"}
+MAX_ITERATIONS=${2:-"2"}
+
 thread_times=()
 
 # trap ctrl-c and call ctrl_c()
@@ -13,24 +14,18 @@ function ctrl_c() {
         exit 1
 }
 
-for thread in "${max_threads[@]}"
-do
-  iteration_times=()
-  for i in $(seq 1 $max_iteration); do
-    start_time=$(date +%s)
-    docker rm --force ors-instance
-    echo "Running iteration $i with ${thread} threads"
-    ch_threads="ors.engine.profile_default.preparation.methods.ch.threads=${thread}"
-    ch_threads_car="ors.engine.profiles.bike-regular.preparation.methods.ch.threads=${thread}"
-    fastisochrones_threads="ors.engine.profile_default.preparation.fastisochrones.threads=${thread}"
-    fastisochrones_threads_car="ors.engine.profiles.car.preparation.methods.fastisochrones.threads=${thread}"
-    docker run -it --user $UID -d -p 8082:8082 --env-file ./config.env -e "${fastisochrones_threads_car}" -e "${ch_threads_car}" -v ./heidelberg.osm.gz:/home/ors/files/osm.pbf:ro --name ors-instance openrouteservice/openrouteservice:v8.1.0
-    ../.github/utils/url_check.sh 127.0.0.1 8082 /ors/v2/health 200 1800 10 3
-    end_time=$(date +%s)
-    iteration_times+="$((end_time - start_time))s "
-  done
-  thread_times+=("Threads: ${thread} [${iteration_times[@]}]")
+docker build -t openrouteservice:local -f ../Dockerfile ..
+
+for i in $(seq 1 "$MAX_ITERATIONS"); do
+  start_time=$(date +%s)
+  docker rm --force ors-instance || true
+  echo "Starting ORS instance with $i iterations"
+  docker run -it --user $UID -d -p 8082:8082 --env-file ./config.env -v "${OSM_FILE}":/home/ors/files/osm.pbf:ro --name ors-instance openrouteservice:local
+  ../.github/utils/url_check.sh 127.0.0.1 8082 /ors/v2/health 200 1800 10 3
+  end_time=$(date +%s)
+  iteration_times+="$((end_time - start_time))s "
 done
+thread_times+=("Iteration timings: ${thread} [${iteration_times[@]}]")
 
 docker rm --force ors-instance
 
