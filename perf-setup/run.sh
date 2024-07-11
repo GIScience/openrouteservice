0a7b6c1910cac8ac92feb4ac171bc9029dd5ea56
Julian Psotta <julian.psotta@heigit.org>
2024-07-11 13:53:59 +0200

    fix: Remove custom jar file and add a backup line
    Defining volumes through variables isn't trivial.

--- perf-setup/run.sh
+++ perf-setup/run.sh
@@ -3,7 +3,6 @@
 OSM_FILE=${1:-"./heidelberg.osm.gz"}
 MAX_ITERATIONS=${2:-"2"}
 XMX=${3:-"5G"}
-JAR_FILE=${4}
 
 thread_times=()
 
@@ -18,18 +17,14 @@ function ctrl_c() {
 
 docker build -t openrouteservice:local -f ../Dockerfile ..
 
-jar_volume=""
-# Check JAR_FILE is not empty
-if [ -n "$JAR_FILE" ]; then
-  jar_volume="-v ${JAR_FILE}:/ors.jar:ro"
-fi
 
 echo "Starting ORS instance with $MAX_ITERATIONS iterations"
 for i in $(seq 1 "$MAX_ITERATIONS"); do
   echo "Iteration $i"
   start_time=$(date +%s)
   docker rm --force ors-instance || true
-  docker run -it --user $UID -d -p 8082:8082 --env-file ./config.env -e XMX="${XMX}" "${jar_volume}" -v "${OSM_FILE}":/home/ors/files/osm.pbf:ro --name ors-instance openrouteservice:local
+#  docker run -it --user $UID -d -p 8082:8082 --env-file ./config.env -e XMX="${XMX}" -v ./ors.jar:/ors.jar -v "${OSM_FILE}":/home/ors/files/osm.pbf:ro --name ors-instance openrouteservice:local
+  docker run -it --user $UID -d -p 8082:8082 --env-file ./config.env -e XMX="${XMX}" -v "${OSM_FILE}":/home/ors/files/osm.pbf:ro --name ors-instance openrouteservice:local
   ../.github/utils/url_check.sh 127.0.0.1 8082 /ors/v2/health 200 1800 10 3
   end_time=$(date +%s)
   iteration_times+="$((end_time - start_time))s "
