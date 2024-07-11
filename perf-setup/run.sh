c516965f38bc41ac6a54bc25358a588769889176
Julian Psotta <julian.psotta@heigit.org>
2024-07-11 13:42:46 +0200

    feat: Add feature to define xmx and a custom jar file

--- perf-setup/run.sh
+++ perf-setup/run.sh
@@ -2,6 +2,8 @@
 
 OSM_FILE=${1:-"./heidelberg.osm.gz"}
 MAX_ITERATIONS=${2:-"2"}
+XMX=${3:-"5G"}
+JAR_FILE=${4}
 
 thread_times=()
 
@@ -16,12 +18,18 @@ function ctrl_c() {
 
 docker build -t openrouteservice:local -f ../Dockerfile ..
 
+jar_volume=""
+# Check JAR_FILE is not empty
+if [ -n "$JAR_FILE" ]; then
+  jar_volume="-v ${JAR_FILE}:/ors.jar:ro"
+fi
+
 echo "Starting ORS instance with $MAX_ITERATIONS iterations"
 for i in $(seq 1 "$MAX_ITERATIONS"); do
   echo "Iteration $i"
   start_time=$(date +%s)
   docker rm --force ors-instance || true
-  docker run -it --user $UID -d -p 8082:8082 --env-file ./config.env -v "${OSM_FILE}":/home/ors/files/osm.pbf:ro --name ors-instance openrouteservice:local
+  docker run -it --user $UID -d -p 8082:8082 --env-file ./config.env -e XMX="${XMX}" "${jar_volume}" -v "${OSM_FILE}":/home/ors/files/osm.pbf:ro --name ors-instance openrouteservice:local
   ../.github/utils/url_check.sh 127.0.0.1 8082 /ors/v2/health 200 1800 10 3
   end_time=$(date +%s)
   iteration_times+="$((end_time - start_time))s "
