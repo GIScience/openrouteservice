7df9b3300a184ee4d32523eaf155bcc9871bd2f2
Julian Psotta <julian.psotta@heigit.org>
2024-07-19 10:25:26 +0200

    fix: Remove unneeded tests for GreenIndex
    These are not necessary anymore, because the ExtendedStorageIndex covers the index special cases

--- ors-api/src/main/java/org/heigit/ors/api/config/ExtendedStorageGreenIndex.java
+++ ors-api/src/main/java/org/heigit/ors/api/config/ExtendedStorageGreenIndex.java
@@ -1,35 +1,11 @@
 package org.heigit.ors.api.config;
 
 import com.fasterxml.jackson.annotation.*;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import org.heigit.ors.api.converters.PathDeserializer;
-import org.heigit.ors.api.converters.PathSerializer;
-
-import java.nio.file.Path;
 
 @JsonTypeName("GreenIndex")
-public class ExtendedStorageGreenIndex extends ExtendedStorage {
-
-    private Path filepath = Path.of("");
+public class ExtendedStorageGreenIndex extends ExtendedStorageIndex {
 
     public ExtendedStorageGreenIndex() {
-    }
-
-    @JsonProperty("filepath")
-    @JsonSerialize(using = PathSerializer.class)
-    public Path getFilepath() {
-        return this.filepath;
-    }
-
-
-    @JsonSetter("filepath")
-    @JsonDeserialize(using = PathDeserializer.class)
-    // Todo deserializer should output absolute paths to reduce complexity
-    public void setFilepath(Path filepath) {
-        // Todo: Decide if we want to use realpath or absolute path only. Realpath could check if the file exists.
-        if (filepath != null && !filepath.toString().isEmpty())
-            this.filepath = filepath.toAbsolutePath();
     }
 
 }
--- ors-api/src/test/java/org/heigit/ors/api/config/ExtendedStorageGreenIndexTest.java
+++ ors-api/src/test/java/org/heigit/ors/api/config/ExtendedStorageGreenIndexTest.java
@@ -13,14 +13,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class ExtendedStorageGreenIndexTest {
 
     @Test
-    void testDefaultConstructor() {
-        ExtendedStorageGreenIndex storage = new ExtendedStorageGreenIndex();
-        assertNotNull(storage, "Default constructor should initialize the object");
-        assertTrue(storage.getEnabled(), "Default constructor should initialize 'enabled' to true");
-        assertEquals("", storage.getFilepath().toString(), "Default constructor should initialize 'filepath' to an empty string");
-    }
-
-    @Test
     void testDeSerializationCorrectJson() throws Exception {
         ObjectMapper objectMapper = new ObjectMapper();
         String json = "{\"GreenIndex\":{\"enabled\":true,\"filepath\":\"" + Paths.get("src/test/resources/index.csv") + "\"}}";
@@ -38,25 +30,5 @@ class ExtendedStorageGreenIndexTest {
         storage.setFilepath(Paths.get(path));
         String json = objectMapper.writeValueAsString(storage);
         assertEquals(actualJson, json, "Serialized JSON should have 'enabled' set to true and 'filepath' set to an absolute path");
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-            ", ''", // Represents the default path for null input
-            "'', ''", // Represents the empty string input
-            "'src/test/resources', 'src/test/resources'"
-    })
-    void testSetFilepath(String input, String expected) {
-        ExtendedStorageGreenIndex storage = new ExtendedStorageGreenIndex();
-        if (input != null)
-            storage.setFilepath(Paths.get(input));
-        else
-            storage.setFilepath(null);
-
-        if (input == null || input.isEmpty()) {
-            assertEquals(Path.of(""), storage.getFilepath(), "setFilepath(null) should result in an empty path");
-        } else {
-            assertEquals(Paths.get(expected).toAbsolutePath(), storage.getFilepath(), "setFilepath('src/test/resources') should result in an absolute path");
-        }
     }
 }
