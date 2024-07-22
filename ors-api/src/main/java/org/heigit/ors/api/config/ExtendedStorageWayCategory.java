924c1d9613ebb52898406cf3fdf5f56558ce7e8e
Julian Psotta <julian.psotta@heigit.org>
2024-07-22 16:25:54 +0200

    chore: Cleanup unused code

--- ors-api/src/main/java/org/heigit/ors/api/config/ExtendedStorageWayCategory.java
+++ ors-api/src/main/java/org/heigit/ors/api/config/ExtendedStorageWayCategory.java
@@ -1,9 +1,6 @@
 package org.heigit.ors.api.config;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
-
-import java.util.LinkedHashMap;
 
 @JsonTypeName("WayCategory")
 public class ExtendedStorageWayCategory extends ExtendedStorage {
