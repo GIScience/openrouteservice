6a506f81b988af480dd453ddcb9a751a1f3146e3
Julian Psotta <julian.psotta@heigit.org>
2024-07-23 16:43:32 +0200

    fix: Repair the broken subtype hierarchy

--- ors-api/src/main/java/org/heigit/ors/api/config/ExtendedStorage.java
+++ ors-api/src/main/java/org/heigit/ors/api/config/ExtendedStorage.java
@@ -7,30 +7,30 @@ import org.heigit.ors.api.converters.PathDeserializer;
 import org.heigit.ors.api.converters.PathSerializer;
 
 import java.nio.file.Path;
-import java.util.LinkedHashMap;
 
 /**
  * This the base class for the extended storage configuration. It contains a boolean field to enable or disable the storage.
  */
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.WRAPPER_OBJECT)
-public class ExtendedStorage {
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = ExtendedStorageWayCategory.class, name = "WayCategory"),
+        @JsonSubTypes.Type(value = ExtendedStorageHeavyVehicle.class, name = "HeavyVehicle"),
+        @JsonSubTypes.Type(value = ExtendedStorageHereTraffic.class, name = "HereTraffic"),
+        @JsonSubTypes.Type(value = ExtendedStorageHillIndex.class, name = "HillIndex"),
+        @JsonSubTypes.Type(value = ExtendedStorageOsmId.class, name = "OsmId"),
+        @JsonSubTypes.Type(value = ExtendedStorageBorders.class, name = "Borders"),
+        @JsonSubTypes.Type(value = ExtendedStorageRoadAccessRestrictions.class, name = "RoadAccessRestrictions"),
+        @JsonSubTypes.Type(value = ExtendedStorageTrailDifficulty.class, name = "TrailDifficulty"),
+        @JsonSubTypes.Type(value = ExtendedStorageWheelchair.class, name = "Wheelchair"),
+        @JsonSubTypes.Type(value = ExtendedStorageWaySurfaceType.class, name = "WaySurfaceType"),
+})
+public abstract class ExtendedStorage {
 
     @JsonProperty
     private Boolean enabled = true;
 
+    @JsonCreator
     public ExtendedStorage() {
-    }
-
-    @JsonCreator
-    // Scenarios where the storage is empty "WayCategory:"
-    public ExtendedStorage(String ignoredEmpty) {
-        super();
-    }
-
-    @JsonCreator
-    // Scenarios where the storage is empty with curly braces "WayCategory: {}"
-    public ExtendedStorage(LinkedHashMap<String, Object> ignoredMap) {
-        super();
     }
 
     @JsonProperty("enabled")
@@ -39,7 +39,7 @@ public class ExtendedStorage {
     }
 
     @JsonSetter("enabled")
-    private void setEnabled(Boolean enabled) {
+    void setEnabled(Boolean enabled) {
         this.enabled = enabled;
     }
 }
@@ -48,7 +48,14 @@ public class ExtendedStorage {
 /**
  * This extends the ExtendedStorage class and adds a filepath field for the Index storages such as GreenIndex and NoiseIndex.
  */
-class ExtendedStorageIndex extends ExtendedStorage {
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.WRAPPER_OBJECT)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = ExtendedStorageNoiseIndex.class, name = "NoiseIndex"),
+        @JsonSubTypes.Type(value = ExtendedStorageCsvIndex.class, name = "csv"),
+        @JsonSubTypes.Type(value = ExtendedStorageGreenIndex.class, name = "GreenIndex"),
+        @JsonSubTypes.Type(value = ExtendedStorageShadowIndex.class, name = "ShadowIndex"),
+})
+abstract class ExtendedStorageIndex extends ExtendedStorage {
 
     private Path filepath = Path.of("");
 
