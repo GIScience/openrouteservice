f9a202aef2de24f763c34b14ca97bb59f0675ea4
Julian Psotta <julian.psotta@heigit.org>
2024-07-26 11:03:08 +0200

    feat: Introduce EncoderNameEnum

--- ors-engine/src/main/java/org/heigit/ors/config/profile/defaults/BikeRoadProfileProperties.java
+++ ors-engine/src/main/java/org/heigit/ors/config/profile/defaults/BikeRoadProfileProperties.java
@@ -1,5 +1,6 @@
 package org.heigit.ors.config.profile.defaults;
 
+import org.heigit.ors.common.EncoderNameEnum;
 import org.heigit.ors.config.profile.ProfileProperties;
 import org.heigit.ors.config.profile.storages.ExtendedStorageHillIndex;
 import org.heigit.ors.config.profile.storages.ExtendedStorageTrailDifficulty;
@@ -9,7 +10,7 @@ import org.heigit.ors.config.profile.storages.ExtendedStorageWaySurfaceType;
 public class BikeRoadProfileProperties extends ProfileProperties {
     public BikeRoadProfileProperties() {
         super();
-        this.setEncoderName("cycling-road");
+        this.setEncoderName(EncoderNameEnum.CYCLING_ROAD);
         getEncoderOptions().setConsiderElevation(true);
         getEncoderOptions().setTurnCosts(true);
         getEncoderOptions().setBlockFords(false);
--- ors-engine/src/main/java/org/heigit/ors/config/profile/defaults/PublicTransportProfileProperties.java
+++ ors-engine/src/main/java/org/heigit/ors/config/profile/defaults/PublicTransportProfileProperties.java
@@ -1,5 +1,6 @@
 package org.heigit.ors.config.profile.defaults;
 
+import org.heigit.ors.common.EncoderNameEnum;
 import org.heigit.ors.config.profile.ProfileProperties;
 
 import java.nio.file.Path;
@@ -7,7 +8,7 @@ import java.nio.file.Path;
 public class PublicTransportProfileProperties extends ProfileProperties {
     public PublicTransportProfileProperties() {
         super();
-        this.setEncoderName("public-transport");
+        this.setEncoderName(EncoderNameEnum.PUBLIC_TRANSPORT);
         getEncoderOptions().setBlockFords(false);
         setElevation(true);
         setMaximumVisitedNodes(1000000);
