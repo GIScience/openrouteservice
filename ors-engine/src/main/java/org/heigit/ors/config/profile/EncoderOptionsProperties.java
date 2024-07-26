653a198f2007779afe33827a05269b3aa89b361c
Julian Psotta <julian.psotta@heigit.org>
2024-07-27 01:31:26 +0200

    chore: Remove unneeded file and remove unused logger

--- ors-engine/src/main/java/org/heigit/ors/config/profile/EncoderOptionsProperties.java
+++ ors-engine/src/main/java/org/heigit/ors/config/profile/EncoderOptionsProperties.java
@@ -1,13 +1,10 @@
 package org.heigit.ors.config.profile;
 
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
@@ -15,8 +12,6 @@ import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 @Setter
 @JsonInclude(NON_NULL)
 public class EncoderOptionsProperties {
-    @JsonIgnore
-    Logger logger = LoggerFactory.getLogger(EncoderOptionsProperties.class);
     @JsonProperty("block_fords")
     private Boolean blockFords = null;
     @JsonProperty("consider_elevation")
