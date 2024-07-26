8fa0ab71b901bd46739cee7fd4c45e8068d25555
Julian Psotta <julian.psotta@heigit.org>
2024-07-27 00:45:19 +0200

    feat: Add deep copy function for properties

--- ors-engine/src/main/java/org/heigit/ors/config/profile/ProfileProperties.java
+++ ors-engine/src/main/java/org/heigit/ors/config/profile/ProfileProperties.java
@@ -146,7 +146,7 @@ public abstract class ProfileProperties {
     @JsonSetter("encoder_options")
     public void setEncoderOptions(EncoderOptionsProperties encoderOptions) {
         DefaultEncoderOptionsProperties defaultEncoderOptionsProperties = new DefaultEncoderOptionsProperties(this.getEncoderName());
-        encoderOptions = (EncoderOptionsProperties) PropertyUtils.copyObjectProperties(defaultEncoderOptionsProperties, encoderOptions, false);
+        encoderOptions = (EncoderOptionsProperties) PropertyUtils.copyObjectPropertiesDeep(defaultEncoderOptionsProperties, encoderOptions, false);
         this.encoderOptions = encoderOptions;
     }
 
