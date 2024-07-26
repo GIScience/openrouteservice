6d04499fb9662650ddf8adf6663450ce6e4767ec
Julian Psotta <julian.psotta@heigit.org>
2024-07-27 00:45:19 +0200

    feat: Add deep copy function for properties

--- ors-engine/src/main/java/org/heigit/ors/config/utils/PropertyUtils.java
+++ ors-engine/src/main/java/org/heigit/ors/config/utils/PropertyUtils.java
@@ -6,7 +6,8 @@ import org.slf4j.LoggerFactory;
 import java.lang.reflect.Field;
 
 public class PropertyUtils {
-    public static Object copyObjectProperties(Object source, Object target, boolean overwrite) {
+    public static Object copyObjectPropertiesDeep(Object source, Object target, boolean overwrite) {
+        // If a field is a class, it will be copied recursively
         Logger logger = LoggerFactory.getLogger(Object.class);
         if (source == null || target == null) {
             throw new IllegalArgumentException("Source and target objects must not be null");
@@ -25,15 +26,18 @@ public class PropertyUtils {
             } catch (IllegalAccessException | IllegalArgumentException e) {
                 logger.warn("Could not access field: {}", field.getName());
             }
-            if (value != null) {
-                try {
-                    Object currentValue = field.get(target);
-                    if (overwrite || currentValue == null || (currentValue instanceof String && ((String) currentValue).isEmpty())) {
-                        field.set(target, value);
-                    }
-                } catch (IllegalAccessException e) {
-                    logger.warn("Could not set field: {}", field.getName());
+            if (value == null) {
+                continue;
+            }
+            try {
+                Object currentValue = field.get(target);
+                if (field.getType().isMemberClass()) {
+                    field.set(target, copyObjectPropertiesDeep(value, currentValue, overwrite));
+                } else if (overwrite || currentValue == null || (currentValue instanceof String && ((String) currentValue).isEmpty())) {
+                    field.set(target, value);
                 }
+            } catch (IllegalAccessException e) {
+                logger.warn("Could not set field: {}", field.getName());
             }
         }
         return target;
--- ors-engine/src/test/java/org/heigit/ors/config/utils/PropertyUtilsTest.java
+++ ors-engine/src/test/java/org/heigit/ors/config/utils/PropertyUtilsTest.java
@@ -10,43 +10,73 @@ class PropertyUtilsTest {
 
     @Test
     void testUpdateObject() {
-        TestProperty source = new TestProperty("testValue", 42, "foo");
-        TestProperty target = new TestProperty("bar", 0, "bar");
-        PropertyUtils.copyObjectProperties(source, target, true);
+        TestProperty source = new TestProperty("testValue", 42, "foo", true);
+        TestProperty target = new TestProperty("bar", 0, "bar", false);
+        PropertyUtils.copyObjectPropertiesDeep(source, target, true);
 
         assertEquals("testValue", target.field1);
         assertEquals(42, target.getField2());
         assertEquals("foo", target.getField3());
+        assertEquals(true, target.subclass.enabled);
+    }
+
+    @Test
+    void testUpdateObjectNoOverwrite() {
+        TestProperty source = new TestProperty("testValue", 42, "foo", true);
+        TestProperty target = new TestProperty("bar", 0, null, false);
+        PropertyUtils.copyObjectPropertiesDeep(source, target, false);
+
+        assertEquals("bar", target.field1);
+        assertEquals(0, target.getField2());
+        // Correctly set as the target field is null.
+        assertEquals("foo", target.getField3());
+        assertEquals(false, target.subclass.enabled);
+    }
+
+    @Test
+    void testUpdateObjectNoOverwriteInSubclass() {
+        TestProperty source = new TestProperty("testValue", 42, "foo", true);
+        TestProperty target = new TestProperty("bar", 0, "baz", null);
+        PropertyUtils.copyObjectPropertiesDeep(source, target, false);
+
+        assertEquals("bar", target.field1);
+        assertEquals(0, target.getField2());
+        assertEquals("baz", target.getField3());
+        // Correctly set as the target field is null.
+        assertEquals(true, target.subclass.enabled);
     }
 
     @Test
     void testUpdateWithSuperclass() {
-        TestProperty source = new TestProperty("testValue", 42, "baz");
-        TestPropertySubclass target = new TestPropertySubclass("foo", 0, "bar");
-        PropertyUtils.copyObjectProperties(source, target, true);
+        // This is not working! The subclass is not updated by the superclass.
+        TestProperty source = new TestProperty("testValue", 42, "baz", true);
+        TestPropertySubclass target = new TestPropertySubclass("foo", 0, "bar", false);
+        PropertyUtils.copyObjectPropertiesDeep(source, target, true);
 
         assertEquals("foo", target.field1);
         assertEquals(0, target.getField2());
         assertEquals("bar", target.getField3());
+        assertEquals(false, target.subclass.enabled);
     }
 
     @Test
     void testUpdateWithSubclass() {
-        TestPropertySubclass source = new TestPropertySubclass("foo", 0, "bar");
-        TestProperty target = new TestProperty("testValue", 42, "baz");
-        PropertyUtils.copyObjectProperties(source, target, true);
+        TestPropertySubclass source = new TestPropertySubclass("foo", 0, "bar", true);
+        TestProperty target = new TestProperty("testValue", 42, "baz", false);
+        PropertyUtils.copyObjectPropertiesDeep(source, target, true);
 
         assertEquals("foo", target.field1);
         assertEquals(0, target.getField2());
         assertEquals("bar", target.getField3());
+        assertEquals(true, target.subclass.enabled);
     }
 
     @Test
     void testUpdateObjectWithNullSource() {
-        TestProperty target = new TestProperty("testValue", 42, "foo");
-        assertThrows(IllegalArgumentException.class, () -> PropertyUtils.copyObjectProperties(null, null, true));
-        assertThrows(IllegalArgumentException.class, () -> PropertyUtils.copyObjectProperties(target, null, true));
-        assertThrows(IllegalArgumentException.class, () -> PropertyUtils.copyObjectProperties(null, target, true));
+        TestProperty target = new TestProperty("testValue", 42, "foo", true);
+        assertThrows(IllegalArgumentException.class, () -> PropertyUtils.copyObjectPropertiesDeep(null, null, true));
+        assertThrows(IllegalArgumentException.class, () -> PropertyUtils.copyObjectPropertiesDeep(target, null, true));
+        assertThrows(IllegalArgumentException.class, () -> PropertyUtils.copyObjectPropertiesDeep(null, target, true));
     }
 
     class TestProperty {
@@ -55,18 +85,33 @@ class PropertyUtilsTest {
         public String field1;
         @Getter
         public int field2;
+        TestPropertyNestedClass subclass;
 
-        TestProperty(String field1, int field2, String field3) {
+        TestProperty(String field1, int field2, String field3, Boolean enabled) {
             this.field1 = field1;
             this.field2 = field2;
             this.field3 = field3;
+            this.subclass = new TestPropertyNestedClass(enabled);
+        }
+
+        public static class TestPropertyNestedClass {
+            Boolean enabled;
+
+            TestPropertyNestedClass(Boolean enabled) {
+                ;
+                this.enabled = enabled;
+            }
         }
     }
 
     class TestPropertySubclass extends TestProperty {
-        TestPropertySubclass(String field1, int field2, String field3) {
-            super(field1, field2, field3);
+
+
+        TestPropertySubclass(String field1, int field2, String field3, Boolean enabled) {
+            super(field1, field2, field3, enabled);
+            this.subclass = new TestPropertyNestedClass(enabled);
         }
+
     }
 
 }
