0d5cf75a6bb85097a0524385cf19a95908babfbe
Julian Psotta <julian.psotta@heigit.org>
2024-07-28 20:35:43 +0200

    fix: Public Transit default config cleanup
    Exclude ext_storages from the public transport config and cleanup an old config setter.

--- ors-engine/src/main/java/org/heigit/ors/config/profile/defaults/PublicTransportProfileProperties.java
+++ ors-engine/src/main/java/org/heigit/ors/config/profile/defaults/PublicTransportProfileProperties.java
@@ -1,15 +1,16 @@
 package org.heigit.ors.config.profile.defaults;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.heigit.ors.common.EncoderNameEnum;
 import org.heigit.ors.config.profile.ProfileProperties;
 
 import java.nio.file.Path;
 
+@JsonIgnoreProperties({"ext_storages"})
 public class PublicTransportProfileProperties extends ProfileProperties {
     public PublicTransportProfileProperties() {
         super();
         this.setEncoderName(EncoderNameEnum.PUBLIC_TRANSPORT);
-        getEncoderOptions().setBlockFords(false);
         setElevation(true);
         setMaximumVisitedNodes(1000000);
         setGtfsFile(Path.of(""));
