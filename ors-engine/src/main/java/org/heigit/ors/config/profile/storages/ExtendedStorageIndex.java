3bc07864e38b24276cffdccdedd8336db866235c
Julian Psotta <julian.psotta@heigit.org>
2024-08-12 23:53:16 +0200

    feat: Add copyProperties for ExtendedStorageIndex

--- ors-engine/src/main/java/org/heigit/ors/config/profile/storages/ExtendedStorageIndex.java
+++ ors-engine/src/main/java/org/heigit/ors/config/profile/storages/ExtendedStorageIndex.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.EqualsAndHashCode;
 import org.heigit.ors.config.utils.PathDeserializer;
 import org.heigit.ors.config.utils.PathSerializer;
 
@@ -21,6 +22,7 @@ import java.nio.file.Path;
         @JsonSubTypes.Type(value = ExtendedStorageGreenIndex.class, name = "GreenIndex"),
         @JsonSubTypes.Type(value = ExtendedStorageShadowIndex.class, name = "ShadowIndex"),
 })
+@EqualsAndHashCode(callSuper = false)
 public abstract class ExtendedStorageIndex extends ExtendedStorage {
 
     private Path filepath = Path.of("");
@@ -40,6 +42,25 @@ public abstract class ExtendedStorageIndex extends ExtendedStorage {
     private void setFilepath(Object filepath) {
         if (filepath instanceof Path objectCast) {
             this.filepath = objectCast;
+        }
+    }
+
+
+    @Override
+    public void copyProperties(ExtendedStorage value, boolean overwrite) {
+        super.copyProperties(value, overwrite);
+        if (value == null) {
+            return;
+        }
+        if (value instanceof ExtendedStorageIndex storage) {
+            Path emptyPath = Path.of("");
+            if (this.getFilepath().equals(emptyPath)) {
+                this.setFilepath(storage.getFilepath());
+            } else {
+                if (!storage.getFilepath().equals(emptyPath) && overwrite) {
+                    this.setFilepath(storage.getFilepath());
+                }
+            }
         }
     }
 }
--- ors-engine/src/test/java/org/heigit/ors/config/profile/storages/ExtendedStorageIndexTest.java
+++ ors-engine/src/test/java/org/heigit/ors/config/profile/storages/ExtendedStorageIndexTest.java
@@ -1,6 +1,8 @@
 package org.heigit.ors.config.profile.storages;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Paths;
@@ -9,6 +11,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 class ExtendedStorageIndexTest {
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    ExtendedStorageIndex source;
+    ExtendedStorageIndex target;
+
+    @BeforeEach
+    void setUp() throws JsonProcessingException {
+        String json1 = "{\"GreenIndex\":{\"enabled\":false,\"filepath\": \"" + Paths.get("src/test/resources/index.csv") + "\"}}";
+        String json2 = "{\"GreenIndex\":{\"enabled\":true,\"filepath\": \"" + Paths.get("other/path/index.csv") + "\"}}";
+        source = objectMapper.readValue(json1, ExtendedStorageIndex.class);
+        target = objectMapper.readValue(json2, ExtendedStorageIndex.class);
+    }
 
     @Test
     void defaultConstructorInitializesObject() {
@@ -34,6 +48,43 @@ class ExtendedStorageIndexTest {
         ExtendedStorageIndex storage = objectMapper.readValue(json, ExtendedStorageIndex.class);
         assertFalse(storage.getEnabled(), "Deserialized object should have 'enabled' set to false");
         assertEquals(Paths.get(""), storage.getFilepath(), "Deserialized object should have 'filepath' set to an empty string");
+    }
+
+    @Test
+    void copyPropertiesWithoutOverwrite() {
+        assertNotEquals(source, target);
+        target.copyProperties(source, false);
+        assertNotEquals(source.getEnabled(), target.getEnabled(), "Enabled should not be copied when overwrite is false");
+    }
+
+    @Test
+    void copyPropertiesWithOverwrite() {
+        assertNotEquals(source, target);
+        target.copyProperties(source, true);
+        assertEquals(source.getEnabled(), target.getEnabled(), "Enabled should be copied when overwrite is true");
+    }
+
+    @Test
+    void copyPropertiesWithNullSource() {
+        target.copyProperties(null, true);
+        assertTrue(target.getEnabled(), "Enabled should remain unchanged when source is null");
+    }
+
+    @Test
+    void copyPropertiesWithEmptySource() throws JsonProcessingException {
+        String emptyJson = "{\"GreenIndex\":\"\"}";
+        ExtendedStorageIndex source = objectMapper.readValue(emptyJson, ExtendedStorageIndex.class);
+        assertNotEquals(source, target);
+        target.copyProperties(source, true);
+        assertEquals(source.getEnabled(), target.getEnabled(), "Enabled should be copied from an empty source when overwrite is true");
+    }
+
+    @Test
+    void copyPropertiesWithEmptyTarget() throws JsonProcessingException {
+        String emptyJson = "{\"GreenIndex\":\"\"}";
+        ExtendedStorageIndex target = objectMapper.readValue(emptyJson, ExtendedStorageIndex.class);
+        target.copyProperties(source, true);
+        assertEquals(source.getEnabled(), target.getEnabled(), "Enabled should be copied from an empty source when overwrite is true");
     }
 
     class HelperClass extends ExtendedStorageIndex {
