9bb75095160d722b60f0a48858362d721301d26a
Julian Psotta <julian.psotta@heigit.org>
2024-08-12 19:29:39 +0200

    feat: Add copyProperties for ElevationProperties

--- ors-engine/src/main/java/org/heigit/ors/config/ElevationProperties.java
+++ ors-engine/src/main/java/org/heigit/ors/config/ElevationProperties.java
@@ -1,5 +1,6 @@
 package org.heigit.ors.config;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
@@ -31,5 +32,52 @@ public class ElevationProperties {
 
     public Boolean isPreprocessed() {
         return preprocessed;
+    }
+
+    @JsonIgnore
+    public void copyProperties(ElevationProperties source, boolean overwrite) {
+        if (source == null) {
+            return;
+        }
+
+        if (this.getPreprocessed() == null) {
+            this.setPreprocessed(source.getPreprocessed());
+        } else {
+            if (source.getPreprocessed() != null && overwrite) {
+                this.setPreprocessed(source.getPreprocessed());
+            }
+        }
+
+        if (this.getDataAccess() == null) {
+            this.setDataAccess(source.getDataAccess());
+        } else {
+            if (source.getDataAccess() != null && overwrite) {
+                this.setDataAccess(source.getDataAccess());
+            }
+        }
+
+        if (this.getCacheClear() == null) {
+            this.setCacheClear(source.getCacheClear());
+        } else {
+            if (source.getCacheClear() != null && overwrite) {
+                this.setCacheClear(source.getCacheClear());
+            }
+        }
+
+        if (this.getProvider() == null) {
+            this.setProvider(source.getProvider());
+        } else {
+            if (source.getProvider() != null && overwrite) {
+                this.setProvider(source.getProvider());
+            }
+        }
+
+        if (this.getCachePath() == null) {
+            this.setCachePath(source.getCachePath());
+        } else {
+            if (source.getCachePath() != null && overwrite) {
+                this.setCachePath(source.getCachePath());
+            }
+        }
     }
 }
--- ors-engine/src/test/java/org/heigit/ors/config/ElevationPropertiesTest.java
+++ ors-engine/src/test/java/org/heigit/ors/config/ElevationPropertiesTest.java
@@ -84,4 +84,94 @@ class ElevationPropertiesTest {
         elevationProperties.setCachePath(null);
         assertNull(elevationProperties.getCachePath());
     }
+
+    @Test
+    void copyPropertiesToEmptyTarget() {
+        ElevationProperties source = new ElevationProperties();
+        ElevationProperties target = new ElevationProperties();
+
+        source.setPreprocessed(true);
+        source.setDataAccess(DataAccessEnum.MMAP);
+        source.setCacheClear(true);
+        source.setProvider("provider1");
+        source.setCachePath(Paths.get("/some/path"));
+
+        // Prepare and check target
+        target.setPreprocessed(false);
+        assertNull(target.getDataAccess());
+        assertNull(target.getCacheClear());
+        assertNull(target.getProvider());
+        assertNull(target.getCachePath());
+
+        // Copy properties
+        target.copyProperties(source, false);
+
+        // Check target properties
+        assertNotEquals(source, target);
+        assertEquals(false, target.isPreprocessed());
+
+        // Set null again to allow for a copy
+        target.setPreprocessed(null);
+
+        // Copy properties again
+        target.copyProperties(source, false);
+
+        assertEquals(source.getPreprocessed(), target.getPreprocessed());
+        assertEquals(source.getDataAccess(), target.getDataAccess());
+        assertEquals(source.getCacheClear(), target.getCacheClear());
+        assertEquals(source.getProvider(), target.getProvider());
+        assertEquals(source.getCachePath(), target.getCachePath());
+
+        // Copy properties again with overwrite
+        target.copyProperties(source, true);
+
+        // Check source and target are equal
+        assertEquals(source, target);
+    }
+
+    @Test
+    void copyWithEmptySource() {
+        ElevationProperties source = new ElevationProperties();
+        ElevationProperties target = new ElevationProperties();
+
+        target.setPreprocessed(true);
+        target.setDataAccess(DataAccessEnum.MMAP);
+        target.setCacheClear(true);
+        target.setProvider("provider1");
+        target.setCachePath(Paths.get("/some/path"));
+
+        // Prepare and check source
+        assertNull(source.isPreprocessed());
+        assertNull(source.getDataAccess());
+        assertNull(source.getCacheClear());
+        assertNull(source.getProvider());
+        assertNull(source.getCachePath());
+
+        // Copy properties
+        target.copyProperties(source, false);
+
+        // Check target properties
+        assertNotEquals(source, target);
+        assertNotNull(target.isPreprocessed());
+        assertNotNull(target.getDataAccess());
+        assertNotNull(target.getCacheClear());
+        assertNotNull(target.getProvider());
+        assertNotNull(target.getCachePath());
+
+        // Copy properties again with overwrite
+        target.copyProperties(source, true);
+
+        // Check source and target are still not equal, since null is never copied
+        assertNotEquals(source, target);
+    }
+
+    @Test
+    void copyPropertiesWithNullSource() {
+        ElevationProperties target = new ElevationProperties();
+
+        target.setPreprocessed(true);
+
+        target.copyProperties(null, false);
+        assertTrue(target.isPreprocessed());
+    }
 }
