abffe664016ac8a7c33aac025c8c3c49d8c32520
Julian Psotta <julian.psotta@heigit.org>
2024-08-14 16:17:54 +0200

    refactor: Migrate the ExtendedStorage class to a simple object
    This will allow spring to initialize that object again.

--- ors-engine/src/main/java/org/heigit/ors/config/profile/storages/ExtendedStorage.java
+++ ors-engine/src/main/java/org/heigit/ors/config/profile/storages/ExtendedStorage.java
@@ -2,12 +2,17 @@ package org.heigit.ors.config.profile.storages;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import org.heigit.ors.config.utils.PathDeserializer;
+import org.heigit.ors.config.utils.PathSerializer;
+
+import java.nio.file.Path;
 
 /**
  * This the base class for the extended storage configuration. It contains a boolean field to enable or disable the storage.
@@ -15,28 +20,110 @@ import lombok.Setter;
 @Getter
 @Setter(AccessLevel.PACKAGE)
 @EqualsAndHashCode
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.WRAPPER_OBJECT)
-@JsonSubTypes({
-        @JsonSubTypes.Type(value = ExtendedStorageWayCategory.class, name = "WayCategory"),
-        @JsonSubTypes.Type(value = ExtendedStorageHeavyVehicle.class, name = "HeavyVehicle"),
-        @JsonSubTypes.Type(value = ExtendedStorageHereTraffic.class, name = "HereTraffic"),
-        @JsonSubTypes.Type(value = ExtendedStorageHillIndex.class, name = "HillIndex"),
-        @JsonSubTypes.Type(value = ExtendedStorageOsmId.class, name = "OsmId"),
-        @JsonSubTypes.Type(value = ExtendedStorageBorders.class, name = "Borders"),
-        @JsonSubTypes.Type(value = ExtendedStorageRoadAccessRestrictions.class, name = "RoadAccessRestrictions"),
-        @JsonSubTypes.Type(value = ExtendedStorageTrailDifficulty.class, name = "TrailDifficulty"),
-        @JsonSubTypes.Type(value = ExtendedStorageWheelchair.class, name = "Wheelchair"),
-        @JsonSubTypes.Type(value = ExtendedStorageWaySurfaceType.class, name = "WaySurfaceType"),
-        @JsonSubTypes.Type(value = ExtendedStorageTollways.class, name = "Tollways"),
-})
-public abstract class ExtendedStorage {
+public class ExtendedStorage {
+
+    // Relevant for all
+    @JsonProperty
+    private Boolean enabled;
+
+    // Relevant for most index storages
+    @JsonProperty
+    @JsonSerialize(using = PathSerializer.class)
+    @JsonDeserialize(using = PathDeserializer.class)
+    private Path filepath;
+
+    // Relevant for HGV profile
+    @JsonProperty
+    private Boolean restrictions;
 
     @JsonProperty
-    private Boolean enabled = true;
+    // Relevant for HereTraffic profile
+    @JsonSerialize(using = PathSerializer.class)
+    @JsonDeserialize(using = PathDeserializer.class)
+    private Path streets;
+    @JsonProperty
+    @JsonSerialize(using = PathSerializer.class)
+    @JsonDeserialize(using = PathDeserializer.class)
+    private Path ref_pattern;
+    @JsonProperty
+    @JsonSerialize(using = PathSerializer.class)
+    @JsonDeserialize(using = PathDeserializer.class)
+    private Path pattern_15min;
+    @JsonProperty
+    private Integer radius;
+    @JsonProperty
+    private Boolean output_log;
+    @JsonProperty
+    @JsonSerialize(using = PathSerializer.class)
+    @JsonDeserialize(using = PathDeserializer.class)
+    private Path log_location;
+
+    // Relevant for HillIndex
+    @JsonProperty("maximum_slope")
+    private Integer maximumSlope;
+
+    // Relevant for borders
+    @JsonProperty
+    @JsonSerialize(using = PathSerializer.class)
+    @JsonDeserialize(using = PathDeserializer.class)
+    private Path boundaries;
+    @JsonProperty
+    @JsonSerialize(using = PathSerializer.class)
+    @JsonDeserialize(using = PathDeserializer.class)
+    private Path ids;
+    @JsonProperty
+    @JsonSerialize(using = PathSerializer.class)
+    @JsonDeserialize(using = PathDeserializer.class)
+    private Path openborders;
+
+    // Relevant for RoadAccessRestrictions
+    @JsonProperty("use_for_warnings")
+    private Boolean use_for_warnings;
+
+    // Relevant for Wheelchair
+    @JsonProperty("KerbsOnCrossings")
+    private Boolean kerbs_on_crossings;
+
+    @Getter
+    @Setter
+    private String ghProfile;
 
     @JsonCreator
     public ExtendedStorage() {
     }
+
+    @JsonCreator
+    public ExtendedStorage(String ignoredEmpty) {
+    }
+
+    @JsonSetter
+    protected void setRestrictions(String restrictions) {
+        this.restrictions = Boolean.parseBoolean(restrictions);
+    }
+
+    @JsonSetter("boundaries")
+    protected void setBoundaries(Path boundaries) {
+        this.boundaries = boundaries != null && !boundaries.toString().isEmpty() ? boundaries.toAbsolutePath() : boundaries;
+    }
+
+    @JsonSetter("ids")
+    @JsonDeserialize(using = PathDeserializer.class)
+    protected void setIds(Path ref_pattern) {
+        this.ids = ref_pattern != null && !ref_pattern.toString().isEmpty() ? ref_pattern.toAbsolutePath() : ref_pattern;
+    }
+
+    @JsonSetter("openborders")
+    @JsonDeserialize(using = PathDeserializer.class)
+    protected void setOpenborders(Path openborders) {
+        this.openborders = openborders != null && !openborders.toString().isEmpty() ? openborders.toAbsolutePath() : openborders;
+    }
+
+    @JsonSetter("filepath")
+    @JsonDeserialize(using = PathDeserializer.class)
+    protected void setFilepath(Path filepath) {
+        this.filepath = filepath != null && !filepath.toString().isEmpty() ? filepath.toAbsolutePath() : filepath;
+    }
+
 }
 
 
--- ors-engine/src/test/java/org/heigit/ors/config/ExtendedStorageTest.java
+++ ors-engine/src/test/java/org/heigit/ors/config/ExtendedStorageTest.java
@@ -4,63 +4,200 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.heigit.ors.config.profile.storages.ExtendedStorage;
 import org.junit.jupiter.api.Test;
 
-import java.util.LinkedHashMap;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ExtendedStorageTest {
 
+    Boolean testConstructorObjectIsEmpty(ExtendedStorage storage) {
+        assertNull(storage.getEnabled(), "Default constructor should initialize 'enabled' to true");
+        assertNull(storage.getFilepath(), "Default constructor should initialize 'filepath' to null");
+        assertNull(storage.getRestrictions(), "Default constructor should initialize 'restrictions' to null");
+        assertNull(storage.getStreets(), "Default constructor should initialize 'streets' to null");
+        assertNull(storage.getRef_pattern(), "Default constructor should initialize 'ref_pattern' to null");
+        assertNull(storage.getPattern_15min(), "Default constructor should initialize 'pattern_15min' to null");
+        assertNull(storage.getRadius(), "Default constructor should initialize 'radius' to null");
+        assertNull(storage.getOutput_log(), "Default constructor should initialize 'output_log' to null");
+        assertNull(storage.getLog_location(), "Default constructor should initialize 'log_location' to null");
+        assertNull(storage.getMaximumSlope(), "Default constructor should initialize 'maximum_slope' to null");
+        assertNull(storage.getBoundaries(), "Default constructor should initialize 'boundaries' to null");
+        return true;
+    }
+
     @Test
     void testDefaultConstructor() {
-        ExtendedStorage storage = new HelperClass();
-        assertTrue(storage.getEnabled(), "Default constructor should initialize 'enabled' to true");
+        ExtendedStorage storage = new ExtendedStorage();
+        assertTrue(testConstructorObjectIsEmpty(storage), "Default constructor should initialize all fields to null");
     }
 
     @Test
     void testStringConstructor() {
-        ExtendedStorage storage = new HelperClass("");
-        assertTrue(storage.getEnabled(), "String constructor should not alter 'enabled' state");
+        ExtendedStorage storage = new ExtendedStorage("");
+        assertTrue(testConstructorObjectIsEmpty(storage), "String constructor should initialize all fields to null");
     }
 
     @Test
-    void testLinkedHashMapConstructor() {
-        ExtendedStorage storage = new HelperClass(new LinkedHashMap<>());
-        assertTrue(storage.getEnabled(), "LinkedHashMap constructor should not alter 'enabled' state");
+    void testSetRestrictions() {
+        HelperClass storage = new HelperClass();
+
+        assertNull(storage.getRestrictions());
+        storage.setRestrictions("true");
+        assertTrue(storage.getRestrictions());
+
+        storage.setRestrictions("false");
+        assertFalse(storage.getRestrictions());
     }
 
     @Test
-    void testGetterAndSetter() {
-        ExtendedStorage storage = new HelperClass();
-        assertTrue(storage.getEnabled(), "setEnabled(true) should result in 'enabled' being true");
+    void testSetBoundaries() {
+        HelperClass storage = new HelperClass();
+
+        assertNull(storage.getBoundaries());
+        Path testBoundariesPath = Paths.get("src/test/resources/boundaries.csv");
+        storage.setBoundaries(testBoundariesPath);
+        assertEquals(testBoundariesPath.toAbsolutePath(), storage.getBoundaries());
+
+        storage.setBoundaries(null);
+        assertNull(storage.getBoundaries());
+
+        storage.setBoundaries(Paths.get(""));
+        assertEquals(Path.of(""), storage.getBoundaries());
     }
 
     @Test
-    void testSerializationProducesCorrectJson() throws Exception {
+    void testSetIds() {
+        HelperClass storage = new HelperClass();
+        Path path = Path.of("/path/to/ids");
+        storage.setIds(path);
+        assertEquals(path.toAbsolutePath(), storage.getIds(), "setIds should correctly set the absolute path");
+
+        storage.setIds(null);
+        assertNull(storage.getIds(), "setIds should set the path to null if the new path is null");
+
+        storage.setIds(Paths.get(""));
+        assertEquals(Path.of(""), storage.getIds(), "setIds should not change the path if the new path is empty");
+    }
+
+    @Test
+    void testSetOpenborders() {
+        HelperClass storage = new HelperClass();
+        Path path = Path.of("/path/to/openborders");
+        storage.setOpenborders(path);
+        assertEquals(path.toAbsolutePath(), storage.getOpenborders(), "setOpenborders should correctly set the absolute path");
+
+        storage.setOpenborders(null);
+        assertNull(storage.getOpenborders(), "setOpenborders should set the path to null if the new path is null");
+        storage.setOpenborders(Paths.get(""));
+        assertEquals(Path.of(""), storage.getOpenborders(), "setOpenborders should not change the path if the new path is empty");
+    }
+
+    @Test
+    void testSetFilepath() {
+        HelperClass storage = new HelperClass();
+        Path path = Path.of("/path/to/filepath");
+        storage.setFilepath(path);
+        assertEquals(path.toAbsolutePath(), storage.getFilepath(), "setFilepath should correctly set the path");
+
+        storage.setFilepath(null);
+        assertNull(storage.getFilepath(), "setFilepath should set the path to null if the new path is null");
+
+        storage.setFilepath(Paths.get(""));
+        assertEquals(Path.of(""), storage.getFilepath(), "setFilepath should not change the path if the new path is empty");
+    }
+
+    @Test
+    void testSerializationEmptyStorageProducesCorrectJson() throws Exception {
         // Step 1: Create and configure an instance of ExtendedStorage
-        ExtendedStorage storage = new HelperClass();
+        ExtendedStorage storage = new ExtendedStorage();
 
         // Step 2: Serialize the object to JSON
         ObjectMapper mapper = new ObjectMapper();
         String jsonResult = mapper.writeValueAsString(storage);
 
+        //language=JSON
+        String expectedJson = """
+                {
+                    "enabled":null,
+                    "restrictions":null,
+                    "streets":null,
+                    "ref_pattern":null,
+                    "pattern_15min":null,
+                    "radius":null,
+                    "output_log":null,
+                    "log_location":null,
+                    "ghProfile":null,
+                    "filepath":null,
+                    "maximum_slope":null,
+                    "boundaries":null,
+                    "ids":null,
+                    "openborders":null,
+                    "use_for_warnings":null,
+                    "KerbsOnCrossings":null
+                }
+                """;
         // Step 3: Assert JSON structure and values including enabled
-        assertTrue(jsonResult.contains("\"enabled\":true"), "Serialized JSON should have 'enabled' set to true");
-        // Assert that jsonResult doesn't contain the default path parameter
-        assertFalse(jsonResult.contains("\"filepath\":"), "Serialized JSON should not contain 'filepath'");
+        assertEquals(mapper.readTree(expectedJson), mapper.readTree(jsonResult), "Serialized JSON should match the expected JSON");
     }
 
-    class HelperClass extends ExtendedStorage {
+    @Test
+    void testSerializationNonEmptyStorageProducesCorrectJson() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+
+        //language=JSON
+        String expectedJson = """
+                {
+                     "enabled":true,
+                     "restrictions":true,
+                     "streets":"/src/test/resources/streets.csv",
+                     "ref_pattern":"/src/test/resources/ref_pattern.csv",
+                     "pattern_15min":"/src/test/resources/pattern_15min.csv",
+                     "radius":10,
+                     "output_log":true,
+                     "log_location":"/src/test/resources/log_location.csv",
+                     "ghProfile":"car",
+                     "filepath":"/src/test/resources/filepath.csv",
+                     "maximum_slope":5,
+                     "boundaries":"/src/test/resources/boundaries.csv",
+                     "ids":"/src/test/resources/ids.csv",
+                     "openborders":"/src/test/resources/openborders.csv",
+                     "use_for_warnings":true,
+                     "KerbsOnCrossings":true
+                 }
+                """;
+        // Serialize the object to JSON
+        ExtendedStorage storage = mapper.readValue(expectedJson, ExtendedStorage.class);
+
+        // Step 2: Serialize the object to JSON
+        String jsonResult = mapper.writeValueAsString(storage);
+        // Step 3: Assert JSON structure and values including enabled
+        assertEquals(mapper.readTree(expectedJson), mapper.readTree(jsonResult), "Serialized JSON should match the expected JSON");
+    }
+
+    static class HelperClass extends ExtendedStorage {
         public HelperClass() {
             super();
         }
 
-        public HelperClass(String ignoredEmpty) {
-            super();
+        public void setRestrictions(String restrictions) {
+            super.setRestrictions(restrictions);
         }
 
-        public HelperClass(LinkedHashMap<String, Object> ignoredMap) {
-            super();
+        public void setBoundaries(Path boundaries) {
+            super.setBoundaries(boundaries);
+        }
+
+        public void setIds(Path ids) {
+            super.setIds(ids);
+        }
+
+        public void setOpenborders(Path openborders) {
+            super.setOpenborders(openborders);
+        }
+
+        public void setFilepath(Path filepath) {
+            super.setFilepath(filepath);
         }
     }
 }
