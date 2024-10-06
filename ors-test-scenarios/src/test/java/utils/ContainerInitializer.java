445fd828e123d6347be935f208aa3e06b958abf0
Julian Psotta <julian.psotta@heigit.org>
2024-10-06 21:29:10 +0200

    feat: Allow for parallel container start
    This accelerates the whole container spawning process massively.

--- ors-test-scenarios/src/test/java/utils/ContainerInitializer.java
+++ ors-test-scenarios/src/test/java/utils/ContainerInitializer.java
@@ -1,4 +1,4 @@
-package integrationtests;
+package utils;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.testcontainers.containers.BindMode;
@@ -6,30 +6,42 @@ import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.HttpWaitStrategy;
 import org.testcontainers.containers.wait.strategy.WaitStrategy;
 import org.testcontainers.images.builder.ImageFromDockerfile;
+import org.testcontainers.lifecycle.Startables;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.time.Duration;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public abstract class ContainerInitializer {
-    private final Map<String, String> defaultEnv = Map.of(
-            "logging.level.org.heigit", "INFO",
-            "ors.engine.graphs_data_access", "MMAP"
-    );
-    private GenericContainer<?> warContainer;
-    private GenericContainer<?> jarContainer;
+    private static final Map<String, String> defaultEnv = Map.of("logging.level.org.heigit", "INFO", "ors.engine.graphs_data_access", "MMAP");
+    private static GenericContainer<?> warContainer;
+    private static GenericContainer<?> jarContainer;
 
-    public GenericContainer<?> initContainer(ContainerTestImage containerTestImage) {
+    static {
+        Startables.deepStart(initContainer(ContainerTestImage.JAR_CONTAINER, false), initContainer(ContainerTestImage.WAR_CONTAINER, false)).join();
+    }
+
+    public static Stream<Object[]> imageStream() {
+        return Stream.of(new Object[]{ContainerTestImage.JAR_CONTAINER}, new Object[]{ContainerTestImage.WAR_CONTAINER});
+    }
+
+    public static GenericContainer<?> initContainer(ContainerTestImage containerTestImage) {
+        return initContainer(containerTestImage, true);
+    }
+
+    public static GenericContainer<?> initContainer(ContainerTestImage containerTestImage, Boolean autoStart) {
         if (containerTestImage == null) {
             throw new IllegalArgumentException("containerTestImage must not be null");
         }
-
+        //@formatter:off
         WaitStrategy waitStrategy = new HttpWaitStrategy()
                 .forPort(8080)
                 .forStatusCode(200)
                 .forPath("/ors/v2/health")
                 .withStartupTimeout(Duration.ofSeconds(80));
-
+        //@formatter:off
         GenericContainer<?> container = new GenericContainer<>(
                 new ImageFromDockerfile(containerTestImage.getName(), false)
                         .withFileFromPath("ors-api", Path.of("../ors-api"))
@@ -42,25 +54,35 @@ public abstract class ContainerInitializer {
                         .withTarget(containerTestImage.getName())
         )
                 .withEnv(defaultEnv)
-                .withFileSystemBind("./graphs-integrationtests", "/home/ors/openrouteservice/graphs", BindMode.READ_WRITE)
+                .withFileSystemBind("./graphs-integrationtests/" + containerTestImage.getName(),
+                        "/home/ors/openrouteservice/graphs", BindMode.READ_WRITE)
                 .withExposedPorts(8080)
                 .withLogConsumer(outputFrame -> System.out.print(outputFrame.getUtf8String()))
                 .waitingFor(waitStrategy);
 
         if (containerTestImage == ContainerTestImage.WAR_CONTAINER) {
-            if (warContainer == null || !warContainer.isRunning()) {
+            if (warContainer == null) {
                 warContainer = container;
+            }
+            if (autoStart && !warContainer.isRunning()) {
                 warContainer.start();
             }
             return warContainer;
         } else {
-            if (jarContainer == null || !jarContainer.isRunning()) {
+            if (jarContainer == null) {
                 jarContainer = container;
-                jarContainer.start();
             }
-            return jarContainer;
+            if (autoStart && !jarContainer.isRunning())
+                jarContainer.start();
         }
+        return jarContainer;
     }
+
+    protected void restartContainer(GenericContainer<?> container) throws IOException, InterruptedException {
+        container.stop();
+        container.start();
+    }
+
 
     @BeforeEach
     public void resetEnv() {
@@ -72,10 +94,9 @@ public abstract class ContainerInitializer {
         }
     }
 
-    // Create enum of available images
+    // Create enum for available test images
     public enum ContainerTestImage {
-        WAR_CONTAINER("ors-test-scenarios-war"),
-        JAR_CONTAINER("ors-test-scenarios-jar");
+        WAR_CONTAINER("ors-test-scenarios-war"), JAR_CONTAINER("ors-test-scenarios-jar");
 
         private final String name;
 
