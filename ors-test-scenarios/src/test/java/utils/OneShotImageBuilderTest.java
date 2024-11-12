797f64bcf7c5b7736bf754b3e33ca534097d6c2f
Julian Psotta <julianpsotta@gmail.com>
2024-11-12 12:42:55 +0100

    test(ors-test-scenarios): Ignore the one shot image builder for regular runs

--- ors-test-scenarios/src/test/java/utils/OneShotImageBuilderTest.java
+++ ors-test-scenarios/src/test/java/utils/OneShotImageBuilderTest.java
@@ -1,9 +1,14 @@
 package utils;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.ShellStrategy;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.junit.jupiter.TestcontainersExtension;
 
 import static utils.ContainerInitializer.initContainerWithSharedGraphs;
 
@@ -11,9 +16,13 @@ import static utils.ContainerInitializer.initContainerWithSharedGraphs;
  * This test function is a helper function to build the necessary container images for the test scenarios.
  * Ít is supposed to used in CI.
  */
+@ExtendWith(TestcontainersExtension.class)
+@Testcontainers(disabledWithoutDocker = true)
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class OneShotImageBuilderTest {
 
     @Test
+    @Disabled("This test is supposed to be used in CI to cache the images")
     void oneShotImageBuilder() {
         GenericContainer<?> containerWar = initContainerWithSharedGraphs(ContainerInitializer.ContainerTestImageBare.WAR_CONTAINER_BARE, false);
         containerWar.setWaitStrategy(new ShellStrategy());
