27e8d2b3fe9a4d6ccd42e82b0c3e07a435fc6d88
Julian Psotta <julianpsotta@gmail.com>
2024-11-25 21:30:58 +0100

    refactor(sonar): Remove local shadow property java:S1117

--- ors-engine/src/main/java/org/heigit/ors/snapping/SnappingRequest.java
+++ ors-engine/src/main/java/org/heigit/ors/snapping/SnappingRequest.java
@@ -6,8 +6,6 @@ import com.graphhopper.routing.util.FlagEncoder;
 import com.graphhopper.routing.weighting.Weighting;
 import com.graphhopper.storage.GraphHopperStorage;
 import com.graphhopper.util.PMap;
-import lombok.Getter;
-import lombok.Setter;
 import org.heigit.ors.common.ServiceRequest;
 import org.heigit.ors.matrix.MatrixSearchContext;
 import org.heigit.ors.matrix.MatrixSearchContextBuilder;
@@ -18,20 +16,25 @@ import org.heigit.ors.routing.graphhopper.extensions.ORSWeightingFactory;
 import org.heigit.ors.util.ProfileTools;
 import org.locationtech.jts.geom.Coordinate;
 
-@Getter
 public class SnappingRequest extends ServiceRequest {
-    @Setter
     private String profileName;
     private final int profileType;
     private final Coordinate[] locations;
     private final double maximumSearchRadius;
-    @Setter
     private int maximumLocations;
 
     public SnappingRequest(int profileType, Coordinate[] locations, double maximumSearchRadius) {
         this.profileType = profileType;
         this.locations = locations;
         this.maximumSearchRadius = maximumSearchRadius;
+    }
+
+    public String getProfileName() {
+        return profileName;
+    }
+
+    public void setProfileName(String profileName) {
+        this.profileName = profileName;
     }
 
     public int getProfileType() {
@@ -62,14 +65,14 @@ public class SnappingRequest extends ServiceRequest {
         int weightingMethod = WeightingMethod.RECOMMENDED; // Only needed to create the profile string
         ProfileTools.setWeightingMethod(hintsMap, weightingMethod, getProfileType(), false);
         ProfileTools.setWeighting(hintsMap, weightingMethod, getProfileType(), false);
-        String profileName = ProfileTools.makeProfileName(encoderName, hintsMap.getString("weighting", ""), false);
+        String localProfileName = ProfileTools.makeProfileName(encoderName, hintsMap.getString("weighting", ""), false);
         GraphHopperStorage ghStorage = gh.getGraphHopperStorage();
         String graphDate = ghStorage.getProperties().get("datareader.import.date");
 
         // TODO: replace usage of matrix search context by snapping-specific class
         MatrixSearchContextBuilder builder = new MatrixSearchContextBuilder(ghStorage, gh.getLocationIndex(), AccessFilter.allEdges(flagEncoder.getAccessEnc()), true);
-        Weighting weighting = new ORSWeightingFactory(ghStorage, gh.getEncodingManager()).createWeighting(gh.getProfile(profileName), hintsMap, false);
-        MatrixSearchContext mtxSearchCntx = builder.create(ghStorage.getBaseGraph(), null, weighting, profileName, getLocations(), getLocations(), getMaximumSearchRadius());
+        Weighting weighting = new ORSWeightingFactory(ghStorage, gh.getEncodingManager()).createWeighting(gh.getProfile(localProfileName), hintsMap, false);
+        MatrixSearchContext mtxSearchCntx = builder.create(ghStorage.getBaseGraph(), null, weighting, localProfileName, getLocations(), getLocations(), getMaximumSearchRadius());
         return new SnappingResult(mtxSearchCntx.getSources().getLocations(), graphDate);
     }
 }
