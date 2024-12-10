4620b14b22134558af03e5aa0d24cd4b589cfc92
Takara Baumbach <takara.baumbach@heigit.org>
2024-12-10 17:06:27 +0100

    refactor: restructure ExportRequest.computeExport(), rename API parameter "debug" to "additional_info"

--- ors-engine/src/main/java/org/heigit/ors/export/ExportRequest.java
+++ ors-engine/src/main/java/org/heigit/ors/export/ExportRequest.java
@@ -39,11 +39,14 @@ public class ExportRequest extends ServiceRequest {
     private String profileName;
     private int profileType = -1;
 
-    private boolean debug;
+    private boolean additionalEdgeInfo;
     private boolean topoJson;
     private boolean useRealGeometry;
 
     private static final int NO_TIME = -1;
+    private OsmIdGraphStorage osmIdGraphStorage;
+    private WheelchairAttributesGraphStorage wheelchairAttributesGraphStorage;
+    private Weighting weighting;
 
     public String getProfileName() {
         return profileName;
@@ -53,24 +56,16 @@ public class ExportRequest extends ServiceRequest {
         this.profileName = profileName;
     }
 
-    public BBox getBoundingBox() {
-        return this.boundingBox;
-    }
-
     public void setBoundingBox(BBox bbox) {
         this.boundingBox = bbox;
-    }
-
-    public int getProfileType() {
-        return profileType;
     }
 
     public void setProfileType(int profileType) {
         this.profileType = profileType;
     }
 
-    public void setDebug(boolean debug) {
-        this.debug = debug;
+    public void setAdditionalEdgeInfo(boolean additionalEdgeInfo) {
+        this.additionalEdgeInfo = additionalEdgeInfo;
     }
 
     public void setTopoJson(boolean equals) {
@@ -84,113 +79,108 @@ public class ExportRequest extends ServiceRequest {
     public ExportResult computeExport(RoutingProfile routingProfile) {
         ExportResult res = new ExportResult();
 
+        // Prepare graph data access
         GraphHopper gh = routingProfile.getGraphhopper();
-        String encoderName = RoutingProfileType.getEncoderName(getProfileType());
+        String encoderName = RoutingProfileType.getEncoderName(profileType);
         Graph graph = gh.getGraphHopperStorage().getBaseGraph();
-
+        NodeAccess nodeAccess = graph.getNodeAccess();
         PMap hintsMap = new PMap();
-        int weightingMethod = WeightingMethod.FASTEST;
-        ProfileTools.setWeightingMethod(hintsMap, weightingMethod, getProfileType(), false);
-        String localProfileName = ProfileTools.makeProfileName(encoderName, hintsMap.getString("weighting_method", ""), false);
-        Weighting weighting = gh.createWeighting(gh.getProfile(localProfileName), hintsMap);
-
-        FlagEncoder flagEncoder = gh.getEncodingManager().getEncoder(encoderName);
-        EdgeExplorer explorer = graph.createEdgeExplorer(AccessFilter.outEdges(flagEncoder.getAccessEnc()));
+        ProfileTools.setWeightingMethod(hintsMap, WeightingMethod.FASTEST, profileType, false);
+        weighting = gh.createWeighting(gh.getProfile(ProfileTools.makeProfileName(encoderName, hintsMap.getString("weighting_method", ""), false)), hintsMap);
+        osmIdGraphStorage = GraphStorageUtils.getGraphExtension(gh.getGraphHopperStorage(), OsmIdGraphStorage.class);
+        wheelchairAttributesGraphStorage = GraphStorageUtils.getGraphExtension(gh.getGraphHopperStorage(), WheelchairAttributesGraphStorage.class);
 
         // filter graph for nodes in Bounding Box
-        LocationIndex index = gh.getLocationIndex();
-        NodeAccess nodeAccess = graph.getNodeAccess();
-
-        Set<Integer> nodesInBBox = new HashSet<>();
-        index.query(boundingBox, edgeId -> {
-            // According to GHUtility.getEdgeFromEdgeKey, edgeIds are calculated as edgeKey/2.
-            EdgeIteratorState edge = graph.getEdgeIteratorStateForKey(edgeId * 2);
-            int baseNode = edge.getBaseNode();
-            int adjNode = edge.getAdjNode();
-            if (boundingBox.contains(nodeAccess.getLat(baseNode), nodeAccess.getLon(baseNode))) {
-                nodesInBBox.add(baseNode);
-            }
-            if (boundingBox.contains(nodeAccess.getLat(adjNode), nodeAccess.getLon(adjNode))) {
-                nodesInBBox.add(adjNode);
-            }
-        });
-
+        Set<Integer> nodesInBBox = nodesInBBox(gh.getLocationIndex(), nodeAccess, graph);
         LOGGER.debug("Found %d nodes in bbox.".formatted(nodesInBBox.size()));
-
-        if (nodesInBBox.isEmpty()) {
-            // without nodes, no export can be calculated
+        if (nodesInBBox.isEmpty()) { // without nodes, no export can be calculated
             res.setWarning(new ExportWarning(ExportWarning.EMPTY_BBOX));
             return res;
         }
-        Map<Long, TopoGeometry> topoGeometries = res.getTopoGeometries();
-        OsmIdGraphStorage osmIdGraphStorage = GraphStorageUtils.getGraphExtension(gh.getGraphHopperStorage(), OsmIdGraphStorage.class);
-        boolean osmIdsAvailable = osmIdGraphStorage != null;
 
-        // calculate node coordinates
+        // iterate over all edges and add them to the result object
         for (int from : nodesInBBox) {
             Coordinate fromCoords = new Coordinate(nodeAccess.getLon(from), nodeAccess.getLat(from));
             res.addLocation(from, fromCoords);
-            EdgeIterator iter = explorer.setBaseNode(from);
+            EdgeIterator iter = graph.createEdgeExplorer(AccessFilter.outEdges(gh.getEncodingManager().getEncoder(encoderName).getAccessEnc())).setBaseNode(from);
             while (iter.next()) {
                 int to = iter.getAdjNode();
                 if (nodesInBBox.contains(to)) {
-                    Pair<Integer, Integer> p = new Pair<>(from, to);
-                    Map<String, Object> extra = new HashMap<>();
-                    res.addEdge(p, weighting.calcEdgeWeight(iter, false, NO_TIME));
                     LOGGER.debug("Edge %d: from %d to %d".formatted(iter.getEdge(), from, to));
-
-                    if (topoJson) {
-                        LineString geo;
-                        if (useRealGeometry) {
-                            geo = iter.fetchWayGeometry(FetchMode.ALL).toLineString(false);
-                        } else {
-                            Coordinate toCoords = new Coordinate(nodeAccess.getLon(to), nodeAccess.getLat(to));
-                            geo = geometryFactory.createLineString(new Coordinate[]{fromCoords, toCoords});
-                        }
-                        if (osmIdsAvailable) {
-                            boolean reverse = iter.getEdgeKey() % 2 == 1;
-                            TopoGeometry topoGeometry = topoGeometries.computeIfAbsent(osmIdGraphStorage.getEdgeValue(iter.getEdge()), x ->
-                                    new TopoGeometry(weighting.getSpeedCalculator().getSpeed(iter, reverse, NO_TIME),
-                                            weighting.getSpeedCalculator().getSpeed(iter, !reverse, NO_TIME))
-                            );
-                            topoGeometry.getArcs().compute(iter.getEdge(), (k, v) -> {
-                                if (v != null) {
-                                    topoGeometry.setBothDirections(true);
-                                    return v;
-                                } else {
-                                    return reverse ? new TopoArc(geo.reverse(), iter.getDistance(), iter.getAdjNode(), iter.getBaseNode()) :
-                                            new TopoArc(geo, iter.getDistance(), iter.getBaseNode(), iter.getAdjNode());
-                                }
-                            });
-                        } else {
-                            extra.put("geometry", geo);
-                        }
+                    LineString geo;
+                    if (useRealGeometry) {
+                        geo = iter.fetchWayGeometry(FetchMode.ALL).toLineString(false);
+                    } else {
+                        Coordinate toCoords = new Coordinate(nodeAccess.getLon(to), nodeAccess.getLat(to));
+                        geo = geometryFactory.createLineString(new Coordinate[]{fromCoords, toCoords});
                     }
 
-                    if (debug) {
-                        extra.put("edge_id", iter.getEdge());
-                        WheelchairAttributesGraphStorage storage = GraphStorageUtils.getGraphExtension(gh.getGraphHopperStorage(), WheelchairAttributesGraphStorage.class);
-                        if (storage != null) {
-                            WheelchairAttributes attributes = new WheelchairAttributes();
-                            byte[] buffer = new byte[WheelchairAttributesGraphStorage.BYTE_COUNT];
-                            storage.getEdgeValues(iter.getEdge(), attributes, buffer);
-                            if (attributes.hasValues()) {
-                                extra.put("incline", attributes.getIncline());
-                                extra.put("surface_quality_known", attributes.isSurfaceQualityKnown());
-                                extra.put("suitable", attributes.isSuitable());
-                            }
-                        }
-                        if (osmIdsAvailable) {
-                            extra.put("osm_id", osmIdGraphStorage.getEdgeValue(iter.getEdge()));
-                        }
-                    }
-
-                    if (!extra.isEmpty()) {
-                        res.addEdgeExtra(p, extra);
+                    if (topoJson && osmIdGraphStorage != null) {
+                        addEdgeToTopoGeometries(res, iter, geo);
+                    } else {
+                        addEdgeToResultObject(res, iter, geo, from, to);
                     }
                 }
             }
         }
         return res;
+    }
+
+    private Set<Integer> nodesInBBox(LocationIndex index, NodeAccess nodeAccess, Graph graph) {
+        Set<Integer> ret = new HashSet<>();
+        index.query(boundingBox, edgeId -> {
+            // According to GHUtility.getEdgeFromEdgeKey, edgeIds are calculated as edgeKey/2.
+            EdgeIteratorState edge = graph.getEdgeIteratorStateForKey(edgeId * 2);
+            int baseNode = edge.getBaseNode();
+            int adjNode = edge.getAdjNode();
+            if (this.boundingBox.contains(nodeAccess.getLat(baseNode), nodeAccess.getLon(baseNode))) {
+                ret.add(baseNode);
+            }
+            if (this.boundingBox.contains(nodeAccess.getLat(adjNode), nodeAccess.getLon(adjNode))) {
+                ret.add(adjNode);
+            }
+        });
+        return ret;
+    }
+
+    private void addEdgeToTopoGeometries(ExportResult res, EdgeIterator iter, LineString geo) {
+        boolean reverse = iter.getEdgeKey() % 2 == 1;
+        TopoGeometry topoGeometry = res.getTopoGeometries().computeIfAbsent(osmIdGraphStorage.getEdgeValue(iter.getEdge()), x ->
+                new TopoGeometry(weighting.getSpeedCalculator().getSpeed(iter, reverse, NO_TIME),
+                        weighting.getSpeedCalculator().getSpeed(iter, !reverse, NO_TIME))
+        );
+        topoGeometry.getArcs().compute(iter.getEdge(), (k, v) -> {
+            if (v != null) {
+                topoGeometry.setBothDirections(true);
+                return v;
+            } else {
+                return reverse ? new TopoArc(geo.reverse(), iter.getDistance(), iter.getAdjNode(), iter.getBaseNode()) :
+                        new TopoArc(geo, iter.getDistance(), iter.getBaseNode(), iter.getAdjNode());
+            }
+        });
+    }
+
+    private void addEdgeToResultObject(ExportResult res, EdgeIterator iter, LineString geo, int from, int to) {
+        Pair<Integer, Integer> p = new Pair<>(from, to);
+        res.addEdge(p, weighting.calcEdgeWeight(iter, false, NO_TIME));
+        res.getEdgeGeometries().put(p, geo);
+        if (additionalEdgeInfo) {
+            Map<String, Object> extra = new HashMap<>();
+            if (osmIdGraphStorage != null) {
+                extra.put("osm_id", osmIdGraphStorage.getEdgeValue(iter.getEdge()));
+            }
+            extra.put("ors_id", iter.getEdge());
+            if (wheelchairAttributesGraphStorage != null) {
+                WheelchairAttributes attributes = new WheelchairAttributes();
+                byte[] buffer = new byte[WheelchairAttributesGraphStorage.BYTE_COUNT];
+                wheelchairAttributesGraphStorage.getEdgeValues(iter.getEdge(), attributes, buffer);
+                if (attributes.hasValues()) {
+                    extra.put("incline", attributes.getIncline());
+                    extra.put("surface_quality_known", attributes.isSurfaceQualityKnown());
+                    extra.put("suitable", attributes.isSuitable());
+                }
+            }
+            res.addEdgeExtra(p, extra);
+        }
     }
 }
