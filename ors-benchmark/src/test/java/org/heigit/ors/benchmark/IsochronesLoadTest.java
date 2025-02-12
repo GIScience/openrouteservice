83c36026eb66c042fc8fdf91d0f768b625ebbc34
takb <takara.baumbach@heigit.org>
2025-02-12 13:11:55 +0100

    feat: batch processing scenarios

--- ors-benchmark/src/test/java/org/heigit/ors/benchmark/IsochronesLoadTest.java
+++ ors-benchmark/src/test/java/org/heigit/ors/benchmark/IsochronesLoadTest.java
@@ -1,9 +1,6 @@
 package org.heigit.ors.benchmark;
 
-import io.gatling.javaapi.core.ChainBuilder;
-import io.gatling.javaapi.core.FeederBuilder;
-import io.gatling.javaapi.core.ScenarioBuilder;
-import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.core.*;
 import io.gatling.javaapi.http.HttpProtocolBuilder;
 
 import static io.gatling.javaapi.core.CoreDsl.*;
@@ -12,7 +9,7 @@ import static io.gatling.javaapi.http.HttpDsl.status;
 
 public class IsochronesLoadTest extends Simulation {
 
-    static final int POINTS = 3;
+    static final int BATCH_SIZE_UPTO = 5;
 
     static String locations(int num) {
         StringBuilder sb = new StringBuilder();
@@ -25,39 +22,46 @@ public class IsochronesLoadTest extends Simulation {
         return sb.toString();
     }
 
-    FeederBuilder<String> feeder = ssv("search.csv").random();
-    ChainBuilder request = exec(
-            feed(feeder, POINTS),
-            http("Post")
-                    .post("/v2/isochrones/driving-car")
-                    .body(StringBody("{\"locations\":[" + locations(POINTS) + "] , \"range\":[100, 200, 300]}"))
-                    .check(status().is(200))
-                    .check(status().saveAs("responseStatus"))
-                    .check(bodyString().saveAs("responseBody"))
-    ).exec(session -> {
-        int responseStatus = session.getInt("responseStatus");
-        if (responseStatus != 200) {
-            System.out.println("Response status: " + responseStatus + ", Response body: " + session.getString("responseBody"));
-        }
-        return session;
-    });
+    static ChainBuilder makeRequest(int batchSize) {
+        FeederBuilder<String> feeder = ssv("search.csv").random();
+        return exec(
+                feed(feeder, batchSize),
+                http("Post")
+                        .post("/v2/isochrones/driving-car")
+                        .body(StringBody("{\"locations\":[" + locations(batchSize) + "] , \"range\":[100, 200, 300]}"))
+                        .check(status().is(200))
+                        .check(status().saveAs("responseStatus"))
+                        .check(bodyString().saveAs("responseBody"))
+        ).exec(session -> {
+            int responseStatus = session.getInt("responseStatus");
+            if (responseStatus != 200) {
+                System.out.println("Response status: " + responseStatus + ", Response body: " + session.getString("responseBody"));
+            }
+            return session;
+        });
+    }
 
     HttpProtocolBuilder httpProtocol =
 //            http.baseUrl("https://api.openrouteservice.org")
 //                    .authorizationHeader("Bearer API_KEY")
             http.baseUrl("http://localhost:8082/ors")
-                    .authorizationHeader("Bearer 5b3ce3597851110001cf6248e9b8b3f4b7b94b5b8f7b4f4b7b94b5b8")
+                    .authorizationHeader("Bearer API KEY")
                     .acceptHeader("application/geo+json; charset=utf-8")
                     .contentTypeHeader("application/json; charset=utf-8")
                     .userAgentHeader(
                             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/119.0"
                     );
 
-    ScenarioBuilder users = scenario("Users").exec(request);
-
+    PopulationBuilder executions;
     {
-        setUp(
-                users.injectOpen(rampUsers(10).during(10))
-        ).protocols(httpProtocol);
+        OpenInjectionStep injection = rampUsers(100).during(5);
+        for (int i = 1; i <= BATCH_SIZE_UPTO; i++) {
+            if (executions == null) {
+                executions = scenario("Scenario " + i).exec(makeRequest(i)).injectOpen(injection);
+            } else {
+                executions = executions.andThen(scenario("Scenario " + i).exec(makeRequest(i)).injectOpen(injection));
+            }
+        }
+        setUp(executions).protocols(httpProtocol);
     }
 }
