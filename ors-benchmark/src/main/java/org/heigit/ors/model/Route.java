f71e620432bd494ae702d3a99aa6f770072c06b4
Julian Psotta <julian.psotta@heigit.org>
2025-03-06 12:02:10 +0100

    refactor: Migrate snapping to the new structure

--- ors-benchmark/src/main/java/org/heigit/ors/model/Route.java
+++ ors-benchmark/src/main/java/org/heigit/ors/model/Route.java
@@ -28,4 +28,28 @@ public class Route {
     public String getProfile() {
         return profile;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Route route = (Route) obj;
+        return Double.compare(route.distance, distance) == 0 && java.util.Arrays.equals(start, route.start)
+                && java.util.Arrays.equals(end, route.end) && profile.equals(route.profile);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = java.util.Arrays.hashCode(start);
+        result = prime * result + java.util.Arrays.hashCode(end);
+        result = prime * result
+                + (int) (Double.doubleToLongBits(distance) ^ (Double.doubleToLongBits(distance) >>> 32));
+        result = prime * result + profile.hashCode();
+        return result;
+    }
 }
