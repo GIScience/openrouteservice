f2776c0785a31b055358b9859f9fd9397e6ddcf8
Julian Psotta <julian.psotta@heigit.org>
2025-03-06 18:36:59 +0100

    fix: Fix sonarlint issues

--- ors-benchmark/src/main/java/org/heigit/ors/benchmark/util/NameUtils.java
+++ ors-benchmark/src/main/java/org/heigit/ors/benchmark/util/NameUtils.java
@@ -3,11 +3,13 @@ package org.heigit.ors.benchmark.util;
 import java.io.File;
 
 public class NameUtils {
+    private NameUtils() {
+        // Hide implicit constructor
+    }
+
     public static String getFileNameWithoutExtension(String filePath) {
         String fileName = new File(filePath).getName();
         int lastDotIndex = fileName.lastIndexOf('.');
         return lastDotIndex > 0 ? fileName.substring(0, lastDotIndex) : fileName;
     }
-
-
 }
