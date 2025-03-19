069c95898dc7840ff928da8706b0fb954f4b78f2
Till Frankenbach <till.frankenbach@gmail.com>
2025-03-19 10:24:40 +0100

    refactor: parametrize tests

--- ors-engine/src/test/java/org/heigit/ors/routing/graphhopper/extensions/ORSGraphHopperConfigTest.java
+++ ors-engine/src/test/java/org/heigit/ors/routing/graphhopper/extensions/ORSGraphHopperConfigTest.java
@@ -2,90 +2,33 @@ package org.heigit.ors.routing.graphhopper.extensions;
 
 import org.heigit.ors.config.EngineProperties;
 import org.heigit.ors.config.profile.ProfileProperties;
-import org.junit.jupiter.api.Test;
 import org.heigit.ors.common.DataAccessEnum;
 import org.heigit.ors.common.EncoderNameEnum;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.nio.file.Path;
 
 
 class ORSGraphHopperConfigTest {
-    @Test
-    void createGHSettingsWithoutElevation() {
+    @ParameterizedTest
+    @CsvSource({"true,/elevation_cache,srtm", "false,/elevation_cache,null", "true,null,srtm"})
+    void createGHSettings(Boolean setElevation, String cachePath, String expectedProvider) {
         ProfileProperties profile = new ProfileProperties();
         profile.setEncoderName(EncoderNameEnum.DRIVING_CAR);
-        profile.getBuild().setElevation(false);
+        profile.getBuild().setElevation(setElevation);
         profile.setProfileName("bobby-car");
 
         EngineProperties engineConfig = new EngineProperties();
         engineConfig.setGraphsDataAccess(DataAccessEnum.MMAP);
-        engineConfig.getElevation().setCachePath(Path.of("/elevation_cache"));
+        engineConfig.getElevation().setCachePath(Path.of(cachePath));
+        engineConfig.getElevation().setProvider(expectedProvider);
         engineConfig.getElevation().setDataAccess(DataAccessEnum.MMAP);
 
         String graphLocation = "";
 
         ORSGraphHopperConfig config = ORSGraphHopperConfig.createGHSettings(profile, engineConfig, graphLocation);
 
-        assert !config.toString().contains("graph.elevation.provider");
-    }
-
-    @Test
-    void createGHSettingsWithElevation() {
-        ProfileProperties profile = new ProfileProperties();
-        profile.setEncoderName(EncoderNameEnum.DRIVING_CAR);
-        profile.getBuild().setElevation(true);
-        profile.setProfileName("bobby-car");
-
-        EngineProperties engineConfig = new EngineProperties();
-        engineConfig.setGraphsDataAccess(DataAccessEnum.MMAP);
-        engineConfig.getElevation().setCachePath(Path.of("/elevation_cache"));
-        engineConfig.getElevation().setProvider("srtm");
-        engineConfig.getElevation().setDataAccess(DataAccessEnum.MMAP);
-
-        String graphLocation = "";
-
-        ORSGraphHopperConfig config = ORSGraphHopperConfig.createGHSettings(profile, engineConfig, graphLocation);
-
-        assert config.toString().contains("graph.elevation.provider");
-    }
-
-    @Test
-    void createGHSettingsWithElevationNoCachePath() {
-        ProfileProperties profile = new ProfileProperties();
-        profile.setEncoderName(EncoderNameEnum.DRIVING_CAR);
-        profile.getBuild().setElevation(true);
-        profile.setProfileName("bobby-car");
-
-        EngineProperties engineConfig = new EngineProperties();
-        engineConfig.setGraphsDataAccess(DataAccessEnum.MMAP);
-        engineConfig.getElevation().setCachePath(null);
-        engineConfig.getElevation().setProvider("srtm");
-        engineConfig.getElevation().setDataAccess(DataAccessEnum.MMAP);
-
-        String graphLocation = "";
-
-        ORSGraphHopperConfig config = ORSGraphHopperConfig.createGHSettings(profile, engineConfig, graphLocation);
-
-        assert !config.toString().contains("graph.elevation.provider");
-        }
-
-    @Test
-    void createGHSettingsWithElevationNoProvider() {
-        ProfileProperties profile = new ProfileProperties();
-        profile.setEncoderName(EncoderNameEnum.DRIVING_CAR);
-        profile.getBuild().setElevation(true);
-        profile.setProfileName("bobby-car");
-
-        EngineProperties engineConfig = new EngineProperties();
-        engineConfig.setGraphsDataAccess(DataAccessEnum.MMAP);
-        engineConfig.getElevation().setCachePath(Path.of("/elevation_cache"));
-        engineConfig.getElevation().setProvider(null);
-        engineConfig.getElevation().setDataAccess(DataAccessEnum.MMAP);
-
-        String graphLocation = "";
-
-        ORSGraphHopperConfig config = ORSGraphHopperConfig.createGHSettings(profile, engineConfig, graphLocation);
-
-        assert !config.toString().contains("graph.elevation.provider");
+        assertEquals(setElevation, config.toString().contains("graph.elevation.provider"));
     }
 }
