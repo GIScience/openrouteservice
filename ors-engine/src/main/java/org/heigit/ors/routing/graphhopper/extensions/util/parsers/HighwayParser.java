475f8d5c4e931fef5c2e74b0ebc985ebd081584c
Sascha Fendrich <sascha.fendrich@heigit.org>
2025-09-24 09:22:53 +0200

    refactor: replace WayCategories 'highway' and 'fords' by EncodedValues

---
+++ ors-engine/src/main/java/org/heigit/ors/routing/graphhopper/extensions/util/parsers/HighwayParser.java
@@ -0,0 +1,52 @@
+/*  This file is part of Openrouteservice.
+ *
+ *  Openrouteservice is free software; you can redistribute it and/or modify it under the terms of the
+ *  GNU Lesser General Public License as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+
+ *  This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU Lesser General Public License for more details.
+
+ *  You should have received a copy of the GNU Lesser General Public License along with this library;
+ *  if not, see <https://www.gnu.org/licenses/>.
+ */
+package org.heigit.ors.routing.graphhopper.extensions.util.parsers;
+
+import com.graphhopper.reader.ReaderWay;
+import com.graphhopper.routing.ev.BooleanEncodedValue;
+import com.graphhopper.routing.ev.EncodedValue;
+import com.graphhopper.routing.ev.EncodedValueLookup;
+import com.graphhopper.routing.ev.Highway;
+import com.graphhopper.routing.util.parsers.TagParser;
+import com.graphhopper.storage.IntsRef;
+
+import java.util.List;
+
+public class HighwayParser implements TagParser {
+    private BooleanEncodedValue highwayEnc;
+
+    public HighwayParser() {
+        this(Highway.create());
+    }
+
+    public HighwayParser(BooleanEncodedValue highwayEnc) {
+        this.highwayEnc = highwayEnc;
+    }
+
+    @Override
+    public void createEncodedValues(EncodedValueLookup encodedValueLookup, List<EncodedValue> list) {
+        list.add(highwayEnc);
+    }
+
+    @Override
+    public IntsRef handleWayTags(IntsRef edgeFlags, ReaderWay way, boolean b, IntsRef intsRef1) {
+        //Keep in sync with documentation: waycategory.md
+
+        String value = way.getTag("highway", "");
+        boolean isHighway = value.equals("motorway") || value.equals("motorway_link");
+        highwayEnc.setBool(isHighway, edgeFlags, false);
+        highwayEnc.setBool(isHighway, edgeFlags, true);
+        return edgeFlags;
+    }
+}
