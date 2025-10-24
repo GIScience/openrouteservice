7b86a4b4be5fe572d464f73039d097954dea4a7b
aoles <andrzej.oles@gmail.com>
2025-10-24 14:08:07 +0200

    refactor: more concise code of `EncodedValuesProperties` for the sake of maintainability

--- ors-engine/src/main/java/org/heigit/ors/config/profile/EncodedValuesProperties.java
+++ ors-engine/src/main/java/org/heigit/ors/config/profile/EncodedValuesProperties.java
@@ -8,8 +8,10 @@ import com.graphhopper.routing.ev.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import static java.util.Optional.ofNullable;
 
@@ -17,41 +19,41 @@ import static java.util.Optional.ofNullable;
 @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class EncodedValuesProperties {
-    @JsonProperty("ford")
+    @JsonProperty(Ford.KEY)
     private Boolean ford;
-    @JsonProperty("highway")
+    @JsonProperty(Highway.KEY)
     private Boolean highway;
-    @JsonProperty("osm_way_id")
+    @JsonProperty(OsmWayId.KEY)
     private Boolean osmWayId;
-    @JsonProperty("way_surface")
+    @JsonProperty(WaySurface.KEY)
     private Boolean waySurface;
-    @JsonProperty("way_type")
+    @JsonProperty(WayType.KEY)
     private Boolean wayType;
-    @JsonProperty("road_environment")
+    @JsonProperty(RoadEnvironment.KEY)
     private Boolean roadEnvironment;
-    @JsonProperty("agricultural_access")
+    @JsonProperty(AgriculturalAccess.KEY)
     private Boolean agriculturalAccess;
-    @JsonProperty("bus_access")
+    @JsonProperty(BusAccess.KEY)
     private Boolean busAccess;
-    @JsonProperty("delivery_access")
+    @JsonProperty(DeliveryAccess.KEY)
     private Boolean deliveryAccess;
-    @JsonProperty("forestry_access")
+    @JsonProperty(ForestryAccess.KEY)
     private Boolean forestryAccess;
-    @JsonProperty("goods_access")
+    @JsonProperty(GoodsAccess.KEY)
     private Boolean goodsAccess;
-    @JsonProperty("hgv_access")
+    @JsonProperty(HgvAccess.KEY)
     private Boolean hgvAccess;
-    @JsonProperty("hazmat_access")
+    @JsonProperty(HazmatAccess.KEY)
     private Boolean hazmatAccess;
-    @JsonProperty("max_axle_load")
+    @JsonProperty(MaxAxleLoad.KEY)
     private Boolean maxAxleLoad;
-    @JsonProperty("max_height")
+    @JsonProperty(MaxHeight.KEY)
     private Boolean maxHeight;
-    @JsonProperty("max_length")
+    @JsonProperty(MaxLength.KEY)
     private Boolean maxLength;
-    @JsonProperty("max_weight")
+    @JsonProperty(MaxWeight.KEY)
     private Boolean maxWeight;
-    @JsonProperty("max_width")
+    @JsonProperty(MaxWidth.KEY)
     private Boolean maxWidth;
 
     public EncodedValuesProperties() {
@@ -62,74 +64,42 @@ public class EncodedValuesProperties {
     }
 
     @JsonIgnore
+    private Map<String, Boolean> getProperties() {
+        Map<String, Boolean> properties = new HashMap<>();
+
+        properties.put(Ford.KEY, ford);
+        properties.put(Highway.KEY, highway);
+        properties.put(OsmWayId.KEY, osmWayId);
+        properties.put(WaySurface.KEY, waySurface);
+        properties.put(WayType.KEY, wayType);
+        properties.put(RoadEnvironment.KEY, roadEnvironment);
+        properties.put(AgriculturalAccess.KEY, agriculturalAccess);
+        properties.put(BusAccess.KEY, busAccess);
+        properties.put(DeliveryAccess.KEY, deliveryAccess);
+        properties.put(ForestryAccess.KEY, forestryAccess);
+        properties.put(GoodsAccess.KEY, goodsAccess);
+        properties.put(HgvAccess.KEY, hgvAccess);
+        properties.put(HazmatAccess.KEY, hazmatAccess);
+        properties.put(MaxAxleLoad.KEY, maxAxleLoad);
+        properties.put(MaxHeight.KEY, maxHeight);
+        properties.put(MaxLength.KEY, maxLength);
+        properties.put(MaxWeight.KEY, maxWeight);
+        properties.put(MaxWidth.KEY, maxWidth);
+
+        return properties;
+    }
+
+    @JsonIgnore
     public boolean isEmpty() {
-        return osmWayId == null &&
-                ford == null && highway == null &&
-                waySurface == null && wayType == null &&
-                roadEnvironment == null &&
-                agriculturalAccess == null && busAccess == null && deliveryAccess == null && forestryAccess == null && goodsAccess == null && hgvAccess == null &&
-                hazmatAccess == null &&
-                maxAxleLoad == null && maxHeight == null && maxLength == null && maxWeight == null && maxWidth == null;
+        return getProperties().values().stream().allMatch(Objects::isNull);
     }
 
     @JsonIgnore
     public String toString() {
-        List<String> out = new ArrayList<>();
-        if (Boolean.TRUE.equals(ford)) {
-            out.add(Ford.KEY);
-        }
-        if (Boolean.TRUE.equals(highway)) {
-            out.add(Highway.KEY);
-        }
-        if (Boolean.TRUE.equals(osmWayId)) {
-            out.add(OsmWayId.KEY);
-        }
-        if (Boolean.TRUE.equals(waySurface)) {
-            out.add(WaySurface.KEY);
-        }
-        if (Boolean.TRUE.equals(wayType)) {
-            out.add(WayType.KEY);
-        }
-        if (Boolean.TRUE.equals(roadEnvironment)) {
-            out.add(RoadEnvironment.KEY);
-        }
-        if (Boolean.TRUE.equals(agriculturalAccess)) {
-            out.add(AgriculturalAccess.KEY);
-        }
-        if (Boolean.TRUE.equals(busAccess)) {
-            out.add(BusAccess.KEY);
-        }
-        if (Boolean.TRUE.equals(deliveryAccess)) {
-            out.add(DeliveryAccess.KEY);
-        }
-        if (Boolean.TRUE.equals(forestryAccess)) {
-            out.add(ForestryAccess.KEY);
-        }
-        if (Boolean.TRUE.equals(goodsAccess)) {
-            out.add(GoodsAccess.KEY);
-        }
-        if (Boolean.TRUE.equals(hgvAccess)) {
-            out.add(HgvAccess.KEY);
-        }
-        if (Boolean.TRUE.equals(hazmatAccess)) {
-            out.add(HazmatAccess.KEY);
-        }
-        if (Boolean.TRUE.equals(maxAxleLoad)) {
-            out.add(MaxAxleLoad.KEY);
-        }
-        if (Boolean.TRUE.equals(maxHeight)) {
-            out.add(MaxHeight.KEY);
-        }
-        if (Boolean.TRUE.equals(maxLength)) {
-            out.add(MaxLength.KEY);
-        }
-        if (Boolean.TRUE.equals(maxWeight)) {
-            out.add(MaxWeight.KEY);
-        }
-        if (Boolean.TRUE.equals(maxWidth)) {
-            out.add(MaxWidth.KEY);
-        }
-        return String.join(",", out);
+        return getProperties().entrySet().stream()
+                .filter(e -> Boolean.TRUE.equals(e.getValue()))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.joining(","));
     }
 
     public void merge(EncodedValuesProperties other) {
@@ -153,5 +123,3 @@ public class EncodedValuesProperties {
         maxWidth = ofNullable(this.maxWidth).orElse(other.maxWidth);
     }
 }
-
-
