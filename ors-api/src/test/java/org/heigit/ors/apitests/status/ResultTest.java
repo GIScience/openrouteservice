5b857b3ebc545549753915b8a0b49a46698179aa
takb <takara.baumbach@heigit.org>
2025-11-14 17:58:49 +0100

    fix: move tests for the Status Endpoint with DynamicData content

--- ors-api/src/test/java/org/heigit/ors/apitests/status/ResultTest.java
+++ ors-api/src/test/java/org/heigit/ors/apitests/status/ResultTest.java
@@ -13,28 +13,17 @@
  */
 package org.heigit.ors.apitests.status;
 
-import org.heigit.ors.apitests.common.AbstractContainerBaseTest;
 import org.heigit.ors.apitests.common.EndPointAnnotation;
+import org.heigit.ors.apitests.common.ServiceTest;
 import org.heigit.ors.apitests.common.VersionAnnotation;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.junit.jupiter.TestcontainersExtension;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
-// Test order is needed to ensure that this test runs first because the
-// database from which the dynamic data is fetched must be available before
-// ORS is started. If other tests run first they may start ORS too early.
-// (see also test/resources/junit-platform.properties)
-@Order(0)
 @EndPointAnnotation(name = "status")
 @VersionAnnotation(version = "v2")
-@ExtendWith(TestcontainersExtension.class)
-@Testcontainers(disabledWithoutDocker = true)
-class ResultTest extends AbstractContainerBaseTest {
+class ResultTest extends ServiceTest {
 
     @Test
     void testGetStatus() {
@@ -46,12 +35,6 @@ class ResultTest extends AbstractContainerBaseTest {
                 .body("any { it.key == 'services' }", is(true))
                 .body("any { it.key == 'languages' }", is(true))
                 .body("any { it.key == 'profiles' }", is(true))
-                .body("profiles.driving-car.dynamic_data.logie_bridges.mapped_edges", is(1))
-                .body("profiles.driving-car.dynamic_data.logie_borders.mapped_edges", is(1))
-                .body("profiles.driving-car.dynamic_data.logie_roads.mapped_edges", is(2))
-                .body("dynamic_data_service.logie_bridges.count_features", is(1))
-                .body("dynamic_data_service.logie_borders.count_features", is(1))
-                .body("dynamic_data_service.logie_roads.count_features", is(1))
                 .statusCode(200);
     }
 }
