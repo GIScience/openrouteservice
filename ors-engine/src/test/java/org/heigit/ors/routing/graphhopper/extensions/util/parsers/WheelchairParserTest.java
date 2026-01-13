36826ad6c67532fa6440d674238d7e0ccba1400a
jarinox <45308098+jarinox@users.noreply.github.com>
2026-01-13 10:44:53 +0100

    test: migrate WheelchairGraphStorageBuilderTest to WheelchairParserTest

--- ors-engine/src/test/java/org/heigit/ors/routing/graphhopper/extensions/util/parsers/WheelchairParserTest.java
+++ ors-engine/src/test/java/org/heigit/ors/routing/graphhopper/extensions/util/parsers/WheelchairParserTest.java
@@ -1,26 +1,71 @@
-package org.heigit.ors.routing.graphhopper.extensions.storages.builders;
+package org.heigit.ors.routing.graphhopper.extensions.util.parsers;
 
 import com.graphhopper.reader.ReaderWay;
+import com.graphhopper.routing.util.EncodingManager;
+import com.graphhopper.storage.IntsRef;
 import org.heigit.ors.routing.graphhopper.extensions.WheelchairAttributes;
+import org.heigit.ors.routing.graphhopper.extensions.util.WheelchairAttributesEncodedValues;
+import org.heigit.ors.routing.graphhopper.extensions.util.parsers.wheelchair.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.locationtech.jts.geom.Coordinate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class WheelchairGraphStorageBuilderTest {
-    private WheelchairGraphStorageBuilder builder;
+class WheelchairParserTest {
+    private EncodingManager em;
+    private IntsRef intsRef, relFlags;
 
-    public WheelchairGraphStorageBuilderTest() {
-        builder = new WheelchairGraphStorageBuilder();
+    private WheelchairSurfaceParser parserSurface;
+    private WheelchairSmoothnessParser parserSmoothness;
+    private WheelchairTrackTypeParser parserTrackType;
+    private WheelchairInclineParser parserIncline;
+    private WheelchairKerbHeightParser parserKerbHeight;
+    private WheelchairWidthParser parserWidth;
+    private WheelchairSuitableParser parserSuitable;
+    private WheelchairSurfaceQualityKnownParser parserSurfaceQualityKnown;
+    private WheelchairSideParser parserSide;
+
+    public WheelchairParserTest() {
+        setUp();
     }
 
     @BeforeEach
-    void reset() {
-        builder = new WheelchairGraphStorageBuilder();
+    void setUp() {
+        parserSurface = new WheelchairSurfaceParser();
+        parserSmoothness = new WheelchairSmoothnessParser();
+        parserTrackType = new WheelchairTrackTypeParser();
+        parserIncline = new WheelchairInclineParser();
+        parserKerbHeight = new WheelchairKerbHeightParser();
+        parserWidth = new WheelchairWidthParser();
+        parserSuitable = new WheelchairSuitableParser();
+        parserSurfaceQualityKnown = new WheelchairSurfaceQualityKnownParser();
+        parserSide = new WheelchairSideParser();
+
+        em = new EncodingManager.Builder()
+                .add(parserSurface)
+                .add(parserSmoothness)
+                .add(parserTrackType)
+                .add(parserIncline)
+                .add(parserKerbHeight)
+                .add(parserWidth)
+                .add(parserSuitable)
+                .add(parserSurfaceQualityKnown)
+                .add(parserSide).build();
+
+        relFlags = em.createRelationFlags();
+        intsRef = em.createEdgeFlags();
+    }
+
+    void executeParsers(ReaderWay way) {
+        parserSurface.handleWayTags(intsRef, way, false, relFlags);
+        parserSmoothness.handleWayTags(intsRef, way, false, relFlags);
+        parserTrackType.handleWayTags(intsRef, way, false, relFlags);
+        parserIncline.handleWayTags(intsRef, way, false, relFlags);
+        parserKerbHeight.handleWayTags(intsRef, way, false, relFlags);
+        parserWidth.handleWayTags(intsRef, way, false, relFlags);
+        parserSuitable.handleWayTags(intsRef, way, false, relFlags);
+        parserSurfaceQualityKnown.handleWayTags(intsRef, way, false, relFlags);
+        parserSide.handleWayTags(intsRef, way, false, relFlags);
     }
 
     @Test
@@ -33,10 +78,11 @@ class WheelchairGraphStorageBuilderTest {
         way.setTag("smoothness", "good");
         way.setTag("surface", "asphalt");
 
+        executeParsers(way);
 
-        builder.processWay(way);
+        WheelchairAttributesEncodedValues encValues = new WheelchairAttributesEncodedValues(em);
+        WheelchairAttributes attrs = encValues.getAttributes(intsRef);
 
-        WheelchairAttributes attrs = builder.getStoredAttributes(WheelchairGraphStorageBuilder.Side.NONE);
         assertEquals(50, attrs.getWidth());
         assertEquals(2, attrs.getIncline());
         assertEquals(3, attrs.getSlopedKerbHeight());
@@ -56,10 +102,11 @@ class WheelchairGraphStorageBuilderTest {
         way.setTag("tracktype", "grade1");
         way.setTag("surface", "asphalt");
 
+        executeParsers(way);
 
-        builder.processWay(way);
+        WheelchairAttributesEncodedValues encValues = new WheelchairAttributesEncodedValues(em);
+        WheelchairAttributes attrs = encValues.getAttributes(intsRef);
 
-        WheelchairAttributes attrs = builder.getStoredAttributes(WheelchairGraphStorageBuilder.Side.NONE);
         assertEquals(50, attrs.getWidth());
         assertEquals(2, attrs.getIncline());
         assertEquals(2, attrs.getSurfaceType());
@@ -71,26 +118,28 @@ class WheelchairGraphStorageBuilderTest {
     @Test
     void TestProcessWayWithLeftSidewalkAttached() {
         ReaderWay way = constructSidedWay("left");
-        builder.processWay(way);
-        WheelchairAttributes attrs = builder.getStoredAttributes(WheelchairGraphStorageBuilder.Side.LEFT);
+        executeParsers(way);
+        WheelchairAttributesEncodedValues encValues = new WheelchairAttributesEncodedValues(em);
+        WheelchairAttributes attrs = encValues.getAttributes(intsRef);
         assertAttributeValues(attrs);
     }
 
     @Test
     void TestProcessWayWithRightSidewalkAttached() {
         ReaderWay way = constructSidedWay("right");
-        builder.processWay(way);
-        WheelchairAttributes attrs = builder.getStoredAttributes(WheelchairGraphStorageBuilder.Side.RIGHT);
+        executeParsers(way);
+        WheelchairAttributesEncodedValues encValues = new WheelchairAttributesEncodedValues(em);
+        WheelchairAttributes attrs = encValues.getAttributes(intsRef);
         assertAttributeValues(attrs);
     }
 
     @Test
     void TestProcessWayWithBothSidewalksAttached() {
         ReaderWay way = constructSidedWay("both");
-        builder.processWay(way);
-        WheelchairAttributes attrs = builder.getStoredAttributes(WheelchairGraphStorageBuilder.Side.RIGHT);
+        WheelchairAttributes attrs = parseForSide(way, "right");
         assertAttributeValues(attrs);
-        attrs = builder.getStoredAttributes(WheelchairGraphStorageBuilder.Side.LEFT);
+
+        attrs = parseForSide(way, "left");
         assertAttributeValues(attrs);
     }
 
@@ -121,62 +170,35 @@ class WheelchairGraphStorageBuilderTest {
         assertFalse(correctWheelchairAttributes.isSurfaceQualityKnown());
         assertFalse(correctWheelchairAttributes.isSuitable());
 
-        builder.processWay(way);
-        WheelchairAttributes left_attrs = builder.getStoredAttributes(WheelchairGraphStorageBuilder.Side.LEFT);
+        WheelchairAttributes left_attrs = parseForSide(way, "left");
         assertTrue(left_attrs.isSurfaceQualityKnown());
         assertTrue(left_attrs.isSuitable());
+        assertEquals(60, left_attrs.getWidth());
+        assertEquals(5, left_attrs.getIncline());
+        assertEquals(1, left_attrs.getSlopedKerbHeight());
 
-        WheelchairAttributes right_attrs = builder.getStoredAttributes(WheelchairGraphStorageBuilder.Side.RIGHT);
+        WheelchairAttributes right_attrs = parseForSide(way, "right");
         assertTrue(right_attrs.isSurfaceQualityKnown());
         assertTrue(right_attrs.isSuitable());
+        assertEquals(50, right_attrs.getWidth());
+        assertEquals(2, right_attrs.getIncline());
+        assertEquals(3, right_attrs.getSlopedKerbHeight());
 
-        WheelchairAttributes attrs = builder.combineAttributesOfWayWhenBothSidesPresent(new WheelchairAttributes());
-        assertFalse(attrs.isSurfaceQualityKnown());
-        assertFalse(attrs.isSuitable());
-
+        WheelchairAttributes attrs = parseForSide(way, "both");
         assertEquals(wheelchairAttributesAsString(correctWheelchairAttributes), wheelchairAttributesAsString(attrs));
     }
 
-    @Test
-    void TestKerbHeightFromNode() {
-        ReaderWay way = new ReaderWay(1);
+    private WheelchairAttributes parseForSide(ReaderWay way, String side){
+        assert(side.equals("left") || side.equals("right") || side.equals("both"));
+        if(side.equals("both")) {
+            way.removeTag("ors-sidewalk-side");
+        } else {
+            way.setTag("ors-sidewalk-side", side);
+        }
 
-        way.setTag("highway", "crossing");
-
-        Map<Integer, Map<String, String>> nodeTags = new HashMap<>();
-        Map<String, String> tags = new HashMap<>();
-        tags.put("kerb:height", "0.03");
-        nodeTags.put(1, tags);
-
-        builder.processWay(way, new Coordinate[0], nodeTags);
-
-        assertEquals(3, builder.getKerbHeightFromNodeTags());
-    }
-
-    @Test
-    void TestAttachKerbHeightToCrossing() {
-        builder = new WheelchairGraphStorageBuilder(true);
-
-        ReaderWay way = new ReaderWay(1);
-
-        way.setTag("footway", "crossing");
-
-        Map<Integer, Map<String, String>> nodeTags = new HashMap<>();
-        Map<String, String> tags = new HashMap<>();
-        tags.put("kerb:height", "0.03");
-        nodeTags.put(1, tags);
-
-        builder.processWay(way, new Coordinate[0], nodeTags);
-
-        assertEquals(3, builder.getKerbHeightForEdge(way));
-
-        way = new ReaderWay(2);
-
-        way.setTag("highway", "footway");
-
-        builder.processWay(way, new Coordinate[0], nodeTags);
-
-        assertEquals(-1, builder.getKerbHeightForEdge(way));
+        executeParsers(way);
+        WheelchairAttributesEncodedValues encValues = new WheelchairAttributesEncodedValues(em);
+        return encValues.getAttributes(intsRef);
     }
 
     private ReaderWay constructSidedWay(String side) {
@@ -189,6 +211,7 @@ class WheelchairGraphStorageBuilderTest {
         way.setTag("sidewalk:" + side + ":smoothness", "good");
         way.setTag("sidewalk:" + side + ":surface", "asphalt");
         way.setTag("sidewalk:" + side + ":tracktype", "grade4");
+        way.setTag("ors-sidewalk-side", side);
 
         return way;
     }
