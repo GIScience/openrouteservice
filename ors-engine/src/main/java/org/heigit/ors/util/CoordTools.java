9c172548078999d190e16744c220e7b8ebcbb049
aoles <andrzej.oles@gmail.com>
2026-02-04 14:02:42 +0100

    refactor: switch to GraphHopper's method for computing Harvesine distance

--- ors-engine/src/main/java/org/heigit/ors/util/CoordTools.java
+++ ors-engine/src/main/java/org/heigit/ors/util/CoordTools.java
@@ -13,9 +13,8 @@
  */
 package org.heigit.ors.util;
 
-import org.locationtech.jts.geom.Coordinate;
-
-import static java.lang.Math.*;
+import com.graphhopper.util.DistanceCalc;
+import com.graphhopper.util.DistanceCalcEarth;
 
 /**
  * <p>
@@ -34,41 +33,12 @@ import static java.lang.Math.*;
  * @version 1.0 2006-05-01
  */
 public class CoordTools {
-    private static final double R = 6371000;
-    private static final double R2 = 2 * R;
-    private static final double DEG_TO_RAD = 0.017453292519943295769236907684886;
-    private static final double DEG_TO_RAD_HALF = 0.017453292519943295769236907684886 / 2.0;
+    private static final DistanceCalc distanceCalc = new DistanceCalcEarth();
 
     private CoordTools() {
     }
 
     public static double calcDistHaversine(double lon0, double lat0, double lon1, double lat1) {
-        double sinDLat = sin(DEG_TO_RAD_HALF * (lat1 - lat0));
-        double sinDLon = sin(DEG_TO_RAD_HALF * (lon1 - lon0));
-        double c = sinDLat * sinDLat + sinDLon * sinDLon * cos(DEG_TO_RAD * lat0) * cos(DEG_TO_RAD * lat1);
-
-        return R2 * asin(sqrt(c));
-    }
-
-    public static Coordinate[] parse(String value, String separator, boolean is3D, boolean inverseXY) {
-        String[] coordValues = value.split(separator);
-        Coordinate[] coords = new Coordinate[coordValues.length];
-
-        for (int i = 0; i < coordValues.length; i++) {
-            String[] locations = coordValues[i].split(",");
-            if (inverseXY) {
-                if (is3D && locations.length == 3)
-                    coords[i] = new Coordinate(Double.parseDouble(locations[1]), Double.parseDouble(locations[0]), Double.parseDouble(locations[2]));
-                else
-                    coords[i] = new Coordinate(Double.parseDouble(locations[1]), Double.parseDouble(locations[0]));
-            } else {
-                if (is3D && locations.length == 3)
-                    coords[i] = new Coordinate(Double.parseDouble(locations[0]), Double.parseDouble(locations[1]), Double.parseDouble(locations[2]));
-                else
-                    coords[i] = new Coordinate(Double.parseDouble(locations[0]), Double.parseDouble(locations[1]));
-            }
-        }
-
-        return coords;
+        return distanceCalc.calcDist(lat0, lon0, lat1, lon1);
     }
 }
