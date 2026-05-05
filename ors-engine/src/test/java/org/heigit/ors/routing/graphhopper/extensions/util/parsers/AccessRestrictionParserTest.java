1d63d3724fe835c548aa4a29e5a8d64cc13bbfc4
aoles <andrzej.oles@gmail.com>
2026-05-05 12:14:31 +0200

    test: re-introduce unit tests
    Co-authored-by: Sascha Fendrich <sascha.fendrich@heigit.org>

---
+++ ors-engine/src/test/java/org/heigit/ors/routing/graphhopper/extensions/util/parsers/AccessRestrictionParserTest.java
@@ -0,0 +1,118 @@
+package org.heigit.ors.routing.graphhopper.extensions.util.parsers;
+
+import com.graphhopper.reader.ReaderWay;
+import com.graphhopper.routing.ev.AccessRestriction;
+import com.graphhopper.routing.ev.IntEncodedValue;
+import com.graphhopper.routing.util.EncodingManager;
+import com.graphhopper.storage.IntsRef;
+import org.heigit.ors.routing.RoutingProfileType;
+import org.heigit.ors.routing.graphhopper.extensions.AccessRestrictionType;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AccessRestrictionParserTest {
+    private IntEncodedValue accessRestrictionEnc;
+    private AccessRestrictionsParser parser;
+    private IntsRef intsRef, relFlags;
+
+
+    void initParser(int profileType) {
+        parser = new AccessRestrictionsParser(profileType);
+        EncodingManager em = new EncodingManager.Builder().add(parser).build();
+        accessRestrictionEnc = em.getIntEncodedValue(AccessRestriction.KEY);
+        relFlags = em.createRelationFlags();
+        intsRef = em.createEdgeFlags();
+    }
+
+    @Test
+    void testAccessTypes() {
+        initParser(RoutingProfileType.UNKNOWN);
+        ReaderWay way = new ReaderWay(1);
+
+        parser.handleWayTags(intsRef, way, false, relFlags);
+        assertEquals(AccessRestrictionType.NONE, accessRestrictionEnc.getInt(false, intsRef));
+
+        way.setTag("access", "no");
+        parser.handleWayTags(intsRef, way, false, relFlags);
+        assertEquals(AccessRestrictionType.NO, accessRestrictionEnc.getInt(false, intsRef));
+
+        way.setTag("access", "customers");
+        parser.handleWayTags(intsRef, way, false, relFlags);
+        assertEquals(AccessRestrictionType.CUSTOMERS, accessRestrictionEnc.getInt(false, intsRef));
+
+        way.setTag("access", "destination");
+        parser.handleWayTags(intsRef, way, false, relFlags);
+        assertEquals(AccessRestrictionType.DESTINATION, accessRestrictionEnc.getInt(false, intsRef));
+
+        way.setTag("access", "delivery");
+        parser.handleWayTags(intsRef, way, false, relFlags);
+        assertEquals(AccessRestrictionType.DELIVERY, accessRestrictionEnc.getInt(false, intsRef));
+
+        way.setTag("access", "private");
+        parser.handleWayTags(intsRef, way, false, relFlags);
+        assertEquals(AccessRestrictionType.PRIVATE, accessRestrictionEnc.getInt(false, intsRef));
+
+        way.setTag("access", "permissive");
+        parser.handleWayTags(intsRef, way, false, relFlags);
+        assertEquals(AccessRestrictionType.PERMISSIVE, accessRestrictionEnc.getInt(false, intsRef));
+
+        way.setTag("access", "permit");
+        parser.handleWayTags(intsRef, way, false, relFlags);
+        assertEquals(AccessRestrictionType.PERMIT, accessRestrictionEnc.getInt(false, intsRef));
+    }
+
+    @Test
+    void testCarWayCreation() {
+        initParser(RoutingProfileType.DRIVING_CAR);
+
+        ReaderWay way = new ReaderWay(1);
+        way.setTag("access", "no");
+        parser.handleWayTags(intsRef, way, false, relFlags);
+        assertEquals(AccessRestrictionType.NO, accessRestrictionEnc.getInt(false, intsRef));
+
+        way.setTag("motorcar", "destination");
+        parser.handleWayTags(intsRef, way, false, relFlags);
+        assertEquals(AccessRestrictionType.NO | AccessRestrictionType.DESTINATION, accessRestrictionEnc.getInt(false, intsRef));
+
+        way.setTag("motorcar", "yes");
+        parser.handleWayTags(intsRef, way, false, relFlags);
+        assertEquals(AccessRestrictionType.NONE, accessRestrictionEnc.getInt(false, intsRef));
+    }
+
+    @Test
+    void testBikeWayCreation() {
+        initParser(RoutingProfileType.CYCLING_REGULAR);
+
+        ReaderWay way = new ReaderWay(1);
+        way.setTag("access", "no");
+        parser.handleWayTags(intsRef, way, false, relFlags);
+        assertEquals(AccessRestrictionType.NO, accessRestrictionEnc.getInt(false, intsRef));
+
+        way.setTag("bicycle", "destination");
+        parser.handleWayTags(intsRef, way, false, relFlags);
+        assertEquals(AccessRestrictionType.NO | AccessRestrictionType.DESTINATION, accessRestrictionEnc.getInt(false, intsRef));
+
+        way.setTag("bicycle", "yes");
+        parser.handleWayTags(intsRef, way, false, relFlags);
+        assertEquals(AccessRestrictionType.NONE, accessRestrictionEnc.getInt(false, intsRef));
+    }
+
+    @Test
+    void testFootWayCreation() {
+        initParser(RoutingProfileType.FOOT_WALKING);
+
+        ReaderWay way = new ReaderWay(1);
+        way.setTag("access", "no");
+        parser.handleWayTags(intsRef, way, false, relFlags);
+        assertEquals(AccessRestrictionType.NO, accessRestrictionEnc.getInt(false, intsRef));
+
+        way.setTag("foot", "destination");
+        parser.handleWayTags(intsRef, way, false, relFlags);
+        assertEquals(AccessRestrictionType.NO | AccessRestrictionType.DESTINATION, accessRestrictionEnc.getInt(false, intsRef));
+
+        way.setTag("foot", "yes");
+        parser.handleWayTags(intsRef, way, false, relFlags);
+        assertEquals(AccessRestrictionType.NONE, accessRestrictionEnc.getInt(false, intsRef));
+    }
+}
