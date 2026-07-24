26190b42b6ec81bd726168639486ddfaaa088c2a
jarinox <45308098+jarinox@users.noreply.github.com>
2026-07-24 12:48:29 +0200

    chore: apply review suggestions

--- ors-engine/src/main/java/com/graphhopper/routing/ev/WheelchairIncline.java
+++ ors-engine/src/main/java/com/graphhopper/routing/ev/WheelchairIncline.java
@@ -2,7 +2,7 @@ package com.graphhopper.routing.ev;
 
 /**
  * EncodedValue for wheelchair incline attribute.
- * Stores incline percentage as a 5-bit unsigned integer value (range: 0 to 30).
+ * Stores incline percentage as a 4-bit unsigned integer value (range: 0 to 15, see WheelchairInclineParser.INCLINE_MAX_VALUE).
  */
 public class WheelchairIncline {
     public static final String KEY = "wheelchair_incline";
@@ -12,8 +12,8 @@ public class WheelchairIncline {
     }
 
     public static IntEncodedValue create() {
-        // 6 bits signed to store 0% to +30% incline
-        return new UnsignedIntEncodedValue(KEY, 5, false) {
+        // 4 bits unsigned to store 0% to +15% incline
+        return new UnsignedIntEncodedValue(KEY, 4, false) {
         };
     }
 }
