64131907d5aeca8bf80d1ffc78436674ff78fcc3
aoles <andrzej.oles@gmail.com>
2026-07-31 19:50:02 +0200

    refactor: define `Label` as subclass of `SPTEntry`
    Drop the need to define a specialized path extractor.

--- ors-engine/src/main/java/org/heigit/ors/routing/algorithms/Label.java
+++ ors-engine/src/main/java/org/heigit/ors/routing/algorithms/Label.java
@@ -1,44 +1,31 @@
 package org.heigit.ors.routing.algorithms;
 
+import com.graphhopper.routing.SPTEntry;
 import com.graphhopper.util.EdgeIterator;
-import lombok.Getter;
-import lombok.Setter;
 
-public class Label implements Comparable<Label> {
-    public int edgeId;
-    public int nodeId;
-    public double weight;
+public class Label extends SPTEntry {
     public double sinceRest;
-    @Getter
-    public Label parent;
-    @Getter
-    @Setter
     private boolean active = true;
 
     public Label(int edgeId, int nodeId, double weight, double sinceRest) {
-        this.edgeId = edgeId;
-        this.nodeId = nodeId;
-        this.weight = weight;
+        super(edgeId, nodeId, weight);
         this.sinceRest = sinceRest;
     }
-
 
     public static Label createStartLabel(int nodeId) {
         return new Label(EdgeIterator.NO_EDGE, nodeId, 0, 0);
     }
 
-    @SuppressWarnings("java:S1210")
     @Override
-    public int compareTo(Label o) {
-        if (weight < o.weight)
-            return -1;
-
-        // assumption no NaN and no -0
-        return weight > o.weight ? 1 : 0;
+    public Label getParent() {
+        return (Label) super.parent;
     }
 
-    @Override
-    public String toString() {
-        return nodeId + " (" + edgeId + ") weight: " + weight;
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
     }
 }
